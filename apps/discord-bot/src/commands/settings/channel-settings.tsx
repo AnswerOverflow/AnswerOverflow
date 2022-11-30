62936f51c1d0bce9d59d99380e91b340caa04de1
Rhys Sullivan <39114868+RhysSullivan@users.noreply.github.com>
2022-11-30 11:56:09 -0500

    Remove prisma query logging

--- apps/discord-bot/src/commands/settings/channel-settings.tsx
+++ apps/discord-bot/src/commands/settings/channel-settings.tsx
@@ -2,6 +2,7 @@ import { ChatInputCommand, Command } from "@sapphire/framework";
 import type { ChatInputCommandInteraction } from "discord.js";
 import { ChannelSettingsMenu } from "@reacord/channel-settings";
 import React from "react";
+import { makeAPICaller, makeChannelUpsert } from "@trpc/create-caller";
 
 export class ChannelSettingsCommand extends Command {
   // Register slash and context menu command
@@ -12,7 +13,7 @@ export class ChannelSettingsCommand extends Command {
       description: "Manage Channel Settings",
     });
   }
-  public override chatInputRun(
+  public override async chatInputRun(
     interaction: ChatInputCommandInteraction,
     // eslint-disable-next-line no-unused-vars
     context: ChatInputCommand.RunContext
@@ -20,10 +21,18 @@ export class ChannelSettingsCommand extends Command {
     if (!interaction.channel) {
       return;
     }
-
+    if (interaction.channel.isDMBased() || interaction.guild == null) {
+      await interaction.reply("Does not work in DMs");
+      return;
+    }
+    const api_caller = await makeAPICaller();
+    const settings = await api_caller.channel_settings.upsert({
+      data: {},
+      channel: makeChannelUpsert(interaction.channel, interaction.guild),
+    });
     this.container.reacord.ephemeralReply(
       interaction,
-      <ChannelSettingsMenu channel={interaction.channel} />
+      <ChannelSettingsMenu channel={interaction.channel} settings={settings} />
     );
   }
 }
--- packages/db/index.ts
+++ packages/db/index.ts
@@ -9,7 +9,7 @@ declare global {
 export const prisma =
   global.prisma ||
   new PrismaClient({
-    log: process.env.NODE_ENV === "development" ? ["query", "error", "warn"] : ["error"],
+    log: process.env.NODE_ENV === "development" ? ["error", "warn"] : ["error"],
   });
 
 export * from "@prisma/client";
