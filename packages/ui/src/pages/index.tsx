571db04f2ac98d6ec41c14acd10e1b7d4f76cf8d
Rhys Sullivan <39114868+RhysSullivan@users.noreply.github.com>
2022-12-03 18:08:35 -0500

    Init with storybook

--- packages/ui/src/pages/index.tsx
+++ packages/ui/src/pages/index.tsx
@@ -20,20 +20,23 @@ const Home: NextPage = () => {
               className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
               href="https://create.t3.gg/en/usage/first-steps"
             >
-              <h3 className="text-2xl font-bold">First Steps →</h3>
-              <div className="text-lg">
-                Just the basics - Everything you need to know to set up your
-                database and authentication.
+              <div>
+                <h3 className="text-2xl font-bold">First Steps →</h3>
+                <div className="text-lg">
+                  Just the basics - Everything you need to know to set up your database and
+                  authentication.
+                </div>
               </div>
             </Link>
             <Link
               className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
               href="https://create.t3.gg/en/introduction"
             >
-              <h3 className="text-2xl font-bold">Documentation →</h3>
-              <div className="text-lg">
-                Learn more about Create T3 App, the libraries it uses, and how
-                to deploy it.
+              <div>
+                <h3 className="text-2xl font-bold">Documentation →</h3>
+                <div className="text-lg">
+                  Learn more about Create T3 App, the libraries it uses, and how to deploy it.
+                </div>
               </div>
             </Link>
           </div>
---
+++ packages/ui/src/stories/Page.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import Home from "../pages";
+
+import "./page.css";
+
+export const Page: React.VFC = () => {
+  return <Home />;
+};
