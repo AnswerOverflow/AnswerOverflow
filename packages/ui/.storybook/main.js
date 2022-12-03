d72d14839372c9ab4cf34238f65fb6611d2001cb
Rhys Sullivan <39114868+RhysSullivan@users.noreply.github.com>
2022-12-03 18:33:47 -0500

    Speed up storybook, fix deprecation warnings, move over to using proper tsconfig

--- packages/ui/.storybook/main.js
+++ packages/ui/.storybook/main.js
@@ -19,5 +19,14 @@ module.exports = {
   "framework": "@storybook/react",
   "core": {
     "builder": "@storybook/builder-webpack5"
-  }
+  },
+  typescript: {
+    check: false,
+    checkOptions: {},
+    reactDocgen: false,
+    reactDocgenTypescriptOptions: {
+       shouldExtractLiteralValuesFromEnum: true,
+       propFilter: (prop) => (prop.parent ? !/node_modules/.test(prop.parent.fileName) : true),
+    },
+ },
 }
