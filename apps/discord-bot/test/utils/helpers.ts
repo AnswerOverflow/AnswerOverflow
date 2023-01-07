1f635d21f3113b9bc9ba3936e2420c0dd25812e4
Rhys Sullivan <39114868+RhysSullivan@users.noreply.github.com>
2023-01-07 02:25:30 -0500

    Update helpers.ts

--- apps/discord-bot/test/utils/helpers.ts
+++ apps/discord-bot/test/utils/helpers.ts
@@ -1,4 +1,24 @@
+import type { Client, ClientEvents } from "discord.js";
+
 // Bit of a hack of a helper function to give async tasks that aren't tracked time to run. A better approach would be to listen to dispatched events
 export async function delay(timeout: number = 100) {
   await new Promise((resolve) => setTimeout(resolve, timeout));
+}
+
+export async function emitEvent<E extends keyof ClientEvents>(
+  client: Client,
+  event: E,
+  ...args: ClientEvents[E]
+) {
+  const status = client.emit(event, ...args);
+  await delay();
+  return status;
+}
+
+export function copyClass<T>(obj: T) {
+  return Object.assign(
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+    Object.create(Object.getPrototypeOf(obj)),
+    obj
+  ) as T;
 }
