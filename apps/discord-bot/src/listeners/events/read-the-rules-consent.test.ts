128bfef0cfc44edd4a94c674990755887d460d60
Rhys Sullivan <39114868+RhysSullivan@users.noreply.github.com>
2023-01-22 15:38:14 -0800

    Remove once from read the rules consent (#92)

--- apps/discord-bot/src/listeners/events/read-the-rules-consent.test.ts
+++ apps/discord-bot/src/listeners/events/read-the-rules-consent.test.ts
@@ -1,9 +1,11 @@
 import { Client, Events } from "discord.js";
+import { mockGuild } from "~discord-bot/test/utils/discordjs/guild-mock";
 import {
   createGuildMemberVariants,
   GuildMemberVariants,
   setupBot,
 } from "~discord-bot/test/utils/discordjs/scenarios";
+import { mockGuildMember } from "~discord-bot/test/utils/discordjs/user-mock";
 import { copyClass, emitEvent, testOnlyAPICall } from "~discord-bot/test/utils/helpers";
 import { toAOServer } from "~discord-bot/utils/conversions";
 
@@ -76,5 +78,46 @@ describe("Read the rules consent", () => {
       })
     );
     expect(updated_settings).toBe(null);
+  });
+  it("should mark multiple users as consenting in a server with read the rules consent enabled", async () => {
+    const server = mockGuild(client);
+    const members = [
+      mockGuildMember({ client, guild: server, data: { pending: true } }),
+      mockGuildMember({ client, guild: server, data: { pending: true } }),
+      mockGuildMember({ client, guild: server, data: { pending: true } }),
+      mockGuildMember({ client, guild: server, data: { pending: true } }),
+      mockGuildMember({ client, guild: server, data: { pending: true } }),
+      mockGuildMember({ client, guild: server, data: { pending: true } }),
+      mockGuildMember({ client, guild: server, data: { pending: true } }),
+      mockGuildMember({ client, guild: server, data: { pending: true } }),
+      mockGuildMember({ client, guild: server, data: { pending: true } }),
+      mockGuildMember({ client, guild: server, data: { pending: true } }),
+      mockGuildMember({ client, guild: server, data: { pending: true } }),
+    ];
+    for await (const pending_member of members) {
+      // setup
+      await testOnlyAPICall((router) =>
+        router.server_settings.upsertWithDeps({
+          server: toAOServer(pending_member.guild),
+          flags: {
+            read_the_rules_consent_enabled: true,
+          },
+        })
+      );
+
+      // act
+      const full_member = copyClass(pending_member, client);
+      full_member.pending = false;
+      await emitEvent(client, Events.GuildMemberUpdate, pending_member, full_member);
+
+      // assert
+      const updated_settings = await testOnlyAPICall((router) =>
+        router.user_server_settings.byId({
+          user_id: full_member.id,
+          server_id: full_member.guild.id,
+        })
+      );
+      expect(updated_settings!.flags.can_publicly_display_messages).toBe(true);
+    }
   });
 });
