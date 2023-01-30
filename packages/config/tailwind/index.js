43ef3c31e1fa442e6e5f9a67bbac671c94d5cf02
Rhys Sullivan <39114868+RhysSullivan@users.noreply.github.com>
2023-01-30 09:00:08 -0500

    Remove unused flowbite dependencies

--- packages/config/tailwind/index.js
+++ packages/config/tailwind/index.js
@@ -5,8 +5,6 @@ module.exports = {
   content: ["./src/**/*.{ts,tsx}", "./src/_app.tsx"],
   plugins: [
     // eslint-disable-next-line no-undef
-    require("flowbite/plugin"),
-    // eslint-disable-next-line no-undef
     require("@tailwindcss/forms"),
     // eslint-disable-next-line no-undef
     require("tailwind-scrollbar-hide"),
