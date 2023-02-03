3dfcadf1487cb48155ee26304f29217a7d77d555
Rhys Sullivan <39114868+RhysSullivan@users.noreply.github.com>
2023-02-02 22:42:04 -0500

    Clean & regenerate database when running tests

--- apps/discord-bot/src/listeners/parity/guild-parity.test.ts
+++ apps/discord-bot/src/listeners/parity/guild-parity.test.ts
@@ -1,14 +1,15 @@
 import type { SapphireClient } from "@sapphire/framework";
 import { Events, Guild } from "discord.js";
-import { prisma } from "@answeroverflow/db";
 import {
   mockGuild,
   mockTextChannel,
   mockForumChannel,
   emitEvent,
   copyClass,
+  delay,
 } from "@answeroverflow/discordjs-mock";
 import { setupAnswerOverflowBot } from "~discord-bot/test/sapphire-mock";
+import { testOnlyAPICall } from "~discord-bot/test/helpers";
 
 let client: SapphireClient;
 let guild: Guild;
@@ -20,38 +21,33 @@ beforeEach(async () => {
 
 describe("Guild Create Parity", () => {
   it("should sync a server on join", async () => {
-    mockTextChannel(client, guild);
-    mockForumChannel(client, guild);
+    const a = mockTextChannel(client, guild);
+    const b = mockForumChannel(client, guild);
     await emitEvent(client, Events.GuildCreate, guild);
-    const created_server = await prisma.server.findUnique({
-      where: { id: guild.id },
-      include: { channels: true },
-    });
+    await delay();
+    const created_server = await testOnlyAPICall((router) => router.servers.byId(guild.id));
+    const created_channels = await testOnlyAPICall((router) =>
+      router.channels.byIdMany([a.id, b.id])
+    );
     expect(created_server).toBeDefined();
     expect(created_server?.name).toBe(guild.name);
-    expect(created_server?.channels).toHaveLength(2);
+    expect(created_channels).toHaveLength(2);
   });
   it("should update an existing server on rejoin", async () => {
     await emitEvent(client, Events.GuildCreate, guild);
-    const created_server = await prisma.server.findUnique({
-      where: { id: guild.id },
-    });
+    const created_server = await testOnlyAPICall((router) => router.servers.byId(guild.id));
     expect(created_server).toBeDefined();
     expect(created_server?.name).toBe(guild.name);
     const new_name = "new name";
     guild.name = new_name;
     await emitEvent(client, Events.GuildCreate, guild);
-    const updated_server = await prisma.server.findUnique({
-      where: { id: guild.id },
-    });
+    const updated_server = await testOnlyAPICall((router) => router.servers.byId(guild.id));
     expect(updated_server).toBeDefined();
     expect(updated_server?.name).toBe(new_name);
   });
   it("should clear the kick status of a server on join", async () => {
     await emitEvent(client, Events.GuildCreate, guild);
-    const created_server = await prisma.server.findUnique({
-      where: { id: guild.id },
-    });
+    const created_server = await testOnlyAPICall((router) => router.servers.byId(guild.id));
     expect(created_server).toBeDefined();
     expect(created_server?.name).toBe(guild.name);
     expect(created_server?.kicked_time).toBeNull();
@@ -61,26 +57,20 @@ describe("Guild Create Parity", () => {
 describe("Guild Delete Parity", () => {
   it("should set the kick status of a server on delete", async () => {
     await emitEvent(client, Events.GuildCreate, guild);
-    const created_server = await prisma.server.findUnique({
-      where: { id: guild.id },
-    });
+    const created_server = await testOnlyAPICall((router) => router.servers.byId(guild.id));
     expect(created_server).toBeDefined();
     expect(created_server?.name).toBe(guild.name);
     expect(created_server?.kicked_time).toBeNull();
 
     await emitEvent(client, Events.GuildDelete, guild);
-    const deleted_server = await prisma.server.findUnique({
-      where: { id: guild.id },
-    });
+    const deleted_server = await testOnlyAPICall((router) => router.servers.byId(guild.id));
     expect(deleted_server).toBeDefined();
     expect(deleted_server?.name).toBe(guild.name);
     expect(deleted_server?.kicked_time?.getUTCDate()).toBeCloseTo(new Date().getUTCDate());
   });
   it("should create a server on delete if it doesn't exist", async () => {
     await emitEvent(client, Events.GuildDelete, guild);
-    const deleted_server = await prisma.server.findUnique({
-      where: { id: guild.id },
-    });
+    const deleted_server = await testOnlyAPICall((router) => router.servers.byId(guild.id));
     expect(deleted_server).toBeDefined();
     expect(deleted_server?.name).toBe(guild.name);
     expect(deleted_server?.kicked_time?.getUTCDate()).toBeCloseTo(new Date().getUTCDate());
@@ -90,17 +80,13 @@ describe("Guild Delete Parity", () => {
 describe("Guild Update Parity", () => {
   it("should update a server on update", async () => {
     await emitEvent(client, Events.GuildCreate, guild);
-    const created_server = await prisma.server.findUnique({
-      where: { id: guild.id },
-    });
+    const created_server = await testOnlyAPICall((router) => router.servers.byId(guild.id));
     expect(created_server).toBeDefined();
     expect(created_server?.name).toBe(guild.name);
 
     const new_guild = copyClass(guild, client, { name: "new name" });
     await emitEvent(client, Events.GuildUpdate, guild, new_guild);
-    const updated_server = await prisma.server.findUnique({
-      where: { id: guild.id },
-    });
+    const updated_server = await testOnlyAPICall((router) => router.servers.byId(guild.id));
     expect(updated_server).toBeDefined();
     expect(updated_server?.name).toBe("new name");
   });
@@ -108,9 +94,7 @@ describe("Guild Update Parity", () => {
     const new_name = "new name";
     const new_guild = copyClass(guild, client, { name: "new name" });
     await emitEvent(client, Events.GuildUpdate, guild, new_guild);
-    const updated_server = await prisma.server.findUnique({
-      where: { id: guild.id },
-    });
+    const updated_server = await testOnlyAPICall((router) => router.servers.byId(guild.id));
     expect(updated_server).toBeDefined();
     expect(updated_server?.name).toBe(new_name);
   });
