ecd3a32529a257c32d5202e4efb4d5532d785168
Rhys Sullivan <39114868+RhysSullivan@users.noreply.github.com>
2023-02-04 20:55:22 -0500

    all user server settings tests passing

--- packages/api/src/router/user-server-settings/user-server-settings.ts
+++ packages/api/src/router/user-server-settings/user-server-settings.ts
@@ -1,12 +1,13 @@
 import {
-  addFlagsToUserServerSettings,
+  createUserServerSettings,
+  findManyUserServerSettings,
+  findUserServerSettingsById,
   getDefaultUserServerSettings,
-  mergeUserServerSettingsFlags,
-  UserServerSettings,
+  mergeUserServerSettings,
+  updateUserServerSettings,
   z_user_server_settings_create,
   z_user_server_settings_create_with_deps,
   z_user_server_settings_find,
-  z_user_server_settings_mutable,
   z_user_server_settings_update,
 } from "@answeroverflow/db";
 import { z } from "zod";
@@ -24,119 +25,33 @@ import { assertIsUser } from "~api/utils/permissions";
 
 export const SERVER_NOT_SETUP_MESSAGE = "Server is not setup for Answer Overflow yet";
 
-async function transformUserServerSettings<T extends UserServerSettings>(
-  user_server_settings: Promise<T> | T
-) {
-  return addFlagsToUserServerSettings(await user_server_settings);
-}
-
-async function transformUserServerSettingsArray<T extends UserServerSettings>(
-  user_server_settings: Promise<T[]>
-) {
-  return (await user_server_settings).map(addFlagsToUserServerSettings);
-}
-
-function mergeUserServerSettings<T extends z.infer<typeof z_user_server_settings_mutable>>(
-  old: UserServerSettings,
-  updated: T
-) {
-  const { flags, ...update_data_without_flags } = updated;
-  return {
-    ...update_data_without_flags,
-    bitfield: flags ? mergeUserServerSettingsFlags(old.bitfield, flags) : undefined,
-  };
-}
-
-const user_server_settings_fetch_router = router({
+const user_server_settings_crud_router = router({
   byId: withDiscordAccountProcedure
     .input(z_user_server_settings_find)
     .query(async ({ input, ctx }) => {
-      return transformUserServerSettings(
-        protectedFetch({
-          permissions: () => assertIsUser(ctx, input.user_id),
-          fetch() {
-            return ctx.prisma.userServerSettings.findUnique({
-              where: {
-                user_id_server_id: input,
-              },
-            });
-          },
-          not_found_message: "User server settings not found",
-        })
-      );
+      return protectedFetch({
+        permissions: () => assertIsUser(ctx, input.user_id),
+        fetch: () => findUserServerSettingsById(input, ctx.prisma),
+        not_found_message: "User server settings not found",
+      });
     }),
-  byIdMany: withDiscordAccountProcedure
-    .input(z.array(z_user_server_settings_find))
-    .query(async ({ input, ctx }) => {
-      const user_ids = input.map((x) => x.user_id);
-      const server_ids = input.map((x) => x.server_id);
-      return transformUserServerSettingsArray(
-        protectedFetch({
-          permissions: () => input.map((user) => assertIsUser(ctx, user.user_id)),
-          fetch: () => {
-            return ctx.prisma.userServerSettings.findMany({
-              where: {
-                AND: {
-                  user_id: {
-                    in: user_ids,
-                  },
-                  server_id: {
-                    in: server_ids,
-                  },
-                },
-              },
-            });
-          },
-          not_found_message: "Could not find user server settings for users",
-        })
-      );
-    }),
-});
-
-const user_server_settings_mutation_router = router({
   create: withDiscordAccountProcedure
     .input(z_user_server_settings_create)
     .mutation(async ({ input, ctx }) => {
-      return transformUserServerSettings(
-        protectedMutation({
-          permissions: () => assertIsUser(ctx, input.user_id),
-          operation: () =>
-            ctx.prisma.userServerSettings.create({
-              data: mergeUserServerSettings(
-                getDefaultUserServerSettings({
-                  ...input,
-                }),
-                input
-              ),
-            }),
-        })
-      );
+      return protectedMutation({
+        permissions: () => assertIsUser(ctx, input.user_id),
+        operation: () => createUserServerSettings(input, ctx.prisma),
+      });
     }),
   update: withDiscordAccountProcedure
     .input(z_user_server_settings_update)
     .mutation(async ({ input, ctx }) => {
-      return transformUserServerSettings(
-        protectedMutationFetchFirst({
-          permissions: () => assertIsUser(ctx, input.user_id),
-          fetch() {
-            return user_server_settings_fetch_router.createCaller(ctx).byId({
-              user_id: input.user_id,
-              server_id: input.server_id,
-            });
-          },
-          not_found_message: "User server settings not found",
-          operation: (old) =>
-            ctx.prisma.userServerSettings.update({
-              where: {
-                user_id_server_id: {
-                  user_id: input.user_id,
-                  server_id: input.server_id,
-                },
-              },
-              data: mergeUserServerSettings(old, input),
-            }),
-        })
-      );
+      return protectedMutationFetchFirst({
+        permissions: () => assertIsUser(ctx, input.user_id),
+        not_found_message: "User server settings not found",
+        fetch: () => findUserServerSettingsById(input, ctx.prisma),
+        operation: (old) => updateUserServerSettings(input, ctx.prisma, old),
+      });
     }),
 });
 
@@ -144,44 +59,31 @@ const user_server_settings_with_deps_router = router({
   createWithDeps: withDiscordAccountProcedure
     .input(z_user_server_settings_create_with_deps)
     .mutation(async ({ input, ctx }) => {
-      return transformUserServerSettings(
-        protectedMutation({
-          permissions: () => assertIsUser(ctx, input.user.id),
-          async operation() {
-            // We do not create the server as a dependency as that action should only be handled by the server owner or Answer Overflow bot
-            try {
-              await serverRouter.createCaller(ctx).byId(input.server_id);
-            } catch (error) {
-              if (error instanceof TRPCError) {
-                if (error.code === "NOT_FOUND") {
-                  throw new TRPCError({
-                    code: "NOT_FOUND",
-                    message: SERVER_NOT_SETUP_MESSAGE,
-                  });
-                }
-              }
-            }
-
-            await discordAccountRouter.createCaller(ctx).upsert(input.user);
-            const merged_data = mergeUserServerSettings(
-              getDefaultUserServerSettings({
-                user_id: input.user.id,
-                ...input,
-              }),
-              {
-                user_id: input.user.id,
-                ...input,
-              }
-            );
-
-            // eslint-disable-next-line @typescript-eslint/no-unused-vars
-            const { user, ...data_without_user } = merged_data;
-            return ctx.prisma.userServerSettings.create({
-              data: data_without_user,
+      try {
+        await serverRouter.createCaller(ctx).byId(input.server_id);
+      } catch (error) {
+        if (error instanceof TRPCError) {
+          if (error.code === "NOT_FOUND") {
+            throw new TRPCError({
+              code: "NOT_FOUND",
+              message: SERVER_NOT_SETUP_MESSAGE,
             });
-          },
-        })
+          }
+        }
+      }
+
+      await discordAccountRouter.createCaller(ctx).upsert(input.user);
+      const merged_data = mergeUserServerSettings(
+        getDefaultUserServerSettings({
+          user_id: input.user.id,
+          ...input,
+        }),
+        {
+          user_id: input.user.id,
+          ...input,
+        }
       );
+      return user_server_settings_crud_router.createCaller(ctx).create(merged_data);
     }),
 });
 
@@ -191,12 +93,12 @@ const user_server_settings_upsert = router({
     .mutation(async ({ input, ctx }) => {
       return upsert(
         () =>
-          user_server_settings_fetch_router.createCaller(ctx).byId({
+          user_server_settings_crud_router.createCaller(ctx).byId({
             user_id: input.user_id,
             server_id: input.server_id,
           }),
-        () => user_server_settings_mutation_router.createCaller(ctx).create(input),
-        () => user_server_settings_mutation_router.createCaller(ctx).update(input)
+        () => user_server_settings_crud_router.createCaller(ctx).create(input),
+        () => user_server_settings_crud_router.createCaller(ctx).update(input)
       );
     }),
   upsertWithDeps: withDiscordAccountProcedure
@@ -204,13 +106,13 @@ const user_server_settings_upsert = router({
     .mutation(async ({ input, ctx }) => {
       return upsert(
         () =>
-          user_server_settings_fetch_router.createCaller(ctx).byId({
+          user_server_settings_crud_router.createCaller(ctx).byId({
             user_id: input.user.id,
             server_id: input.server_id,
           }),
         () => user_server_settings_with_deps_router.createCaller(ctx).createWithDeps(input),
         () =>
-          user_server_settings_mutation_router
+          user_server_settings_crud_router
             .createCaller(ctx)
             .update({ ...input, user_id: input.user.id })
       );
@@ -218,8 +120,7 @@ const user_server_settings_upsert = router({
 });
 
 export const userServerSettingsRouter = mergeRouters(
-  user_server_settings_fetch_router,
-  user_server_settings_mutation_router,
+  user_server_settings_crud_router,
   user_server_settings_with_deps_router,
   user_server_settings_upsert
 );
--- packages/db/src/user-server-settings.ts
+++ packages/db/src/user-server-settings.ts
@@ -1,5 +1,11 @@
+import type { z } from "zod";
+import { getDefaultUserServerSettings, PrismaClient, UserServerSettings } from "..";
 import { z_discord_account_upsert } from "./discord-account";
-import { z_user_server_settings } from "./zod-schemas";
+import {
+  addFlagsToUserServerSettings,
+  mergeUserServerSettingsFlags,
+  z_user_server_settings,
+} from "./zod-schemas";
 
 export const z_user_server_settings_required = z_user_server_settings.pick({
   user_id: true,
@@ -29,3 +35,96 @@ export const z_user_server_settings_create_with_deps = z_user_server_settings_cr
 export const z_user_server_settings_update = z_user_server_settings_mutable.merge(
   z_user_server_settings_find
 );
+
+export function mergeUserServerSettings<T extends z.infer<typeof z_user_server_settings_mutable>>(
+  old: UserServerSettings,
+  updated: T
+) {
+  const { flags, ...update_data_without_flags } = updated;
+  return {
+    ...update_data_without_flags,
+    bitfield: flags ? mergeUserServerSettingsFlags(old.bitfield, flags) : undefined,
+  };
+}
+
+interface UserServerSettingsFindById {
+  user_id: string;
+  server_id: string;
+}
+
+export async function findUserServerSettingsById(
+  where: UserServerSettingsFindById,
+  prisma: PrismaClient
+) {
+  const data = await prisma.userServerSettings.findUnique({
+    where: {
+      user_id_server_id: {
+        user_id: where.user_id,
+        server_id: where.server_id,
+      },
+    },
+  });
+  return data ? addFlagsToUserServerSettings(data) : null;
+}
+
+export async function findManyUserServerSettings(
+  where: UserServerSettingsFindById[],
+  prisma: PrismaClient
+) {
+  // TODO: Maybe just make it only selecting for 1 server at a time
+  const data = await prisma.userServerSettings.findMany({
+    where: {
+      AND: {
+        user_id: {
+          in: where.map((x) => x.user_id),
+        },
+        server_id: {
+          in: where.map((x) => x.server_id),
+        },
+      },
+    },
+  });
+  return data.map(addFlagsToUserServerSettings);
+}
+
+export async function createUserServerSettings(
+  data: z.infer<typeof z_user_server_settings_create>,
+  prisma: PrismaClient
+) {
+  const created = await prisma.userServerSettings.create({
+    data: mergeUserServerSettings(
+      getDefaultUserServerSettings({
+        ...data,
+      }),
+      data
+    ),
+  });
+  return addFlagsToUserServerSettings(created);
+}
+
+export async function updateUserServerSettings(
+  data: z.infer<typeof z_user_server_settings_update>,
+  prisma: PrismaClient,
+  existing: UserServerSettings | null
+) {
+  if (!existing)
+    existing = await findUserServerSettingsById(
+      {
+        user_id: data.user_id,
+        server_id: data.server_id,
+      },
+      prisma
+    );
+  if (!existing) throw new Error("UserServerSettings not found");
+
+  const updated = await prisma.userServerSettings.update({
+    where: {
+      user_id_server_id: {
+        user_id: data.user_id,
+        server_id: data.server_id,
+      },
+    },
+    data: mergeUserServerSettings(existing, data),
+  });
+  return addFlagsToUserServerSettings(updated);
+}
