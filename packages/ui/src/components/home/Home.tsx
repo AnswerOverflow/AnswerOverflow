df2467b24f4305930e75321db0db833cb36814e6
JoltCode <jcollischandler@gmail.com>
2023-02-05 13:46:08 +0000

    tweak(ui/pages): home landing section

--- packages/ui/src/components/home/Home.tsx
+++ packages/ui/src/components/home/Home.tsx
@@ -1,16 +1,25 @@
-// Disable prettier
+import { Button } from "../newComps/Button";
+import { Navbar } from "../newComps/Navbar";
+
 export const Home = () => {
   return (
     <div className="h-screen w-screen bg-ao-white">
-      <div className="mx-[4rem] flex h-full w-full flex-row">
+      <Navbar />
+      <div className="flex h-full w-full flex-row px-[4rem] transition-all 2xl:px-[6rem]">
         <div className="flex w-[40%] flex-col items-start justify-center gap-4">
-          <h1 className="font-header text-6xl font-bold text-ao-black">
+          <h1 className="font-header text-6xl font-bold leading-[114.5%] text-ao-black">
             Bringing your discord channels to google
           </h1>
           <h4 className="font-body text-xl text-ao-black/[.85]">
             Answer Overflow is an open source project designed to......................
           </h4>
+          <Button type={"solid"} color={"black"}>
+            <span className="text-xl">Get Started</span>
+          </Button>
         </div>
+
+        {/* Discord animation */}
+        <div></div>
       </div>
     </div>
   );
