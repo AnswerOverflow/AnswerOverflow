3f2b4a92f97bf1777b4bdc64375d6a90b1fbfe3b
JoltCode <joe@joecc.dev>
2023-02-18 18:44:01 +0000

    fix(ui/home): moving arrow causing offset

--- packages/ui/src/components/home/LandingAnimation/NewLandingAnimation.tsx
+++ packages/ui/src/components/home/LandingAnimation/NewLandingAnimation.tsx
@@ -21,37 +21,38 @@ export const NewLandingAnimation = ({
   const theme = useTheme();
 
   return (
-    <div className="flex flex-col gap-10 rounded-lg border-1 border-[#C5C5C5] bg-gradient-to-br-light-glass px-16 py-20 shadow-[0px_0px_98px_-19px_#D4EDFF0F] backdrop-blur-md dark:border-[#343434] dark:bg-gradient-to-br-dark-glass xl:py-10 2xl:py-16">
-      {/* Discord */}
-      <div className="rounded-md bg-[#36393F]" ref={discordPageRef}>
-        {/* Channel name */}
-        <div className="border-b-1 border-black/50 px-5 py-2">
-          <span className="text-white">{discordChannelName}</span>
+    <>
+      <div className="flex flex-col gap-10 rounded-lg border-1 border-[#C5C5C5] bg-gradient-to-br-light-glass py-20 pl-16 pr-8 shadow-[0px_0px_98px_-19px_#D4EDFF0F] backdrop-blur-md dark:border-[#343434] dark:bg-gradient-to-br-dark-glass xl:py-10 2xl:py-12">
+        {/* Discord */}
+        <div className="rounded-md bg-[#36393F]" ref={discordPageRef}>
+          {/* Channel name */}
+          <div className="border-b-1 border-black/50 px-5 py-2">
+            <span className="text-white">{discordChannelName}</span>
+          </div>
+
+          <div ref={discordPageRef}>
+            <Message {...questionMessage} showLinkIcon={false} darkMode={true} />
+            <Message
+              {...answerMessage}
+              messageBoxClassName="rounded-b-md"
+              showLinkIcon={false}
+              darkMode={true}
+            />
+          </div>
         </div>
 
-        <div ref={discordPageRef}>
-          <Message {...questionMessage} showLinkIcon={false} darkMode={true} />
-          <Message
-            {...answerMessage}
-            messageBoxClassName="rounded-b-md"
-            showLinkIcon={false}
-            darkMode={true}
-          />
-        </div>
+        {/* Google */}
+        <GooglePage
+          result={{
+            url: "https://www.answeroverflow.com > ...",
+            title: "How do I index my discord channels into google?",
+            // TODO: Add description, will require arrow offsets to be changed
+            description:
+              "How do I index my discord channels into google? How do I index my discord channels into google? How do I index my discord channels into google?",
+          }}
+          ref={googlePageRef}
+        />
       </div>
-
-      {/* Google */}
-      <GooglePage
-        result={{
-          url: "https://www.answeroverflow.com > ...",
-          title: "How do I index my discord channels into google?",
-          // TODO: Add description, will require arrow offsets to be changed
-          description:
-            "How do I index my discord channels into google? How do I index my discord channels into google? How do I index my discord channels into google?",
-        }}
-        ref={googlePageRef}
-      />
-
       <div className="arrow">
         <Xarrow
           start={discordPageRef}
@@ -85,6 +86,6 @@ export const NewLandingAnimation = ({
         _cpx1Offset={60}
         _cpx2Offset={60}
       /> */}
-    </div>
+    </>
   );
 };
