1ed16b9c96cf6280460a53981779ae719d54794f
JoltCode <joe@joecc.dev>
2023-02-20 09:07:35 +0000

    tweak(ui/stories): homepage a11y rules

--- packages/ui/src/components/home/Home.stories.tsx
+++ packages/ui/src/components/home/Home.stories.tsx
@@ -5,6 +5,20 @@ export default {
   component: Home,
   parameters: {
     layout: "fullscreen",
+    a11y: {
+      config: {
+        rules: [
+          {
+            id: "heading-order",
+            enabled: false,
+          },
+          {
+            id: "duplicate-id",
+            enabled: false,
+          },
+        ],
+      },
+    },
   },
 } as Meta<typeof Home>;
 
