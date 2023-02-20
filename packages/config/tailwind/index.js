cac8ff894c3adcbd88ac435249a4832763611776
JoltCode <joe@joecc.dev>
2023-02-20 11:27:16 +0000

    feat(ui): new homepage, navbar and button (#146)
    * add(ui): homepage, new button component
    
    Co-authored-by: Rhys Sullivan <39114868+RhysSullivan@users.noreply.github.com>

--- packages/config/tailwind/index.js
+++ packages/config/tailwind/index.js
@@ -15,8 +15,27 @@ module.exports = {
     extend: {
       fontFamily: {
         mono: ["var(--font-poppins)"],
+        discord: ["Noto Sans", "sans-serif"],
+        header: ["Montserrat", "sans-serif"],
+        body: ["Source Sans Pro", "sans-serif"],
+      },
+      colors: {
+        ao: {
+          white: "#FAFCFF",
+          black: "#070A0D",
+        },
+      },
+      borderWidth: {
+        1: "1px",
       },
       visibility: ["group-hover"],
+      // Custom linear gradient
+      backgroundImage: () => ({
+        "gradient-to-br-dark-glass":
+          "linear-gradient(145.98deg, rgba(31, 32, 34, 0.37) -3.49%, rgba(31, 32, 34, 0.37) -3.48%, rgba(31, 32, 34, 0.1591) 108.92%);",
+        "gradient-to-br-light-glass":
+          "linear-gradient(145.98deg, rgba(183, 190, 206, 0.37) -3.49%, rgba(183, 190, 206, 0.1591) 108.92%);",
+      }),
     },
   },
 
