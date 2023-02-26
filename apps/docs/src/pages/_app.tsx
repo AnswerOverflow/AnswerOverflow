e0fd3e76d9b501ea35768283901b6431711a7b95
Rhys Sullivan <39114868+RhysSullivan@users.noreply.github.com>
2023-02-26 17:43:04 -0500

    Init nextra

---
+++ apps/docs/src/pages/_app.tsx
@@ -0,0 +1,31 @@
+// src/pages/App.tsx
+import "../styles/globals.css";
+import "highlight.js/styles/github.css";
+import "highlight.js/styles/github-dark.css";
+import { SessionProvider } from "next-auth/react";
+import type { Session } from "next-auth";
+import type { AppType } from "next/app";
+import hljs from "highlight.js";
+
+import { NextTRPC, trpc } from "@answeroverflow/ui";
+import { Footer, Navbar } from "@answeroverflow/ui";
+import { useEffect } from "react";
+
+// eslint-disable-next-line @typescript-eslint/naming-convention
+const MyApp: AppType<{ session: Session | null }> = ({
+  Component,
+  pageProps: { session, ...pageProps },
+}) => {
+  useEffect(() => {
+    hljs.configure({
+      ignoreUnescapedHTML: true, // TODO: Revisit this, discord-markdown escapes the HTML so it should be safe
+    });
+    hljs.highlightAll();
+  }, []);
+  return (
+    <SessionProvider session={session}>
+      <Component {...pageProps} />
+    </SessionProvider>
+  );
+};
+export default (trpc as NextTRPC).withTRPC(MyApp);
