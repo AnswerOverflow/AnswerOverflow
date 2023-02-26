f84f5445130ddf1e671d3ac989afb8f2127c32d6
Rhys Sullivan <39114868+RhysSullivan@users.noreply.github.com>
2023-02-25 23:10:57 -0500

    add redis package

---
+++ packages/redis/index.ts
@@ -0,0 +1,21 @@
+export * from "@prisma/client";
+export { prisma } from "./src/prisma";
+export * from "./src/bitfield";
+export * from "./src/zod-schemas";
+export * from "./src/default";
+
+declare global {
+  namespace NodeJS {
+    interface ProcessEnv {
+      /* Prisma */
+      // eslint-disable-next-line @typescript-eslint/naming-convention
+      VITE_DATABASE_URL: string;
+      // eslint-disable-next-line @typescript-eslint/naming-convention
+      DATABASE_URL: string;
+
+      // common
+      // eslint-disable-next-line @typescript-eslint/naming-convention
+      readonly NODE_ENV: "development" | "production" | "test";
+    }
+  }
+}
