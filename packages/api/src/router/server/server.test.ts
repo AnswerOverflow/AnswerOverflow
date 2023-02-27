5ed962ef644c60551f4cac21ee6ff3b9dbca28de
Rhys Sullivan <39114868+RhysSullivan@users.noreply.github.com>
2023-02-26 19:44:03 -0500

    Remove useless procedures

--- packages/api/src/router/server/server.test.ts
+++ packages/api/src/router/server/server.test.ts
@@ -2,52 +2,12 @@ import { mockServer } from "@answeroverflow/db-mock";
 import { createServer, Server } from "@answeroverflow/db";
 import { pickPublicServerData } from "~api/test/public-data";
 import {
-  testAllSourceAndPermissionVariantsThatThrowErrors,
   mockAccountWithServersCallerCtx,
   testAllPublicAndPrivateDataVariants,
 } from "~api/test/utils";
-import { MISSING_PERMISSIONS_TO_EDIT_SERVER_MESSAGE } from "~api/utils/permissions";
 import { serverRouter } from "./server";
 
 describe("Server Operations", () => {
-  describe("Server Create", () => {
-    it("should test all permission and caller variants to ensure only calls from the Discord bot with Manage Guild & Administrator can succeed", async () => {
-      await testAllSourceAndPermissionVariantsThatThrowErrors({
-        sourcesThatShouldWork: ["discord-bot"],
-        permissionsThatShouldWork: ["ManageGuild", "Administrator"],
-        permissionFailureMessage: MISSING_PERMISSIONS_TO_EDIT_SERVER_MESSAGE,
-        async operation({ permission, source }) {
-          const server = mockServer();
-          const account = await mockAccountWithServersCallerCtx(server, source, permission);
-          const router = serverRouter.createCaller(account.ctx);
-          await router.create(server);
-        },
-      });
-    });
-  });
-  describe("Server Update", () => {
-    it("should test all permission and caller variants to ensure only calls from the Discord bot with Manage Guild & Administrator can succeed", async () => {
-      return await testAllSourceAndPermissionVariantsThatThrowErrors({
-        async operation({ source, permission }) {
-          const server = mockServer();
-          await createServer(server);
-          const account = await mockAccountWithServersCallerCtx(server, source, permission);
-          const router = serverRouter.createCaller(account.ctx);
-          await router.update({
-            id: server.id,
-            name: "new name",
-            flags: {
-              readTheRulesConsentEnabled: true,
-            },
-          });
-        },
-        sourcesThatShouldWork: ["discord-bot"],
-        permissionsThatShouldWork: ["ManageGuild", "Administrator"],
-        permissionFailureMessage: MISSING_PERMISSIONS_TO_EDIT_SERVER_MESSAGE,
-      });
-    });
-  });
-
   describe("Server Fetch", () => {
     let server2: Server;
     beforeEach(async () => {
@@ -71,25 +31,5 @@ describe("Server Operations", () => {
         permissionsThatShouldWork: ["ManageGuild", "Administrator"],
       });
     });
-  });
-
-  describe("Server Upsert", () => {
-    it("should test all server create upsert variants", async () => {
-      await testAllSourceAndPermissionVariantsThatThrowErrors({
-        sourcesThatShouldWork: ["discord-bot"],
-        permissionsThatShouldWork: ["ManageGuild", "Administrator"],
-        permissionFailureMessage: MISSING_PERMISSIONS_TO_EDIT_SERVER_MESSAGE,
-        async operation({ permission, source }) {
-          const server = mockServer();
-          const account = await mockAccountWithServersCallerCtx(server, source, permission);
-          const router = serverRouter.createCaller(account.ctx);
-          await router.upsert(server);
-        },
-      });
-    });
-  });
-
-  describe("Server Delete", () => {
-    test.todo("should test all server delete variants");
   });
 });
--- packages/api/src/router/server/server.ts
+++ packages/api/src/router/server/server.ts
@@ -1,37 +1,10 @@
-import {
-  createServer,
-  zServerPublic,
-  zServerCreate,
-  zServerUpdate,
-  zServerUpsert,
-  updateServer,
-  findServerById,
-  upsert,
-} from "@answeroverflow/db";
+import { zServerPublic, findServerById } from "@answeroverflow/db";
 import { z } from "zod";
-import { MergeRouters, router, publicProcedure, withUserServersProcedure } from "~api/router/trpc";
-import { assertCanEditServer, assertCanEditServerBotOnly } from "~api/utils/permissions";
-import {
-  protectedFetchWithPublicData,
-  protectedMutation,
-  protectedMutationFetchFirst,
-} from "~api/utils/protected-procedures";
+import { router, withUserServersProcedure } from "~api/router/trpc";
+import { assertCanEditServer } from "~api/utils/permissions";
+import { protectedFetchWithPublicData } from "~api/utils/protected-procedures";
 
-const serverCrudRouter = router({
-  create: publicProcedure.input(zServerCreate).mutation(({ ctx, input }) => {
-    return protectedMutation({
-      operation: () => createServer(input),
-      permissions: () => assertCanEditServerBotOnly(ctx, input.id),
-    });
-  }),
-  update: publicProcedure.input(zServerUpdate).mutation(({ ctx, input }) => {
-    return protectedMutationFetchFirst({
-      fetch: () => findServerById(input.id),
-      operation: (old) => updateServer(input, old),
-      notFoundMessage: "Server not found",
-      permissions: () => assertCanEditServerBotOnly(ctx, input.id),
-    });
-  }),
+export const serverRouter = router({
   byId: withUserServersProcedure.input(z.string()).query(async ({ ctx, input }) => {
     return protectedFetchWithPublicData({
       fetch: () => findServerById(input),
@@ -41,15 +14,3 @@ const serverCrudRouter = router({
     });
   }),
 });
-
-const serverUpsertRouter = router({
-  upsert: publicProcedure.input(zServerUpsert).mutation(async ({ ctx, input }) => {
-    return upsert({
-      find: () => findServerById(input.id),
-      create: () => serverCrudRouter.createCaller(ctx).create(input),
-      update: () => serverCrudRouter.createCaller(ctx).update(input),
-    });
-  }),
-});
-
-export const serverRouter = MergeRouters(serverUpsertRouter, serverCrudRouter);
