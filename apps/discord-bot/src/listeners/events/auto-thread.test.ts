5ecb38d97051970c6886fe0673e1f8a843c24024
JoltCode <joe@joecc.dev>
2023-02-28 19:01:09 +0000

    tweak(bot/autothread): tests

--- apps/discord-bot/src/listeners/events/auto-thread.test.ts
+++ apps/discord-bot/src/listeners/events/auto-thread.test.ts
@@ -8,6 +8,8 @@ import {
 import { setupAnswerOverflowBot } from "~discord-bot/test/sapphire-mock";
 import { randomSnowflake } from "@answeroverflow/discordjs-utils";
 import { mockPublicThread } from "@answeroverflow/discordjs-mock";
+import { createChannel, createServer } from "@answeroverflow/db";
+import { toAOServer, toAOChannel } from "~discord-bot/utils/conversions";
 
 let client: Client;
 let textChannel: TextChannel;
@@ -17,13 +19,40 @@ beforeEach(async () => {
   textChannel = mockTextChannel(client);
 });
 
+const mockMessageCorrect = async () => {
+  // Channel with flag enabled
+  await createServer(toAOServer(textChannel.guild));
+  await createChannel({
+    ...toAOChannel(textChannel),
+    flags: { sendMarkSolutionInstructionsInNewThreads: true },
+  });
+  const author = mockGuildMember({ client });
+  const message = mockMessage({
+    client,
+    channel: textChannel,
+    author: author.user,
+    override: {
+      content: "Message",
+    },
+  });
+
+  await emitEvent(client, Events.MessageCreate, message);
+
+  expect(message.startThread).toHaveBeenCalled();
+};
+
 describe("Auto thread", () => {
   it("should not create a thread on a channel thread channel", async () => {
+    await mockMessageCorrect();
+
     const channel = mockPublicThread({ client });
     const author = mockGuildMember({ client });
     const message = mockMessage({
       client,
-      channel,
+      channel: channel,
+      override: {
+        content: "Hey",
+      },
       author: author.user,
     });
 
@@ -32,6 +61,7 @@ describe("Auto thread", () => {
     expect(message.startThread).not.toHaveBeenCalled();
   });
   it("should not create a thread if the author is a bot", async () => {
+    await mockMessageCorrect();
     const author = mockGuildMember({
       client,
       data: {
@@ -45,6 +75,9 @@ describe("Auto thread", () => {
     const message = mockMessage({
       client,
       channel: textChannel,
+      override: {
+        content: "Hey",
+      },
       author: author.user,
     });
 
@@ -54,6 +87,8 @@ describe("Auto thread", () => {
     expect(message.startThread).not.toHaveBeenCalled();
   });
   it("should not create if the author is the system", async () => {
+    await mockMessageCorrect();
+
     const author = mockGuildMember({
       client,
       data: {
@@ -67,6 +102,9 @@ describe("Auto thread", () => {
     const message = mockMessage({
       client,
       channel: textChannel,
+      override: {
+        content: "Hey",
+      },
       author: author.user,
     });
 
@@ -75,22 +113,33 @@ describe("Auto thread", () => {
     // assert
     expect(message.startThread).not.toHaveBeenCalled();
   });
-  it("should not create a thread if it does not have auto thread enabled", () => {});
+  it("should not create a thread if it does not have auto thread enabled", async () => {
+    await mockMessageCorrect();
 
-  it("should create a thread", async () => {
-    // Channel with flag enabled
     const channel = mockTextChannel(client);
+
+    await createServer(toAOServer(channel.guild));
+    await createChannel({
+      ...toAOChannel(channel),
+      flags: { sendMarkSolutionInstructionsInNewThreads: false },
+    });
     const author = mockGuildMember({ client });
+
     const message = mockMessage({
       client,
       channel: channel,
+      override: {
+        content: "Hey",
+      },
       author: author.user,
     });
 
-    // Mock channel flag from db
-
     await emitEvent(client, Events.MessageCreate, message);
 
-    expect(message.startThread).toHaveBeenCalled();
+    expect(message.startThread).not.toHaveBeenCalled();
+  });
+
+  it("should create a thread", async () => {
+    await mockMessageCorrect();
   });
 });
