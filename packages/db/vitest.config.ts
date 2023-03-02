20b14ec49e7eeb49c57b8a5ea70e22c9b1bf509d
Rhys Sullivan <39114868+RhysSullivan@users.noreply.github.com>
2023-03-02 17:21:12 -0500

    Update vitest.config.ts

--- packages/db/vitest.config.ts
+++ packages/db/vitest.config.ts
@@ -1,7 +1,5 @@
 // eslint-disable-next-line no-restricted-imports
 import { createVitestConfig } from "../../scripts/vitest.config";
-// eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
-require("dotenv").config();
 
 export default createVitestConfig({
   envDir: "../../",
