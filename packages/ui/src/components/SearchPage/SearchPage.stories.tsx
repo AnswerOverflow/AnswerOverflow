857563c33a8c75bc4f838febf66e1e6cc8483306
JoltCode <joe@joecc.dev>
2023-03-05 18:57:18 +0000

    add(ui/pages): search result page

---
+++ packages/ui/src/components/SearchPage/SearchPage.stories.tsx
@@ -0,0 +1,28 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import { SearchPage } from "./SearchPage";
+
+const meta = {
+  component: SearchPage,
+  title: "pages/SearchPage",
+  parameters: {
+    layout: "fullscreen",
+    a11y: {
+      config: {
+        rules: [
+          {
+            id: "heading-order",
+            enabled: false,
+          },
+          {
+            id: "duplicate-id",
+            enabled: false,
+          },
+        ],
+      },
+    },
+  },
+} as Meta<typeof SearchPage>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+export const Searchpage: Story = {};
---
+++ packages/ui/src/components/SearchPage/SearchPage.tsx
@@ -0,0 +1,19 @@
+import { useState } from "react";
+import { Input } from "../primitives/Input";
+import { Navbar } from "../primitives/Navbar";
+
+export const SearchPage = () => {
+  const [searchInput, setSearchInput] = useState<string>("");
+
+  return (
+    <div className="min-h-screen w-full bg-ao-black">
+      <Navbar />
+      <div className="w-full px-[4rem] 2xl:px-[6rem]">
+        <h1 className="py-4 font-header text-3xl text-white xl:text-5xl">Search</h1>
+        <Input onChange={setSearchInput} type="buttonInput" fill placeholder="Search">
+          Search
+        </Input>
+      </div>
+    </div>
+  );
+};
