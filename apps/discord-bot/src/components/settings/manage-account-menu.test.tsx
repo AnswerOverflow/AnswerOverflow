ee04b3981f5b3f2c4663ea2ee853bd61be490f5f
Rhys Sullivan <39114868+RhysSullivan@users.noreply.github.com>
2023-03-06 04:39:39 -0500

    working manage account menu

--- apps/discord-bot/src/components/settings/manage-account-menu.test.tsx
+++ apps/discord-bot/src/components/settings/manage-account-menu.test.tsx
@@ -5,13 +5,14 @@ import {
   createServer,
   createUserServerSettings,
   deleteDiscordAccount,
+  findIgnoredDiscordAccountById,
+  findUserServerSettingsById,
   getDefaultUserServerSettingsWithFlags,
   UserServerSettingsWithFlags,
 } from "@answeroverflow/db";
 import { mockUserServerSettingsWithFlags } from "@answeroverflow/db-mock";
-import type { ReacordTester } from "@answeroverflow/discordjs-react";
 import type { Guild, TextChannel } from "discord.js";
-import { mockReacord, setupAnswerOverflowBot } from "~discord-bot/test/sapphire-mock";
+import { setupAnswerOverflowBot } from "~discord-bot/test/sapphire-mock";
 import {
   createGuildMemberVariants,
   GuildMemberVariants,
@@ -28,14 +29,12 @@ import {
   REVOKE_CONSENT_LABEL,
   STOP_IGNORING_ACCOUNT_LABEL,
 } from "@answeroverflow/constants";
-let reacord: ReacordTester;
 let textChannel: TextChannel;
 let guild: Guild;
 let members: GuildMemberVariants;
 let defaultSettings: UserServerSettingsWithFlags;
 beforeEach(async () => {
   const client = await setupAnswerOverflowBot();
-  reacord = mockReacord();
   guild = mockGuild(client);
   members = await createGuildMemberVariants(client, guild);
   textChannel = mockTextChannel(client, guild);
@@ -49,18 +48,24 @@ beforeEach(async () => {
 describe("Manage Account Menu", () => {
   describe("Toggle Consent Button", () => {
     it("should enable consent", async () => {
-      const message = await reply(
-        reacord,
-        <ManageAccountMenu initialSettings={defaultSettings} initialIsGloballyIgnored={false} />
-      );
+      const message = await reply({
+        channel: textChannel,
+        content: (
+          <ManageAccountMenu initialSettings={defaultSettings} initialIsGloballyIgnored={false} />
+        ),
+        member: members.guildMemberOwner,
+      });
       await toggleButtonTest({
-        clicker: members.guildMemberOwner,
+        clicker: members.guildMemberOwner.user,
         preClickLabel: GRANT_CONSENT_LABEL,
         postClickLabel: REVOKE_CONSENT_LABEL,
         message: message,
-        reacord,
-        channel: textChannel,
       });
+      const updated = await findUserServerSettingsById({
+        serverId: guild.id,
+        userId: members.guildMemberOwner.id,
+      });
+      expect(updated?.flags.canPubliclyDisplayMessages).toBeTruthy();
     });
     it("should disable consent", async () => {
       await createDiscordAccount(toAODiscordAccount(members.guildMemberOwner.user));
@@ -74,18 +79,24 @@ describe("Manage Account Menu", () => {
         })
       );
 
-      const message = await reply(
-        reacord,
-        <ManageAccountMenu initialSettings={initialSettings} initialIsGloballyIgnored={false} />
-      );
+      const message = await reply({
+        channel: textChannel,
+        content: (
+          <ManageAccountMenu initialSettings={initialSettings} initialIsGloballyIgnored={false} />
+        ),
+        member: members.guildMemberOwner,
+      });
       await toggleButtonTest({
-        clicker: members.guildMemberOwner,
+        clicker: members.guildMemberOwner.user,
         preClickLabel: REVOKE_CONSENT_LABEL,
         postClickLabel: GRANT_CONSENT_LABEL,
         message: message,
-        reacord,
-        channel: textChannel,
       });
+      const updated = await findUserServerSettingsById({
+        serverId: guild.id,
+        userId: members.guildMemberOwner.id,
+      });
+      expect(updated?.flags.canPubliclyDisplayMessages).toBeFalsy();
     });
   });
   describe("Toggle Indexing Of User Messages Button", () => {
@@ -100,74 +111,85 @@ describe("Manage Account Menu", () => {
           },
         })
       );
-      const message = await reply(
-        reacord,
-        <ManageAccountMenu initialSettings={initialSettings} initialIsGloballyIgnored={false} />
-      );
+      const message = await reply({
+        channel: textChannel,
+        content: (
+          <ManageAccountMenu initialSettings={initialSettings} initialIsGloballyIgnored={false} />
+        ),
+        member: members.guildMemberOwner,
+      });
       await toggleButtonTest({
-        clicker: members.guildMemberOwner,
+        clicker: members.guildMemberOwner.user,
         preClickLabel: ENABLE_INDEXING_LABEL,
         postClickLabel: DISABLE_INDEXING_LABEL,
         message: message,
-        reacord,
-        channel: textChannel,
       });
-      const consentButton = message.findButtonByLabel(GRANT_CONSENT_LABEL, reacord);
+      const consentButton = message.findButtonByLabel(GRANT_CONSENT_LABEL);
       expect(consentButton?.disabled).toBeFalsy();
+      const updated = await findUserServerSettingsById({
+        serverId: guild.id,
+        userId: members.guildMemberOwner.id,
+      });
+      expect(updated?.flags.messageIndexingDisabled).toBeFalsy();
     });
     it("should disable indexing of user messages", async () => {
-      const message = await reply(
-        reacord,
-        <ManageAccountMenu initialSettings={defaultSettings} initialIsGloballyIgnored={false} />
-      );
+      const message = await reply({
+        channel: textChannel,
+        content: (
+          <ManageAccountMenu initialSettings={defaultSettings} initialIsGloballyIgnored={false} />
+        ),
+        member: members.guildMemberOwner,
+      });
       await toggleButtonTest({
-        clicker: members.guildMemberOwner,
+        clicker: members.guildMemberOwner.user,
         preClickLabel: DISABLE_INDEXING_LABEL,
         postClickLabel: ENABLE_INDEXING_LABEL,
         message: message,
-        reacord,
-        channel: textChannel,
       });
-      const consentButton = message.findButtonByLabel(GRANT_CONSENT_LABEL, reacord);
+      const consentButton = message.findButtonByLabel(GRANT_CONSENT_LABEL);
       expect(consentButton?.disabled).toBeTruthy();
+      const updated = await findUserServerSettingsById({
+        serverId: guild.id,
+        userId: members.guildMemberOwner.id,
+      });
+      expect(updated?.flags.messageIndexingDisabled).toBeTruthy();
     });
   });
   describe("Toggle Globally Ignored Button", () => {
     it("should enable globally ignored", async () => {
-      const message = await reply(
-        reacord,
-        <ManageAccountMenu initialSettings={defaultSettings} initialIsGloballyIgnored={false} />
-      );
+      const message = await reply({
+        channel: textChannel,
+        content: (
+          <ManageAccountMenu initialSettings={defaultSettings} initialIsGloballyIgnored={false} />
+        ),
+        member: members.guildMemberOwner,
+      });
       await toggleButtonTest({
-        clicker: members.guildMemberOwner,
         preClickLabel: GLOBALLY_IGNORE_ACCOUNT_LABEL,
         postClickLabel: STOP_IGNORING_ACCOUNT_LABEL,
         message: message,
-        reacord,
-        channel: textChannel,
+        clicker: members.guildMemberOwner.user,
       });
+
+      const found = await findIgnoredDiscordAccountById(members.guildMemberOwner.id);
+      expect(found!.id).toBe(members.guildMemberOwner.id);
     });
     it("should disable globally ignored", async () => {
       await deleteDiscordAccount(toAODiscordAccount(members.guildMemberOwner.user).id);
 
-      const message = await reply(
-        reacord,
-        <ManageAccountMenu
-          initialSettings={getDefaultUserServerSettingsWithFlags({
-            serverId: guild.id,
-            userId: members.guildMemberOwner.id,
-          })}
-          initialIsGloballyIgnored={true}
-        />
-      );
+      const message = await reply({
+        channel: textChannel,
+        content: (
+          <ManageAccountMenu initialSettings={defaultSettings} initialIsGloballyIgnored={true} />
+        ),
+        member: members.guildMemberOwner,
+      });
 
       await toggleButtonTest({
-        clicker: members.guildMemberOwner,
+        clicker: members.guildMemberOwner.user,
         preClickLabel: STOP_IGNORING_ACCOUNT_LABEL,
         postClickLabel: GLOBALLY_IGNORE_ACCOUNT_LABEL,
         message: message,
-        reacord,
-        channel: textChannel,
       });
     });
   });
