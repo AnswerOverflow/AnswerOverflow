2d5fce234bf1a1f3f4c9ba56690018a4e1b3f6f7
Rhys Sullivan <39114868+RhysSullivan@users.noreply.github.com>
2023-03-10 18:24:35 -0500

    start of mark solution

--- apps/docs/src/components/links.tsx
+++ apps/docs/src/components/links.tsx
@@ -1,18 +1,18 @@
 import Link from "next/link";
 import { DISCORD_LINK } from "@answeroverflow/constants";
-
+import { Callout } from "nextra-theme-docs";
 export const AnswerOverflowServerInviteLink = () => (
   <Link
     // eslint-disable-next-line tailwindcss/no-custom-classname
     className="nx-text-primary-600 nx-underline nx-decoration-from-font [text-underline-position:from-font]"
     href={DISCORD_LINK}
   >
-    Answer Overflow
+    Answer Overflow Discord
   </Link>
 );
 
 export const TryLiveDemo = () => (
-  <>
-    Join the <AnswerOverflowServerInviteLink /> Discord server to try this with 0 setup.
-  </>
+  <Callout type="info">
+    Join the <AnswerOverflowServerInviteLink /> server to try this with 0 setup.
+  </Callout>
 );
