571348cc936ade5973eeb6106b7e102d5d4645f8
Rhys Sullivan <39114868+RhysSullivan@users.noreply.github.com>
2023-03-11 00:32:47 -0500

    styling of video

---
+++ apps/docs/src/utils/styling.ts
@@ -0,0 +1,11 @@
+import type { CSSProperties } from "react";
+
+export const ytVideoStyle: CSSProperties = {
+  width: "100%",
+  height: "100%",
+  maxWidth: "100%",
+  maxHeight: "100%",
+  marginTop: "20px",
+  margin: "0 auto",
+  display: "block",
+};
