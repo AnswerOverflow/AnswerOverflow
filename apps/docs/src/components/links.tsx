f8b0188c86d1bf65ca3f023a5d9474ef7cdcf1c5
Rhys Sullivan <39114868+RhysSullivan@users.noreply.github.com>
2023-03-11 03:36:46 -0500

    docs v1

--- apps/docs/src/components/links.tsx
+++ apps/docs/src/components/links.tsx
@@ -1,6 +1,16 @@
 import Link from "next/link";
 import { DISCORD_LINK } from "@answeroverflow/constants";
 import { Callout } from "nextra-theme-docs";
+export const NextraStyledLink = ({ text, href }: { text: string; href: string }) => (
+  <Link
+    // eslint-disable-next-line tailwindcss/no-custom-classname
+    className="nx-text-primary-600 nx-underline nx-decoration-from-font [text-underline-position:from-font]"
+    href={href}
+  >
+    {text}
+  </Link>
+);
+
 export const AnswerOverflowServerInviteLink = () => (
   <Link
     // eslint-disable-next-line tailwindcss/no-custom-classname
