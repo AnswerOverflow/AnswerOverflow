a7338e06b3c193126a3fb3b5848d9e3aa7e89f22
Rhys Sullivan <39114868+RhysSullivan@users.noreply.github.com>
2023-03-11 23:51:15 -0500

    cleanup

--- packages/config/prettierrc.js
+++
@@ -1,5 +0,0 @@
-/** @type {import("prettier").Config} */
-// eslint-disable-next-line no-undef, @typescript-eslint/no-unsafe-assignment
-module.exports = {
-	...require("@answeroverflow/prettier-config"),
-};
