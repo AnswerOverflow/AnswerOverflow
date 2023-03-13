e273d837756911d2f92f256a9ade3448f01f478e
Rhys Sullivan <39114868+RhysSullivan@users.noreply.github.com>
2023-03-13 16:49:28 -0400

    feat (bot): sync embeds, reactions, components & more! (#267)

--- apps/discord-bot/src/utils/conversions.ts
+++ apps/discord-bot/src/utils/conversions.ts
@@ -1,5 +1,7 @@
-import type {
+import {
 	AnyThreadChannel,
+	ButtonStyle,
+	ComponentType,
 	Guild,
 	GuildBasedChannel,
 	GuildChannel,
@@ -44,6 +46,95 @@ export function toDiscordAPIServer(
 	};
 }
 
+function toAOComponent(
+	component: Message['components'][number]['components'][number],
+): AOMessage['components'][number]['components'][number] {
+	if (component.type === ComponentType.Button) {
+		if (component.style === ButtonStyle.Link) {
+			return {
+				type: component.type,
+				label: component.label ?? undefined,
+				url: component.url ?? '',
+				disabled: component.disabled ?? false,
+				style: component.style,
+				emoji: component.emoji ?? undefined,
+			};
+		} else {
+			return {
+				type: component.type,
+				label: component.label ?? undefined,
+				customId: component.customId ?? '',
+				disabled: component.disabled ?? false,
+				style: component.style,
+				emoji: component.emoji ?? undefined,
+			};
+		}
+	}
+	if (component.type === ComponentType.StringSelect) {
+		return {
+			type: component.type,
+			customId: component.customId ?? '',
+			disabled: component.disabled ?? false,
+			options: component.options ?? [],
+			placeholder: component.placeholder ?? undefined,
+			minValues: component.minValues ?? 1,
+			maxValues: component.maxValues ?? 1,
+		};
+	}
+	if (component.type === ComponentType.RoleSelect) {
+		return {
+			type: component.type,
+			customId: component.customId ?? '',
+			disabled: component.disabled ?? false,
+			placeholder: component.placeholder ?? undefined,
+			minValues: component.minValues ?? 1,
+			maxValues: component.maxValues ?? 1,
+		};
+	}
+	if (component.type === ComponentType.UserSelect) {
+		return {
+			type: component.type,
+			customId: component.customId ?? '',
+			disabled: component.disabled ?? false,
+			placeholder: component.placeholder ?? undefined,
+			minValues: component.minValues ?? 1,
+			maxValues: component.maxValues ?? 1,
+		};
+	}
+	if (component.type === ComponentType.ChannelSelect) {
+		return {
+			type: component.type,
+			customId: component.customId ?? '',
+			disabled: component.disabled ?? false,
+			placeholder: component.placeholder ?? undefined,
+			minValues: component.minValues ?? 1,
+			maxValues: component.maxValues ?? 1,
+		};
+	}
+	if (component.type === ComponentType.MentionableSelect) {
+		return {
+			type: component.type,
+			customId: component.customId ?? '',
+			disabled: component.disabled ?? false,
+			placeholder: component.placeholder ?? undefined,
+			minValues: component.minValues ?? 1,
+			maxValues: component.maxValues ?? 1,
+		};
+	}
+	throw new Error('Unknown component type');
+}
+
+// ActionRow<MessageActionRowComponent>[]
+function toAOActionRow(
+	components: Message['components'],
+): AOMessage['components'] {
+	return components.map((row) => ({
+		type: row.type,
+		components: row.components.map(toAOComponent),
+	}));
+}
+
+// top 10 ugliest functions in this codebase
 export function toAOMessage(message: Message): AOMessage {
 	if (!message.guild) throw new Error('Message is not in a guild');
 
@@ -54,21 +145,104 @@ export function toAOMessage(message: Message): AOMessage {
 		parentChannelId: message.channel.isThread()
 			? message.channel.parentId
 			: null,
-		images: message.attachments.map((attachment) => {
+		attachments: message.attachments.map((attachment) => {
 			return {
+				id: attachment.id,
 				url: attachment.url,
-				width: attachment.width,
+				proxyUrl: attachment.proxyURL,
+				filename: attachment.name ?? '',
+				size: attachment.size,
 				height: attachment.height,
-				description: attachment.description,
+				width: attachment.width,
+				contentType: attachment.contentType ?? undefined,
+				description: attachment.description ?? '',
+				ephemeral: attachment.ephemeral ?? false,
 			};
 		}),
+		applicationId: message.applicationId,
+		flags: message.flags.bitfield,
+		nonce: message.nonce ? message.nonce.toString() : null,
+		tts: message.tts,
+		reactions: message.reactions.cache.map((reaction) => ({
+			emojiId: reaction.emoji.id,
+			emojiName: reaction.emoji.name,
+			reactorIds: reaction.users.cache.map((user) => user.id),
+		})),
+		components: toAOActionRow(message.components),
+		embeds: message.embeds.map((embed) => ({
+			title: embed.title ?? undefined,
+			description: embed.description ?? undefined,
+			url: embed.url ?? undefined,
+			color: embed.color ?? undefined,
+			type: undefined,
+			timestamp: embed.timestamp ?? undefined,
+			footer: embed.footer
+				? {
+						text: embed.footer.text,
+						iconUrl: embed.footer.iconURL ?? undefined,
+						proxyIconUrl: embed.footer.proxyIconURL ?? undefined,
+				  }
+				: undefined,
+			image: embed.image
+				? {
+						url: embed.image.url,
+						proxyUrl: embed.image.proxyURL ?? undefined,
+						height: embed.image.height ?? undefined,
+						width: embed.image.width ?? undefined,
+				  }
+				: undefined,
+			video: embed.video
+				? {
+						height: embed.video.height ?? undefined,
+						width: embed.video.width ?? undefined,
+						url: embed.video.url,
+						proxyUrl: embed.video.proxyURL ?? undefined,
+				  }
+				: undefined,
+			provider: embed.provider
+				? {
+						name: embed.provider.name ?? undefined,
+						url: embed.provider.url ?? undefined,
+				  }
+				: undefined,
+			thumbnail: embed.thumbnail
+				? {
+						url: embed.thumbnail.url,
+						proxyUrl: embed.thumbnail.proxyURL ?? undefined,
+						height: embed.thumbnail.height ?? undefined,
+						width: embed.thumbnail.width ?? undefined,
+				  }
+				: undefined,
+			author: embed.author
+				? {
+						name: embed.author.name ?? undefined,
+						url: embed.author.url ?? undefined,
+						iconUrl: embed.author.iconURL ?? undefined,
+						proxyIconUrl: embed.author.proxyIconURL ?? undefined,
+				  }
+				: undefined,
+			fields: embed.fields.map((field) => ({
+				name: field.name,
+				value: field.value,
+				inline: field.inline ?? false,
+			})),
+		})),
+		interactionId: message.interaction?.id ?? null,
+		mentionChannels: message.mentions.channels.map((channel) => channel.id),
+		mentionEveryone: message.mentions.everyone,
+		mentionRoles: message.mentions.roles.map((role) => role.id),
+		mentions: message.mentions.users.map((user) => user.id),
+		pinned: message.pinned,
+		stickerIds: message.stickers.map((sticker) => sticker.id),
+		type: message.type,
+		webhookId: message.webhookId,
 		messageReference: message.reference
 			? toAOMessageReference(message.reference)
 			: null,
 		authorId: message.author.id,
 		serverId: message.guild?.id,
 		solutionIds: [],
-		childThread: message.thread?.id ?? null,
+		childThreadId: message.thread?.id ?? null,
 	};
 	return convertedMessage;
 }
