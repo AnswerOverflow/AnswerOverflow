602fa1d8290ef469e3785bb83c1e36ffc5aa4968
Rhys Sullivan <39114868+RhysSullivan@users.noreply.github.com>
2023-03-14 04:11:12 -0400

    Search result varaints

--- packages/ui/src/components/search/SearchResult.stories.tsx
+++ packages/ui/src/components/search/SearchResult.stories.tsx
@@ -16,12 +16,59 @@ export default meta;
 
 type Story = StoryObj<typeof meta>;
 
-export const Primary: Story = {
+export const PublicSolution: Story = {
 	args: {
 		result: {
 			message: {
 				...mockMessageWithDiscordAccount(),
 				solutionMessages: [mockMessageWithDiscordAccount()],
+				referencedMessage: mockMessageWithDiscordAccount(),
+			},
+			thread: mockChannelWithSettings(),
+			score: 0.5,
+			channel: mockChannelWithSettings(),
+			server: mockServer(),
+		},
+	},
+	render: ({ result }: { result: APISearchResult[number] }) => (
+		<div className="xl:w-2/3">
+			<SearchResult result={result} />
+		</div>
+	),
+};
+
+export const PrivateSolution: Story = {
+	args: {
+		result: {
+			message: {
+				...mockMessageWithDiscordAccount(),
+				solutionMessages: [
+					{
+						...mockMessageWithDiscordAccount(),
+						public: false,
+					},
+				],
+				referencedMessage: mockMessageWithDiscordAccount(),
+			},
+			thread: mockChannelWithSettings(),
+			score: 0.5,
+			channel: mockChannelWithSettings(),
+			server: mockServer(),
+		},
+	},
+	render: ({ result }: { result: APISearchResult[number] }) => (
+		<div className="xl:w-2/3">
+			<SearchResult result={result} />
+		</div>
+	),
+};
+
+export const NoSolution: Story = {
+	args: {
+		result: {
+			message: {
+				...mockMessageWithDiscordAccount(),
+				solutionMessages: [],
 				referencedMessage: mockMessageWithDiscordAccount(),
 			},
 			thread: mockChannelWithSettings(),
