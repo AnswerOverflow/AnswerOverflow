18ff6a0be5b6c510fced93199b71050f3ad683fa
JoltCode <joe@joecc.dev>
2023-03-14 09:34:57 +0000

    feat(web): noindex on staging

--- apps/nextjs/src/pages/_document.tsx
+++ apps/nextjs/src/pages/_document.tsx
@@ -1,39 +1,38 @@
-import Document, {
+import NextDocument, {
 	Html,
 	Head,
 	Main,
 	NextScript,
 	DocumentContext,
-	DocumentInitialProps,
 } from 'next/document';
 
-class MyDocument extends Document {
-	static override async getInitialProps(
-		ctx: DocumentContext,
-	): Promise<DocumentInitialProps> {
-		const initialProps = await Document.getInitialProps(ctx);
+const MyDocument = ({ isStaging }: { isStaging?: boolean }) => {
+	return (
+		<Html className="dark">
+			<Head>
+				{/* TODO: Swap for Next font */}
+				<link
+					href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;600&display=swap"
+					rel="stylesheet"
+				/>
+				{isStaging ? (
+					<meta name="robots" content="noindex, follow" />
+				) : (
+					<meta name="robots" content="index, follow" />
+				)}
+			</Head>
+			<body className="dark:bg-neutral-800">
+				<Main />
+				<NextScript />
+			</body>
+		</Html>
+	);
+};
 
-		return initialProps;
-	}
-
-	override render() {
-		return (
-			// eslint-disable-next-line tailwindcss/no-custom-classname
-			<Html className="dark">
-				<Head>
-					{/* TODO: Swap for Next font */}
-					<link
-						href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;600&display=swap"
-						rel="stylesheet"
-					/>
-				</Head>
-				<body className="dark:bg-neutral-800">
-					<Main />
-					<NextScript />
-				</body>
-			</Html>
-		);
-	}
-}
+MyDocument.getInitialProps = async (ctx: DocumentContext) => {
+	const initialProps = await NextDocument.getInitialProps(ctx);
+	const isStaging = process.env.STAGING === '1';
+	return { isStaging, ...initialProps };
+};
 
 export default MyDocument;
