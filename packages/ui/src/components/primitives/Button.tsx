155d84d650b7c842d411ad0057fcd7b95f557bac
JoltCode <joe@joecc.dev>
2023-03-15 12:22:20 +0000

    feat(ui): new buttons

--- packages/ui/src/components/primitives/Button.tsx
+++ packages/ui/src/components/primitives/Button.tsx
@@ -1,64 +1,53 @@
-import { cva, VariantProps } from 'cva';
-import type { Merge, SetNonNullable } from 'type-fest';
-import {
-	buttonStyles as buttonStylesData,
-	convertToCva,
-} from './button-styles';
+import * as React from 'react';
+import { VariantProps, cva } from 'cva';
 
-export type ButtonVariantProps = Merge<
-	SetNonNullable<Required<VariantProps<typeof buttonStyles>>>,
-	Pick<VariantProps<typeof buttonStyles>, 'disabled'>
->;
+import { cn } from '~ui/utils/styling';
 
-export const buttonStyles = cva(
-	'rounded-md px-8 py-2 font-body font-bold transition-all duration-100',
+const buttonVariants = cva(
+	'active:scale-95 inline-flex items-center justify-center rounded-md text-sm font-medium transition-colors focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 dark:hover:bg-slate-800 dark:hover:text-slate-100 disabled:opacity-50 dark:focus:ring-slate-400 disabled:pointer-events-none dark:focus:ring-offset-slate-900 data-[state=open]:bg-slate-100 dark:data-[state=open]:bg-slate-800',
 	{
-		// We don't need to declare any of these, as all the types are required, we only need to do compound variants
 		variants: {
-			type: {
-				solid: '',
-				ghost: '',
+			variant: {
+				default:
+					'bg-slate-900 text-white hover:bg-slate-700 dark:bg-slate-50 dark:text-slate-900',
+				destructive:
+					'bg-red-500 text-white hover:bg-red-600 dark:hover:bg-red-600',
+				outline:
+					'bg-transparent border border-slate-200 hover:bg-slate-100 dark:border-slate-700 dark:text-slate-100',
+				subtle:
+					'bg-slate-100 text-slate-900 hover:bg-slate-200 dark:bg-slate-700 dark:text-slate-100',
+				ghost:
+					'bg-transparent hover:bg-slate-100 dark:hover:bg-slate-800 dark:text-slate-100 dark:hover:text-slate-100 data-[state=open]:bg-transparent dark:data-[state=open]:bg-transparent',
+				link: 'bg-transparent dark:bg-transparent underline-offset-4 hover:underline text-slate-900 dark:text-slate-100 hover:bg-transparent dark:hover:bg-transparent',
 			},
-			color: {
-				red: '',
-				blue: '',
-				green: '',
-				black: '',
-				white: '',
-			},
-			disabled: {
-				true: '',
+			size: {
+				default: 'h-10 py-2 px-4',
+				sm: 'h-9 px-2 rounded-md',
+				lg: 'h-11 px-8 rounded-md',
 			},
 		},
-		compoundVariants: [...convertToCva(buttonStylesData)],
+		defaultVariants: {
+			variant: 'default',
+			size: 'default',
+		},
 	},
 );
 
-export interface ButtonProps extends ButtonVariantProps {
-	onClick?: () => void;
-	className?: string;
-}
+export interface ButtonProps
+	extends React.ButtonHTMLAttributes<HTMLButtonElement>,
+		VariantProps<typeof buttonVariants> {}
 
-export const Button: React.FC<React.PropsWithChildren<ButtonProps>> = ({
-	type,
-	color,
-	disabled,
-	onClick,
-	children,
-	className,
-}) => {
-	return (
-		<button
-			className={buttonStyles({
-				type,
-				color,
-				disabled: disabled ?? false,
-				className,
-			})}
-			onClick={() => onClick?.()}
-			disabled={disabled ?? false}
-		>
-			{children}
-		</button>
-	);
-};
+const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
+	({ className, variant, size, ...props }, ref) => {
+		return (
+			<button
+				className={cn(buttonVariants({ variant, size, className }))}
+				ref={ref}
+				{...props}
+			/>
+		);
+	},
+);
+Button.displayName = 'Button';
+
+export { Button, buttonVariants };
