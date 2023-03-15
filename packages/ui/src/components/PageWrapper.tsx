ca32174b42aefe215b0ecb523e44b1bd79e8c264
JoltCode <joe@joecc.dev>
2023-03-15 17:15:57 +0000

    tweak(ui): page wrapper styling and props

--- packages/ui/src/components/PageWrapper.tsx
+++ packages/ui/src/components/PageWrapper.tsx
@@ -1,10 +1,22 @@
 import { Navbar } from './primitives/Navbar';
 import { Footer } from './Footer';
 
-export const PageWrapper = ({ children }: { children: React.ReactNode }) => (
-	<div className="mx-auto w-full max-w-screen-2xl overflow-y-scroll  bg-ao-white scrollbar-hide overflow-x-hidden dark:bg-ao-black sm:px-4">
+export const PageWrapper = ({
+	children,
+	disableDefaultBackground,
+}: {
+	children: React.ReactNode;
+	disableDefaultBackground?: boolean;
+}) => (
+	<div className="mx-auto w-full overflow-y-scroll bg-ao-white scrollbar-hide overflow-x-hidden dark:bg-ao-black sm:px-4">
 		<Navbar />
-		{children}
+		<main
+			className={`${
+				disableDefaultBackground ? '' : 'bg-ao-white dark:bg-ao-black '
+			} px-[4rem] 2xl:px-[6rem]`}
+		>
+			{children}
+		</main>
 		<Footer />
 	</div>
 );
