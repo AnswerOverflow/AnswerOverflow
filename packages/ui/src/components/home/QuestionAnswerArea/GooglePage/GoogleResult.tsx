b2a7fecd42f2aa389fddf8d0123aa3513d12c180
JoltCode <joe@joecc.dev>
2023-03-15 12:36:19 +0000

    tweak(home): remove hover effect on google area

--- packages/ui/src/components/home/QuestionAnswerArea/GooglePage/GoogleResult.tsx
+++ packages/ui/src/components/home/QuestionAnswerArea/GooglePage/GoogleResult.tsx
@@ -7,10 +7,10 @@ export const GoogleResult = forwardRef<
 >(function GoogleResultComp({ url, title, description }, ref) {
 	return (
 		<div className="flex flex-col items-start justify-center px-5 font-['arial']">
-			<div className="group hover:cursor-pointer">
+			<div className="group">
 				<div className="flex w-full flex-row items-center justify-start">
 					<span className="text-[14px] text-[#bdc1c6]">{url}</span>
-					<div className="ml-[12px] h-[22px] w-[22px] hover:cursor-pointer">
+					<div className="ml-[12px] h-[22px] w-[22px]">
 						<svg
 							focusable="false"
 							xmlns="http://www.w3.org/2000/svg"
@@ -22,7 +22,7 @@ export const GoogleResult = forwardRef<
 					</div>
 				</div>
 				<div ref={ref}>
-					<h3 className="mb-[3px] pt-[5px] text-[20px] text-[#8ab4f8] group-hover:underline">
+					<h3 className="mb-[3px] pt-[5px] text-[20px] text-[#8ab4f8]">
 						{title}
 					</h3>
 				</div>
