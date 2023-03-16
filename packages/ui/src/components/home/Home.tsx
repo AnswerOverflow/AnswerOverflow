dfb589e07685e0063788464df8cfeabd24e4bac2
JoltCode <joe@joecc.dev>
2023-03-16 10:27:51 +0000

    tweak(ui/home): styling

--- packages/ui/src/components/home/Home.tsx
+++ packages/ui/src/components/home/Home.tsx
@@ -2,19 +2,21 @@ import { ChevronDownIcon } from '@heroicons/react/24/outline';
 import { HomeLeadText } from './HomeLeadText';
 import { QuestionAnswerArea } from './QuestionAnswerArea/QuestionAnswerArea';
 import { messageData } from './HomeMessages';
-import { PageWrapper } from '../PageWrapper';
+import { Navbar } from '../primitives/Navbar';
 
 export const Home = () => {
 	const DownChevron = () => (
-		<div className="absolute bottom-0 w-full">
+		<div className="absolute">
 			<div className="mx-auto h-16 w-16 text-black/[.65] dark:text-white/[.65]">
 				{<ChevronDownIcon strokeWidth={0.5} />}
 			</div>
 		</div>
 	);
 	return (
-		<PageWrapper>
-			<div className="relative z-50 min-h-[calc(100vh-4rem)] py-10">
+		<div className="bg-[linear-gradient(180.49deg,_#1A1818_-12.07%,_#0E0D0D_-12.07%,_#040405_-12.06%,_#101214_103.52%)] sm:px-4">
+			<Navbar />
+
+			<div className="relative z-50 min-h-[calc(100vh-10rem)] py-10 px-4 sm:px-[4rem] 2xl:px-[6rem]">
 				<DownChevron />
 				<div className="flex h-full w-full flex-row transition-all lg:gap-32 2xl:gap-72">
 					<HomeLeadText />
@@ -29,6 +31,6 @@ export const Home = () => {
 					</div>
 				</div>
 			</div>
-		</PageWrapper>
+		</div>
 	);
 };
