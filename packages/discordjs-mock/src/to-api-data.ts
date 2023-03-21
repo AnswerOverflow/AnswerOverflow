f58552b46c75b639cc04001d5191a50aeb1565cd
Rhys Sullivan <39114868+RhysSullivan@users.noreply.github.com>
2023-03-21 02:44:07 -0400

    Update to-api-data.ts

--- packages/discordjs-mock/src/to-api-data.ts
+++ packages/discordjs-mock/src/to-api-data.ts
@@ -45,7 +45,7 @@ export function channelToAPIChannel(channel: Channel): APIChannel {
 			type: channel.type,
 			flags: channel.flags?.bitfield,
 			last_message_id: channel.lastMessageId,
-			name: channel.recipient?.username,
+			name: null,
 			recipients: [channel.client.user, channel.recipient].map((user) =>
 				userToAPIUser(user!),
 			), // TODO: Is the bot a recipient?
