b6b36571d6bd444b9642e6c335dd6d8b0895e022
Rhys Sullivan <39114868+RhysSullivan@users.noreply.github.com>
2023-03-22 15:52:01 -0400

    fix(web): add more padding to navbar

--- packages/ui/src/components/home/Home.tsx
+++ packages/ui/src/components/home/Home.tsx
@@ -7,7 +7,7 @@ export const Home = () => {
 	return (
 		<>
 			<div className="relative bg-[linear-gradient(180.49deg,_#1A1818_-12.07%,_#0E0D0D_-12.07%,_#040405_-12.06%,_#101214_103.52%)]">
-				<Navbar additionalPadding />
+				<Navbar />
 				<div className="sm:px-4">
 					<HeroArea />
 				</div>
