d2d95885d5c2824d90a429818d68bc4ea9c03e35
Rhys Sullivan <39114868+RhysSullivan@users.noreply.github.com>
2023-03-22 16:40:15 -0400

    feat(web): make down chevron clickable

--- packages/ui/src/components/home/HeroArea.tsx
+++ packages/ui/src/components/home/HeroArea.tsx
@@ -1,6 +1,5 @@
 import { QuestionAnswerArea } from './QuestionAnswerArea/QuestionAnswerArea';
 import { messageData } from './HomeMessages';
-import { ChevronDownIcon } from '@heroicons/react/24/outline';
 import { Button } from '../primitives/Button';
 
 const HomeLeadText = () => {
@@ -24,18 +23,9 @@ const HomeLeadText = () => {
 	);
 };
 
-const DownChevron = () => (
-	<div className="absolute bottom-0 left-1/2 -translate-x-1/2">
-		<div className="mx-auto h-16 w-16 text-black/[.65] dark:text-white/[.65]">
-			{<ChevronDownIcon strokeWidth={0.5} />}
-		</div>
-	</div>
-);
-
 export const HeroArea = () => {
 	return (
 		<div className="z-20 min-h-[calc(100vh-10rem)] px-4 pt-10 pb-20 sm:px-[4rem] 2xl:px-[6rem]">
-			<DownChevron />
 			<div className="flex h-full w-full flex-row transition-all lg:gap-32 2xl:gap-72">
 				<HomeLeadText />
 				<div className="hidden items-center justify-center xl:flex 2xl:grow">
--- packages/ui/src/components/home/Home.tsx
+++ packages/ui/src/components/home/Home.tsx
@@ -2,8 +2,30 @@ import { Footer } from '../Footer';
 import { Navbar } from '../primitives/Navbar';
 import { AboutArea } from './AboutArea';
 import { HeroArea } from './HeroArea';
-
+import { ChevronDownIcon } from '@heroicons/react/24/solid';
+import { Button } from '../primitives';
+import { useRef } from 'react';
+const DownChevron = (props: { scrollIntoView: () => unknown }) => (
+	<div className="absolute bottom-10 left-1/2 -translate-x-1/2">
+		<div className="mx-auto h-16 w-16 text-black/[.65] dark:text-white/[.65]">
+			<Button
+				variant="ghost"
+				className="h-16 w-16 rounded-lg focus:ring-0 focus:ring-offset-0 dark:hover:bg-ao-black"
+				onClick={props.scrollIntoView}
+			>
+				<ChevronDownIcon className="h-16 w-16" />
+			</Button>
+		</div>
+	</div>
+);
 export const Home = () => {
+	const aboutRef = useRef<HTMLDivElement>(null);
+	const executeScroll = () =>
+		aboutRef.current?.scrollIntoView({
+			behavior: 'smooth',
+			block: 'start',
+		});
+
 	return (
 		<>
 			<div className="relative bg-ao-white dark:bg-[linear-gradient(180.49deg,_#1A1818_-12.07%,_#0E0D0D_-12.07%,_#040405_-12.06%,_#101214_103.52%)]">
@@ -12,7 +34,11 @@ export const Home = () => {
 					<HeroArea />
 				</div>
 			</div>
-			<div className="flex justify-center bg-ao-white/[.95] dark:bg-gradient-to-b dark:from-[#0F1113] dark:to-ao-black sm:px-4">
+			<DownChevron scrollIntoView={executeScroll} />
+			<div
+				className="flex justify-center bg-ao-white/[.95] dark:bg-gradient-to-b dark:from-[#0F1113] dark:to-ao-black sm:px-4"
+				ref={aboutRef}
+			>
 				<AboutArea />
 			</div>
 			<Footer />
