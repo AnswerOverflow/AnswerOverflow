3c0f877d9800a2e293a71bf209781e953d780295
Rhys Sullivan <39114868+RhysSullivan@users.noreply.github.com>
2023-03-22 21:09:37 -0400

    feat(home): add used by marquee

--- packages/ui/src/components/home/Home.tsx
+++ packages/ui/src/components/home/Home.tsx
@@ -10,10 +10,10 @@ const DownChevron = (props: { scrollIntoView: () => unknown }) => (
 		<div className="mx-auto h-16 w-16 text-black/[.65] dark:text-white/[.65]">
 			<Button
 				variant="ghost"
-				className="h-16 w-16 rounded-lg focus:ring-0 focus:ring-offset-0 dark:hover:bg-ao-black"
+				className="h-16 w-16 rounded-lg focus:ring-0 focus:ring-offset-0 dark:hover:bg-inherit"
 				onClick={props.scrollIntoView}
 			>
-				<ChevronDownIcon className="h-16 w-16" />
+				<ChevronDownIcon className="h-16 w-16 text-ao-black/[.6] hover:text-ao-black dark:text-ao-white/[.6] dark:hover:text-ao-white" />
 			</Button>
 		</div>
 	</div>
@@ -27,21 +27,20 @@ export const Home = () => {
 		});
 
 	return (
-		<>
-			<div className="relative bg-ao-white dark:bg-[linear-gradient(180.49deg,_#1A1818_-12.07%,_#0E0D0D_-12.07%,_#040405_-12.06%,_#101214_103.52%)]">
-				<Navbar />
-				<div className="sm:px-4">
-					<HeroArea />
+		<div className="flex w-full flex-col items-center bg-ao-white dark:bg-[linear-gradient(180.49deg,_#1A1818_-12.07%,_#0E0D0D_-12.07%,_#040405_-12.06%,_#101214_103.52%)]">
+			<div className="max-w-screen-2xl">
+				<div className="relative ">
+					<Navbar />
+					<div className="sm:px-4">
+						<HeroArea />
+					</div>
 				</div>
+				<DownChevron scrollIntoView={executeScroll} />
+				<div className="flex justify-center " ref={aboutRef}>
+					<AboutArea />
+				</div>
+				<Footer />
 			</div>
-			<DownChevron scrollIntoView={executeScroll} />
-			<div
-				className="flex justify-center bg-ao-white/[.95] dark:bg-gradient-to-b dark:from-[#0F1113] dark:to-ao-black sm:px-4"
-				ref={aboutRef}
-			>
-				<AboutArea />
-			</div>
-			<Footer />
-		</>
+		</div>
 	);
 };
