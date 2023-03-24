ade7a4b6370878581ef6ac5889975f97534c83db
JoltCode <joe@joecc.dev>
2023-03-24 15:46:12 +0000

    fix(ui/home): drop shadow render bug (#294)

--- packages/ui/src/components/pages/home/AboutArea.tsx
+++ packages/ui/src/components/pages/home/AboutArea.tsx
@@ -11,7 +11,7 @@ const HomeFeature = (props: {
 	featureDescription?: React.ReactNode;
 }) => {
 	return (
-		<div className="flex flex-col items-center justify-center rounded-standard border-2 border-white/[.13] bg-[#fafafa] px-2 py-4 text-center drop-shadow-xl dark:bg-[#191B1F] md:px-20 md:py-10 ">
+		<div className="flex flex-col items-center justify-center rounded-standard border-2 border-white/[.13] bg-[#fafafa] px-2 py-4 text-center dark:bg-[#191B1F] md:px-20 md:py-10 ">
 			<Paragraph className="text-xl md:text-2xl">{props.featureName}</Paragraph>
 			<Paragraph className="text-lg">{props.featureDescription}</Paragraph>
 		</div>
