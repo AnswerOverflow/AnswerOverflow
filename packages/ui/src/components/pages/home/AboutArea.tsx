a5e740b4c72832ac474d4a10d4a54adc887a27f3
JoltCode <joe@joecc.dev>
2023-03-27 14:11:47 +0100

    tweak(ui/home): about area styling

--- packages/ui/src/components/pages/home/AboutArea.tsx
+++ packages/ui/src/components/pages/home/AboutArea.tsx
@@ -1,10 +1,11 @@
-import { Heading, Paragraph, GetStarted } from '~ui/components/primitives';
+import { Heading, Paragraph } from '~ui/components/primitives';
 import Balancer from 'react-wrap-balancer';
 import Image from 'next/image';
 import Marquee from 'react-fast-marquee';
 import Link from 'next/link';
 import { cn } from '~ui/utils/styling';
 import { trackEvent } from '@answeroverflow/hooks';
+import { PricingArea } from './PricingArea';
 
 // TODO: Link to docs for feature?
 const HomeFeature = (props: {
@@ -12,7 +13,7 @@ const HomeFeature = (props: {
 	featureDescription?: React.ReactNode;
 }) => {
 	return (
-		<div className="flex flex-col items-center justify-center rounded-standard border-2 border-gray-300 bg-[#fafafa] px-2 py-4 text-center dark:border-white/[.13] dark:bg-[#191B1F] md:px-20 md:py-10 ">
+		<div className="flex flex-col items-center justify-center rounded-standard border-2 border-gray-300 bg-[#fafafa] px-2 py-4 text-center dark:border-white/[.13] dark:bg-ao-black md:px-20 md:py-10 ">
 			<Paragraph className="text-xl md:text-2xl">{props.featureName}</Paragraph>
 			<Paragraph className="text-lg">{props.featureDescription}</Paragraph>
 		</div>
@@ -25,11 +26,11 @@ const FeaturesSection = (props: { className?: string }) => {
 			<Heading.H2 className="text-center md:text-right">
 				Tools tailored to your community
 			</Heading.H2>
-			<Heading.H2 className="pt-0 text-center text-lg md:text-right">
+			<Heading.H3 className="pt-0 text-center text-lg md:text-right">
 				<Balancer>
 					Your community is unique, {"it's"} time you had a bot that matches it
 				</Balancer>
-			</Heading.H2>
+			</Heading.H3>
 
 			<div className="mt-2 grid grid-cols-1 grid-rows-1 gap-10 md:grid-cols-2 md:grid-rows-2">
 				<HomeFeature
@@ -58,27 +59,6 @@ const FeaturesSection = (props: { className?: string }) => {
 	);
 };
 
-const EndSection = (props: { className?: string }) => {
-	return (
-		<section
-			className={cn(
-				'flex w-full flex-col items-center justify-center border-2 border-white/[.13] bg-[#ffffffd2] drop-shadow-2xl dark:bg-[#191B1F] dark:drop-shadow',
-				props.className,
-			)}
-		>
-			<Heading.H2 className="text-center md:w-full">
-				<Balancer>Setup in your server for free</Balancer>
-			</Heading.H2>
-			<Paragraph className="text-center md:w-full">
-				<Balancer className="md:w-full">
-					Join over 50+ communities using Answer Overflow to make the most of
-					their support
-				</Balancer>
-			</Paragraph>
-			<GetStarted variant={'default'} location="About Area" />
-		</section>
-	);
-};
 type FeaturedCommunityProps = {
 	id: string;
 	name: string;
@@ -181,7 +161,7 @@ const FeaturedCommunitiesSection = (props: { className?: string }) => {
 			<Heading.H2>
 				<Balancer>Used by some of the largest support servers</Balancer>
 			</Heading.H2>
-			<div className="flex max-w-vw90 items-center pt-20 sm:max-w-vw70 2xl:max-w-screen-xl">
+			<div className="max-w-vw90 sm:max-w-vw70 flex items-center pt-20 2xl:max-w-screen-xl">
 				<Marquee gradient={false} speed={30}>
 					{featuredCommunities.map((community) => (
 						<div key={community.id} className="mx-10 my-0">
@@ -199,7 +179,7 @@ export const AboutArea = () => {
 		<div className="flex flex-col items-center px-4 pt-10 pb-20 sm:px-[4rem] 2xl:px-[6rem]">
 			<FeaturesSection />
 			<FeaturedCommunitiesSection className="py-20" />
-			<EndSection className="py-20" />
+			<PricingArea className="py-20" />
 		</div>
 	);
 };
