defb4457b646a2c300ba7f62f5d87def4cfd34c1
Cole-Heigis <cole.heigis@mymail.champlain.edu>
2023-03-27 20:47:53 +0000

    Changed status looping implementation

--- apps/discord-bot/src/listeners/events/status.ts
+++ apps/discord-bot/src/listeners/events/status.ts
@@ -1,38 +1,43 @@
 import { ApplyOptions } from "@sapphire/decorators";
 import { Listener, SapphireClient } from "@sapphire/framework";
-import { ActivityType, Events } from "discord.js";
+import { ActivityOptions, ActivityType, Events } from "discord.js";
 
-const minutesBetweenSwitches = 0.25;
+type StatusUpdate = {
+  getStatus: (() => Promise<string> | string) | string;
+} & ActivityOptions;
 
-//add messages in the form ['<message body>', ActivityType.<your type>]
-const messages: [
-  string,
-  (
-    | ActivityType.Playing
-    | ActivityType.Streaming
-    | ActivityType.Listening
-    | ActivityType.Watching
-    | ActivityType.Competing
-  )
-][] = [
-  [" X communities!", ActivityType.Watching],
-  [" over X questions!", ActivityType.Listening],
-  [" with something I need ideas", ActivityType.Playing],
-  [" hell idk", ActivityType.Streaming],
-];
-
-function newStatus(client: SapphireClient) {
-  const time = new Date().getSeconds();
-  const index = Math.round(time / (60 * minutesBetweenSwitches));
-  const message = messages[index];
-  if (message != undefined) {
-    client.user?.setActivity(message[0], { type: message[1] });
-  }
+function getStatuses(client: SapphireClient) {
+  const statuses: StatusUpdate[] = [
+      { 
+        getStatus: () => `${client.guilds.cache.size} communities.`, 
+        type: ActivityType.Watching, 
+      },
+      {
+          type: ActivityType.Listening,
+          async getStatus() {
+              const messageCount = 10; //await getNumberOfIndexedMessages();
+              return ` to ${messageCount} messages`;
+          },
+      },
+      {
+          type: ActivityType.Watching,
+          getStatus: 'hello',
+      },
+  ];
+  return statuses;
 }
-
+ 
 @ApplyOptions<Listener.Options>({ event: Events.ClientReady })
-export class StartStatusLoop extends Listener<typeof Events.ClientReady> {
-  public run(client: SapphireClient) {
-    setInterval(newStatus, 60 * minutesBetweenSwitches * 1000, client);
-  }
-}
+export class SendMarkSolutionInstructionsOnThreadCreate extends Listener {
+  public async run() {
+      // eslint-disable-next-line @typescript-eslint/no-misused-promises
+      setInterval(async () => {
+          const statuses = getStatuses(this.container.client);
+          //const status = statuses[Math.floor(Math.random() * statuses.length)]!;
+          var index = Math.floor((new Date().getSeconds())/60 * statuses.length);
+          const status = statuses[index]!;
+    const statusText = typeof status.getStatus === 'string' ? status.getStatus : await status.getStatus();
+          this.container.client.user?.setActivity(statusText, {
+              type: status.type,
+          });
+      }, 1000 * 15)}};
