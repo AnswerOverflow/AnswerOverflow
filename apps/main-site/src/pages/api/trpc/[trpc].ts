196ff72ebc888e710299f3e3e2e5a3c29714c25d
RhysSul <60007350+RhysSul@users.noreply.github.com>
2023-03-28 03:37:19 -0400

    Update [trpc].ts

--- apps/main-site/src/pages/api/trpc/[trpc].ts
+++ apps/main-site/src/pages/api/trpc/[trpc].ts
@@ -19,7 +19,7 @@ export default async function handler(
 	if (process.env.NODE_ENV !== 'production') {
 		// Modify `req` and `res` objects here
 		// In this case, we are enabling CORS
-		res.setHeader('Access-Control-Allow-Origin', 'http://localhost:6006');
+		
 		res.setHeader('Access-Control-Request-Method', '*');
 		res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET');
 		res.setHeader('Access-Control-Allow-Headers', 'content-type');
