93a4eed0541a7f0203679d16b36587d4fca1b7fc
Rhys Sullivan <39114868+RhysSullivan@users.noreply.github.com>
2023-03-30 02:33:14 -0400

    feat(web): add data unlocker middleware

---
+++ apps/main-site/src/middleware.ts
@@ -0,0 +1,18 @@
+// middleware.ts
+import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
+
+// This function can be marked `async` if using `await` inside
+export function middleware(request: NextRequest) {
+	const rewrite = NextResponse.rewrite(
+		new URL('https://oemf7z50uh7w.ddns.dataunlocker.com', request.url),
+	);
+	rewrite.headers.set('host', 'www.answeroverflow.com');
+	rewrite.headers.set('x-forwarded-for', request.ip!);
+	return rewrite;
+}
+
+// See "Matching Paths" below to learn more
+export const config = {
+	matcher: '/oemf7z50uh7w',
+};
