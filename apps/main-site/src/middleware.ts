6dd09cf068fdeec1a12c8fa7afedec7dd66079ca
Rhys Sullivan <39114868+RhysSullivan@users.noreply.github.com>
2023-03-30 02:53:20 -0400

    fix(web): add wildcard to matcher for data unlocker

--- apps/main-site/src/middleware.ts
+++ apps/main-site/src/middleware.ts
@@ -4,15 +4,19 @@ import type { NextRequest } from 'next/server';
 
 // This function can be marked `async` if using `await` inside
 export function middleware(request: NextRequest) {
+	const target = request.nextUrl.pathname.split('/oemf7z50uh7w/')[1];
 	const rewrite = NextResponse.rewrite(
-		new URL('https://oemf7z50uh7w.ddns.dataunlocker.com', request.url),
+		new URL(
+			`https://oemf7z50uh7w.ddns.dataunlocker.com/${target ?? ''}${
+				request.nextUrl.search
+			}`,
+		),
 	);
 	rewrite.headers.set('host', 'www.answeroverflow.com');
 	rewrite.headers.set('x-forwarded-for', request.ip!);
 	return rewrite;
 }
-
 // See "Matching Paths" below to learn more
 export const config = {
-	matcher: '/oemf7z50uh7w',
+	matcher: '/oemf7z50uh7w/:path*',
 };
