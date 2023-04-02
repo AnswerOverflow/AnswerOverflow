15c222d9a993797480b0051935c02e7eb3b0c7a0
Rhys Sullivan <39114868+RhysSullivan@users.noreply.github.com>
2023-04-02 16:56:56 -0400

    fix(web): fix ISR breaking on pages w/ private messages

--- apps/main-site/src/pages/m/[messageId].tsx
+++ apps/main-site/src/pages/m/[messageId].tsx
@@ -11,7 +11,8 @@ export default function MessageResult(
 ) {
 	const { serverId, messageId, areAllMessagesPublic } = props;
 	const isUserInServer = useIsUserInServer(serverId);
-	const shouldFetchPrivateMessages = isUserInServer && !areAllMessagesPublic;
+	const shouldFetchPrivateMessages =
+		isUserInServer !== 'loading' && !areAllMessagesPublic;
 	const { data } = trpc.messages.threadFromMessageId.useQuery(messageId, {
 		// For authenticated users that are in the server, we fetch the messages incase any of them are private
 		enabled: shouldFetchPrivateMessages,
