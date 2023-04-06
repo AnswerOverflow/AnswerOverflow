39a75d924d8fa3b87a3ad1cd20555abb534ee791
Rhys Sullivan <39114868+RhysSullivan@users.noreply.github.com>
2023-04-06 15:11:40 -0400

    fix

--- scripts/vitest.config.ts
+++ scripts/vitest.config.ts
@@ -1,28 +1,32 @@
 // Reference https://github.com/sapphiredev/utilities/blob/main/scripts/vitest.config.ts
-import type { ESBuildOptions } from "vite";
-import { defineConfig, type UserConfig } from "vitest/config";
+import type { ESBuildOptions } from 'vite';
+import { defineConfig, type UserConfig } from 'vitest/config';
 
-export const createVitestConfig = (options: UserConfig = {}) =>
-  defineConfig({
-    ...options,
-    test: {
-      ...options?.test,
-      testTimeout: 60000,
-      globals: true,
-      coverage: {
-        ...options.test?.coverage,
-        enabled: false,
-        reporter: ["text", "lcov", "clover"],
-        exclude: [
-          ...(options.test?.coverage?.exclude ?? []),
-          "**/node_modules/**",
-          "**/dist/**",
-          "**/tests/**",
-        ],
-      },
-    },
-    esbuild: {
-      ...options?.esbuild,
-      target: (options?.esbuild as ESBuildOptions | undefined)?.target ?? "es2020",
-    },
-  });
+export const createVitestConfig = (options: UserConfig = {}) => {
+	defineConfig({
+		...options,
+		test: {
+			...options?.test,
+			testTimeout: 60000,
+			globals: true,
+			coverage: {
+				...options.test?.coverage,
+				enabled: false,
+				provider: 'c8',
+				reporter: ['text', 'lcov', 'clover'],
+				exclude: [
+					...(options.test?.coverage?.exclude ?? []),
+					'**/node_modules/**',
+					'**/dist/**',
+					'**/tests/**',
+				],
+			},
+		},
+		esbuild: {
+			...options?.esbuild,
+			target:
+				(options?.esbuild as ESBuildOptions | undefined)?.target ?? 'es2020',
+		},
+	});
+};
+return;
