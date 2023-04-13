120506e64d3edb41776a2934109d6f280b93bb62
JoltCode <joe@joecc.dev>
2023-04-13 14:47:54 +0100

    tweak(ui): get started, added checkbox

--- packages/ui/src/components/pages/getStarted/getStartedPage/GetStartedSectionPages.tsx
+++ packages/ui/src/components/pages/getStarted/getStartedPage/GetStartedSectionPages.tsx
@@ -85,7 +85,17 @@ const configPages: {
 	readonly [key: string]: FC<GetStartedPageProps>;
 } = {
 	ChannelSettings: () => {
-		return <div>foo</div>;
+		return (
+			<div className="page-slide-in">
+				<Heading.H1 className="pb-16 text-center text-4xl">
+					Let{"'"}s set up your channel settings!
+				</Heading.H1>
+
+				{/* Checkbox showing  */}
+
+				<Heading.H2>Enable indexing on your favorite channels</Heading.H2>
+			</div>
+		);
 	},
 };
 
