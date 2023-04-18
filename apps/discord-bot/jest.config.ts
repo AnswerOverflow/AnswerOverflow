1e1b9bc702785b561fe63e5d3c73d24b47bcfcf6
Rhys Sullivan <39114868+RhysSullivan@users.noreply.github.com>
2023-04-18 02:38:45 -0400

    fix(bot): remove open handles warning

--- apps/discord-bot/jest.config.ts
+++ apps/discord-bot/jest.config.ts
@@ -8,6 +8,7 @@ const jestConfig: JestConfigWithTsJest = {
 	preset: 'ts-jest',
 	resetMocks: true,
 	testTimeout: 60000,
+	openHandlesTimeout: 0, // We don't care about open handles
 	testEnvironment: 'node',
 	testMatch: ['**/*.test.ts(x)?'],
 	moduleNameMapper: {
