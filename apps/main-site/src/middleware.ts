d76736f5e0ed74cc9598c75be1a4129ec2807719
Rhys Sullivan <39114868+RhysSullivan@users.noreply.github.com>
2023-04-19 01:19:13 -0400

    Update middleware.ts

--- apps/main-site/src/middleware.ts
+++ apps/main-site/src/middleware.ts
@@ -3,18 +3,35 @@ import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 
 // This function can be marked `async` if using `await` inside
-export function middleware(request: NextRequest) {
-	const rewrite = NextResponse.rewrite(
-		new URL(
-			`${request.nextUrl.pathname}${request.nextUrl.search}`,
-			`https://oemf7z50uh7w.ddns.dataunlocker.com/`,
-		),
-	);
-	rewrite.headers.set('host', 'www.answeroverflow.com');
-	rewrite.headers.set('x-forwarded-for', request.ip!);
-	return rewrite;
+export function middleware(req: NextRequest) {
+	const url = req.nextUrl;
+	const hostname = req.headers.get('host') || 'www.answeroverflow.com'; // fallback to www.answeroverflow.com
+	const path = url.pathname;
+	if (path.startsWith('/oemf7z50uh7w/')) {
+		const rewrite = NextResponse.rewrite(
+			new URL(
+				`${req.nextUrl.pathname}${req.nextUrl.search}`,
+				`https://oemf7z50uh7w.ddns.dataunlocker.com/`,
+			),
+		);
+		rewrite.headers.set('host', 'www.answeroverflow.com');
+		rewrite.headers.set('x-forwarded-for', req.ip!);
+		return rewrite;
+	}
+	const isMainSite = hostname === 'www.answeroverflow.com';
+	if (isMainSite) {
+		return NextResponse.next();
+	}
+
+	// Redirect the homepage to their community page
+	const isHomepage = path === '/';
+	if (isHomepage) {
+		return NextResponse.rewrite(
+			new URL(`/c/1081235771270369380`, `https://www.answeroverflow.com/`),
+		);
+	}
 }
 // See "Matching Paths" below to learn more
 export const config = {
-	matcher: '/oemf7z50uh7w/:path*',
+	matcher: ['/oemf7z50uh7w/:path*', '/m/:path*', '/c/:path*', '/'],
 };
