6ea2f7f041c8b27783160c959b3735a911e39ae9
Rhys Sullivan <39114868+RhysSullivan@users.noreply.github.com>
2023-04-19 03:46:13 -0400

    some wip stuff

--- apps/main-site/src/middleware.ts
+++ apps/main-site/src/middleware.ts
@@ -2,11 +2,24 @@
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 
+const mainSiteHostName =
+	process.env.NODE_ENV === 'production'
+		? 'www.answeroverflow.com'
+		: 'localhost:3000';
+
+const mainSiteBase =
+	process.env.NODE_ENV === 'production'
+		? `https://${mainSiteHostName}`
+		: `http://${mainSiteHostName}`;
+
 // This function can be marked `async` if using `await` inside
 export function middleware(req: NextRequest) {
 	const url = req.nextUrl;
-	const hostname = req.headers.get('host') || 'www.answeroverflow.com'; // fallback to www.answeroverflow.com
+	const hostname = req.headers.get('host') || mainSiteHostName; // fallback to www.answeroverflow.com
 	const path = url.pathname;
+	const params = req.nextUrl.search;
+
+	// Data Unlocker
 	if (path.startsWith('/oemf7z50uh7w/')) {
 		const rewrite = NextResponse.rewrite(
 			new URL(
@@ -14,24 +27,49 @@ export function middleware(req: NextRequest) {
 				`https://oemf7z50uh7w.ddns.dataunlocker.com/`,
 			),
 		);
-		rewrite.headers.set('host', 'www.answeroverflow.com');
+		rewrite.headers.set('host', mainSiteHostName);
 		rewrite.headers.set('x-forwarded-for', req.ip!);
 		return rewrite;
 	}
-	const isMainSite = hostname === 'www.answeroverflow.com';
-	if (isMainSite) {
-		return NextResponse.next();
-	}
+	return NextResponse.next();
 
-	// Redirect the homepage to their community page
-	const isHomepage = path === '/';
-	if (isHomepage) {
-		return NextResponse.rewrite(
-			new URL(`/c/1081235771270369380`, `https://www.answeroverflow.com/`),
-		);
-	}
+	// const isMessageRoute = path.startsWith('/m/');
+	// if (isMessageRoute && hostname === mainSiteHostName) {
+	// 	return NextResponse.redirect(
+	// 		new URL(`${req.nextUrl.pathname}${params}`, `http://localhost:3001/`),
+	// 	);
+	// }
+
+	// // Don't do anything if the hostname is the main site
+	// const isMainSiteHostname = hostname === mainSiteHostName;
+	// if (isMainSiteHostname) {
+	// 	// TODO: Can we just return NextResponse.next() here?
+	// 	return NextResponse.next();
+	// }
+
+	// // TODO: Look up what server this is for
+
+	// // Redirect the homepage to their community page
+	// const isHomepage = path === '/';
+	// if (isHomepage) {
+	// 	return NextResponse.rewrite(
+	// 		new URL(`/c/1037547185492996207`, mainSiteBase),
+	// 	);
+	// }
+	// url.hostname = mainSiteHostName;
+	// return NextResponse.rewrite(url);
 }
 // See "Matching Paths" below to learn more
 export const config = {
-	matcher: ['/oemf7z50uh7w/:path*', '/m/:path*', '/c/:path*', '/'],
+	matcher: [
+		'/oemf7z50uh7w/:path*',
+		/*
+		 * Match all paths except for:
+		 * 1. /api routes
+		 * 2. /_next (Next.js internals)
+		 * 3. /examples (inside /public)
+		 * 4. all root files inside /public (e.g. /favicon.ico)
+		 */
+		'/((?!api/|_next/|_static/|[\\w-]+\\.\\w+).*)',
+	],
 };
