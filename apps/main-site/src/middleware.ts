650d9107fc27a04d13f4192b61cee744edf3d595
Rhys Sullivan <39114868+RhysSullivan@users.noreply.github.com>
2023-04-19 04:37:10 -0400

    ugly??? or great, to be decided

--- apps/main-site/src/middleware.ts
+++ apps/main-site/src/middleware.ts
@@ -1,6 +1,7 @@
 // middleware.ts
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
+import type { NextURL } from 'next/dist/server/web/next-url';
 
 const mainSiteHostName =
 	process.env.NODE_ENV === 'production'
@@ -12,14 +13,41 @@ const mainSiteBase =
 		? `https://${mainSiteHostName}`
 		: `http://${mainSiteHostName}`;
 
-// This function can be marked `async` if using `await` inside
-export function middleware(req: NextRequest) {
-	const url = req.nextUrl;
-	const hostname = req.headers.get('host') || mainSiteHostName; // fallback to www.answeroverflow.com
-	const path = url.pathname;
-	const params = req.nextUrl.search;
+/*
 
-	// Data Unlocker
+  Middleware Notes:
+
+  1. Data Unlocker
+  Rewrite requests to the data unlocker domain to capture analytics, doesn't matter what the hostname is
+
+  TODO: Duplication of work in redirection, flow is: main site -> redirect to tenant site -> rewrite to main site. could optimize?
+  2. Tenant Routes, (/c/, /m/, /)
+   a. /m/
+      - If the hostname is the main site, redirect to the tenant site
+
+    b. /c/
+      - If the hostname is the main site, redirect to the tenant site homepage
+
+    c. /
+      - If the hostname is the main site, keep the path the same
+      - If the hostname is the tenant site, rewrite to the tenant site community page
+
+
+
+
+*/
+
+type PathHandler = {
+	url: NextURL;
+	req: NextRequest;
+	hostname: string;
+	path: string;
+	params: string;
+	isMainSiteHostname: boolean;
+};
+
+function dataUnlockerRouteHandler(input: PathHandler) {
+	const { path, req } = input;
 	if (path.startsWith('/oemf7z50uh7w/')) {
 		const rewrite = NextResponse.rewrite(
 			new URL(
@@ -31,33 +59,97 @@ export function middleware(req: NextRequest) {
 		rewrite.headers.set('x-forwarded-for', req.ip!);
 		return rewrite;
 	}
-	return NextResponse.next();
+	return undefined;
+}
 
-	// const isMessageRoute = path.startsWith('/m/');
-	// if (isMessageRoute && hostname === mainSiteHostName) {
-	// 	return NextResponse.redirect(
-	// 		new URL(`${req.nextUrl.pathname}${params}`, `http://localhost:3001/`),
-	// 	);
-	// }
+function messageRouteHandler(input: PathHandler) {
+	const { path, req, params, isMainSiteHostname } = input;
+	// Redirect tenant messages on the main site to the tenant site
+	const isMessageRoute = path.startsWith('/m/');
+	if (isMessageRoute && isMainSiteHostname) {
+		// TODO: Get the server id from the database
+		return NextResponse.redirect(
+			new URL(`${req.nextUrl.pathname}${params}`, `http://localhost:3001/`),
+			{
+				status: 308,
+			},
+		);
+	}
+	return undefined;
+}
 
-	// // Don't do anything if the hostname is the main site
-	// const isMainSiteHostname = hostname === mainSiteHostName;
-	// if (isMainSiteHostname) {
-	// 	// TODO: Can we just return NextResponse.next() here?
-	// 	return NextResponse.next();
-	// }
+function communityPageRouteHandler(input: PathHandler) {
+	const { path, req, params, isMainSiteHostname } = input;
+	// Redirect tenant communities on the main site to the tenant site
+	const isCommunityRoute = path.startsWith('/c/');
+	if (isCommunityRoute) {
+		if (isMainSiteHostname) {
+			// TODO: Get the server id from the database
+			return NextResponse.redirect(
+				new URL(`${req.nextUrl.pathname}${params}`, `http://localhost:3001/`),
+				{
+					status: 308,
+				},
+			);
+		} else {
+			// Redirect back to homepage, tenant sites only have one community
+			return NextResponse.redirect(new URL(`/`, mainSiteBase), {
+				status: 308,
+			});
+		}
+	}
+	return undefined;
+}
 
-	// // TODO: Look up what server this is for
+function homePageRouteHandler(input: PathHandler) {
+	const { path, isMainSiteHostname } = input;
+	// Rewrite tenant homepages to be community pages
+	const isHomepage = path === '/';
+	if (isHomepage && !isMainSiteHostname) {
+		return NextResponse.rewrite(
+			new URL(`/c/1037547185492996207`, mainSiteBase),
+		);
+	}
+	return undefined;
+}
 
-	// // Redirect the homepage to their community page
-	// const isHomepage = path === '/';
-	// if (isHomepage) {
-	// 	return NextResponse.rewrite(
-	// 		new URL(`/c/1037547185492996207`, mainSiteBase),
-	// 	);
-	// }
-	// url.hostname = mainSiteHostName;
-	// return NextResponse.rewrite(url);
+export function middleware(req: NextRequest) {
+	const url = req.nextUrl;
+	const hostname = req.headers.get('host') || mainSiteHostName; // fallback to www.answeroverflow.com
+	const path = url.pathname;
+	const params = req.nextUrl.search;
+	const isMainSiteHostname = hostname === mainSiteHostName;
+	const input: PathHandler = {
+		url,
+		req,
+		hostname,
+		path,
+		params,
+		isMainSiteHostname,
+	};
+	const dataUnlockerHandler = dataUnlockerRouteHandler(input);
+	if (dataUnlockerHandler) {
+		return dataUnlockerHandler;
+	}
+
+	const messageHandler = messageRouteHandler(input);
+	if (messageHandler) {
+		return messageHandler;
+	}
+
+	const communityPageHandler = communityPageRouteHandler(input);
+	if (communityPageHandler) {
+		return communityPageHandler;
+	}
+
+	const homePageHandler = homePageRouteHandler(input);
+	if (homePageHandler) {
+		return homePageHandler;
+	}
+
+	// Everything else gets rewritten to the main site
+	url.hostname = mainSiteHostName;
+	return NextResponse.rewrite(url);
 }
 // See "Matching Paths" below to learn more
 export const config = {
