3931046d2f428a062e5edea9347d6daebf4cd8ed
JoltCode <joe@joecc.dev>
2023-04-20 11:44:59 +0100

    feat(ui/getstarted): progress bar radix, style changes

--- packages/ui/src/components/pages/getStarted/GetStartedPage.tsx
+++ packages/ui/src/components/pages/getStarted/GetStartedPage.tsx
@@ -1,15 +1,45 @@
+import * as Progress from '@radix-ui/react-progress';
 import { AOHead, Footer, Navbar } from '~ui/components/primitives';
 import { GetStartedPageHandler } from './getStartedPage/GetStartedSection';
 import {
 	AllPageIndex,
 	getStartedPages,
 } from './getStartedPage/GetStartedSectionPages';
+import { useState } from 'react';
 
 export interface GetStartedPageProps {
 	initialPageIndex: AllPageIndex;
 }
 
+const GetStartedPageProgress = ({ progress }: { progress: number }) => {
+	return (
+		<Progress.Root
+			className="relative my-auto mb-4 h-8 w-full overflow-hidden rounded-full bg-black lg:mb-auto lg:mr-8 lg:h-96 lg:w-[25px]"
+			style={{
+				// Fix overflow clipping in Safari
+				// https://gist.github.com/domske/b66047671c780a238b51c51ffde8d3a0
+				transform: 'translateZ(0)',
+			}}
+			value={progress}
+		>
+			{/* Desktop */}
+			<Progress.Indicator
+				className="hidden h-full w-full bg-gradient-to-t from-ao-blue to-ao-red transition-transform duration-500 ease-[cubic-bezier(0.65,_0,_0.35,_1)] lg:block"
+				style={{ transform: `translateY(${100 - progress}%)` }}
+			/>
+
+			{/* Mobile */}
+			<Progress.Indicator
+				className="block h-full w-full bg-gradient-to-r from-ao-blue to-ao-red transition-transform duration-500 ease-[cubic-bezier(0.65,_0,_0.35,_1)] lg:hidden"
+				style={{ transform: `translateX(-${100 - progress}%)` }}
+			/>
+		</Progress.Root>
+	);
+};
+
 export const GetStartedPage = ({ initialPageIndex }: GetStartedPageProps) => {
+	const [progress, setProgress] = useState<number>(0);
+
 	return (
 		<div className="mx-auto flex min-h-screen w-full flex-col overflow-y-scroll bg-ao-white scrollbar-hide overflow-x-hidden dark:bg-ao-black">
 			<Navbar />
@@ -20,11 +50,12 @@ export const GetStartedPage = ({ initialPageIndex }: GetStartedPageProps) => {
 					path="/get-started"
 				/>
 
-				<div className="relative w-full">
-					<div className="absolute left-0 ml-32 h-full w-8 bg-black"></div>
-					<div className="mx-auto max-w-6xl gap-16 rounded-standard border-1 bg-ao-black/[0.01] p-16 drop-shadow-2xl dark:border-ao-white/25 dark:bg-ao-white/[.01]">
+				<div className="relative mx-auto flex flex-col lg:flex-row">
+					<GetStartedPageProgress progress={progress} />
+					<div className="min-h-[45vh] min-w-[50vw] max-w-6xl gap-16 rounded-standard border-1 bg-ao-black/[0.01] p-16 drop-shadow-2xl dark:border-ao-white/25 dark:bg-ao-white/[.01] lg:min-h-[75vh]">
 						<GetStartedPageHandler
 							pages={getStartedPages}
+							setProgress={setProgress}
 							initialPageIndex={initialPageIndex}
 						/>
 					</div>
--- packages/ui/src/components/pages/getStarted/getStartedPage/GetStartedSection.tsx
+++ packages/ui/src/components/pages/getStarted/getStartedPage/GetStartedSection.tsx
@@ -3,38 +3,42 @@ import type { AllPageIndex } from './GetStartedSectionPages';
 
 export interface GetStartedPageProps {
 	setPage: (pageIndex: AllPageIndex) => void;
+	setProgress: (progress: number) => void;
 }
 
 export interface GetStartedPage {
 	pageIndex: AllPageIndex;
-	component: React.FC<GetStartedPageProps>;
+	component: React.FC<GetStartedPageProps> | undefined;
 }
 
 export interface GetStartedPageHandlerProps {
 	pages: readonly GetStartedPage[];
 	initialPageIndex?: AllPageIndex;
+	setProgress: (progress: number) => void;
 }
 
 export const GetStartedPageHandler: React.FC<GetStartedPageHandlerProps> = ({
 	pages,
 	initialPageIndex,
+	setProgress,
 }) => {
 	const [currentPage, setCurrentPage] = useState(
 		initialPageIndex ?? 'introPage',
 	);
 
 	return (
-		<div className="py-8">
-			<div className="flex flex-col items-center justify-center">
-				{pages.map((page) => {
-					return page.pageIndex === currentPage ? (
-						<page.component
-							key={`get-started-modal-page-${page.pageIndex}`}
-							setPage={(pageIndex) => setCurrentPage(pageIndex)}
-						/>
-					) : null;
-				})}
-			</div>
+		<div className="flex h-full flex-col items-center justify-center">
+			{pages.map((page) => {
+				return page.pageIndex === currentPage
+					? page.component && (
+							<page.component
+								key={`get-started-modal-page-${page.pageIndex}`}
+								setPage={(pageIndex) => setCurrentPage(pageIndex)}
+								setProgress={(progress) => setProgress(progress)}
+							/>
+					  )
+					: null;
+			})}
 		</div>
 	);
 };
