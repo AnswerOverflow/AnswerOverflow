985aa64154a3940765831af4d52f22cf5573ce1c
JoltCode <joe@joecc.dev>
2023-05-03 11:42:23 +0100

    feat(storybook): bg pattern

--- packages/ui/src/utils/decorators.tsx
+++ packages/ui/src/utils/decorators.tsx
@@ -27,6 +27,8 @@ export function WithTailwindTheme(
 					flexDirection: 'center',
 					padding:
 						context.parameters.layout === 'fullscreen' ? 0 : '2rem 0 2rem',
+					backgroundImage:
+						"url(\"data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='100' height='199' viewBox='0 0 100 199'%3E%3Cg fill='%23bababa' fill-opacity='0.34'%3E%3Cpath d='M0 199V0h1v1.99L100 199h-1.12L1 4.22V199H0zM100 2h-.12l-1-2H100v2z'%3E%3C/path%3E%3C/g%3E%3C/svg%3E\")",
 				}}
 			/>
 		);
@@ -34,7 +36,7 @@ export function WithTailwindTheme(
 
 	const { tailwindTheme } = context.globals as Globals;
 	const Dark = () => (
-		<Flex className="dark bg-ao-black">
+		<Flex className="dark bg-black">
 			<ThemeProvider defaultTheme="dark">
 				<Story />
 			</ThemeProvider>
