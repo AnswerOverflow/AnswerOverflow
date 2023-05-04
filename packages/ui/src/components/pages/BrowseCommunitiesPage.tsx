ab9c9b68065b4a29e5768e950ab7a1971e87c566
JoltCode <joe@joecc.dev>
2023-05-04 15:47:38 +0100

    tweak(ui/pages): browse communities styling, change to using server card component

--- packages/ui/src/components/pages/BrowseCommunitiesPage.tsx
+++ packages/ui/src/components/pages/BrowseCommunitiesPage.tsx
@@ -1,38 +1,5 @@
 import type { ServerPublic } from '~api/router/server/types';
-import {
-	AOHead,
-	Button,
-	Footer,
-	Heading,
-	LinkButton,
-	LockIcon,
-	Navbar,
-	ServerIcon,
-} from '../primitives';
-
-const ServerArea = ({ server }: { server: ServerPublic }) => {
-	return (
-		<div className="flex flex-col rounded-standard bg-ao-black/5 px-8 py-4 dark:bg-ao-white/5">
-			<div className="flex flex-row items-center justify-between">
-				<ServerIcon server={server} />
-				{server.vanityUrl ? (
-					<LinkButton
-						href={`https://discord.gg/${server.vanityUrl}`}
-						variant="default"
-					>
-						Join Server
-					</LinkButton>
-				) : (
-					<Button disabled>
-						<LockIcon className="mr-1 h-6 w-6" />
-						Private Server
-					</Button>
-				)}
-			</div>
-			<Heading.H2 className="pt-4">{server.name}</Heading.H2>
-		</div>
-	);
-};
+import { AOHead, Footer, Heading, ServerCard, Navbar } from '../primitives';
 
 const MainContent = ({ servers }: { servers: ServerPublic[] }) => {
 	return (
@@ -41,7 +8,11 @@ const MainContent = ({ servers }: { servers: ServerPublic[] }) => {
 			<div className="grid grid-cols-4 gap-x-4 gap-y-8">
 				{servers.map((server) => {
 					return (
-						<ServerArea server={server} key={`server-${server.id}-area`} />
+						<ServerCard
+							server={server}
+							key={`server-${server.id}-area`}
+							type="join"
+						/>
 					);
 				})}
 			</div>
@@ -59,7 +30,7 @@ export const BrowseCommunitiesPage = ({
 	return (
 		<div className="mx-auto w-full overflow-x-hidden overflow-y-scroll bg-ao-white scrollbar-hide dark:bg-ao-black">
 			<Navbar />
-			<main className="bg-ao-white dark:bg-ao-black">
+			<main className="mb-24 mt-8 bg-ao-white dark:bg-ao-black">
 				<AOHead
 					title="Browse Communities On Answer Overflow"
 					description="Browse communities on Answer Overflow."
