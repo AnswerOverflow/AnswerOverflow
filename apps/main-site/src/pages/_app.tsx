5f7a5153cc0192d897b8e39a4912989965082871
Rhys Sullivan <39114868+RhysSullivan@users.noreply.github.com>
2023-05-04 04:21:52 -0400

    feat(web): add loading progress bar

--- apps/main-site/src/pages/_app.tsx
+++ apps/main-site/src/pages/_app.tsx
@@ -20,6 +20,19 @@ import { MDXProvider } from '@mdx-js/react';
 import Link from 'next/link';
 import type { Components } from '@mdx-js/react/lib';
 import { CommitBanner } from '@answeroverflow/ui/src/components/dev/CommitBanner';
+import ProgressBar from '@badrap/bar-of-progress';
+import Router from 'next/router';
+
+const progress = new ProgressBar({
+	size: 2,
+	color: '#0094ff',
+	className: 'bar-of-progress',
+	delay: 100,
+});
+
+Router.events.on('routeChangeStart', progress.start);
+Router.events.on('routeChangeComplete', progress.finish);
+Router.events.on('routeChangeError', progress.finish);
 
 const components: Components = {
 	h1: Heading.H1,
