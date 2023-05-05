a9d243084bae039b6fdeb751a5b913b1358691e6
JoltCode <joe@joecc.dev>
2023-05-05 12:21:45 +0100

    refactor(ui/pages): /browse move components around and add pageWrapper to story

--- packages/ui/src/components/pages/BrowseCommunitiesPage.stories.tsx
+++ packages/ui/src/components/pages/BrowseCommunitiesPage.stories.tsx
@@ -2,11 +2,17 @@ import type { Meta, StoryObj } from '@storybook/react';
 
 import { BrowseCommunitiesPage } from './BrowseCommunitiesPage';
 import { mockServer } from '~ui/test/props';
+import { PageWrapper } from './PageWrapper';
 const meta = {
 	component: BrowseCommunitiesPage,
 	parameters: {
 		layout: 'fullscreen',
 	},
+	render: (args) => (
+		<PageWrapper>
+			<BrowseCommunitiesPage {...args} />
+		</PageWrapper>
+	),
 } as Meta<typeof BrowseCommunitiesPage>;
 
 export default meta;
--- packages/ui/src/components/pages/BrowseCommunitiesPage.tsx
+++ packages/ui/src/components/pages/BrowseCommunitiesPage.tsx
@@ -1,10 +1,22 @@
 import type { ServerPublic } from '~api/router/server/types';
-import { AOHead, Footer, Heading, ServerCard, Navbar } from '../primitives';
+import { AOHead, Heading, ServerCard } from '../primitives';
 
-const MainContent = ({ servers }: { servers: ServerPublic[] }) => {
+interface BrowseCommunitiesPageProps {
+	servers: ServerPublic[];
+}
+
+export const BrowseCommunitiesPage = ({
+	servers,
+}: BrowseCommunitiesPageProps) => {
 	return (
-		<div className="px-4 2xl:px-[6rem]">
-			<Heading.H1 className="mb-16">Browse Communities</Heading.H1>
+		<>
+			<AOHead
+				title="Browse Communities On Answer Overflow"
+				description="Browse communities on Answer Overflow."
+				path="/communities"
+			/>
+
+			<Heading.H1 className="my-16">Browse Communities</Heading.H1>
 			<div className="grid grid-cols-4 gap-x-4 gap-y-8">
 				{servers.map((server) => {
 					return (
@@ -16,30 +28,6 @@ const MainContent = ({ servers }: { servers: ServerPublic[] }) => {
 					);
 				})}
 			</div>
-		</div>
-	);
-};
-
-interface BrowseCommunitiesPageProps {
-	servers: ServerPublic[];
-}
-
-export const BrowseCommunitiesPage = ({
-	servers,
-}: BrowseCommunitiesPageProps) => {
-	return (
-		<div className="mx-auto w-full overflow-x-hidden overflow-y-scroll bg-ao-white scrollbar-hide dark:bg-ao-black">
-			<Navbar />
-			<main className="mb-24 mt-8 bg-ao-white dark:bg-ao-black">
-				<AOHead
-					title="Browse Communities On Answer Overflow"
-					description="Browse communities on Answer Overflow."
-					path="/communities"
-				/>
-
-				<MainContent servers={servers} />
-			</main>
-			<Footer />
-		</div>
+		</>
 	);
 };
