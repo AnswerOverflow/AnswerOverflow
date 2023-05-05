9a7c209831c222c78d49b07f7b339d8e6e9a3c80
JoltCode <joe@joecc.dev>
2023-05-05 12:24:07 +0100

    remove(ui/icons): lock icon

--- packages/ui/src/components/primitives/base/Icons.tsx
+++ packages/ui/src/components/primitives/base/Icons.tsx
@@ -47,7 +47,7 @@ export function DiscordIcon(
 }
 
 import * as React from 'react';
-import { classNames, cn } from '~ui/utils/styling';
+import { classNames } from '~ui/utils/styling';
 
 export function AnswerOverflowLogo({
 	className,
@@ -172,25 +172,6 @@ export const CloseIcon = () => {
 				strokeLinecap="round"
 				strokeLinejoin="round"
 				d="M6 18L18 6M6 6l12 12"
-			/>
-		</svg>
-	);
-};
-
-export const LockIcon = ({ className }: { className?: string }) => {
-	return (
-		<svg
-			xmlns="http://www.w3.org/2000/svg"
-			fill="none"
-			viewBox="0 0 24 24"
-			strokeWidth={1.5}
-			stroke="currentColor"
-			className={cn('h-6 w-6', className)}
-		>
-			<path
-				strokeLinecap="round"
-				strokeLinejoin="round"
-				d="M16.5 10.5V6.75a4.5 4.5 0 10-9 0v3.75m-.75 11.25h10.5a2.25 2.25 0 002.25-2.25v-6.75a2.25 2.25 0 00-2.25-2.25H6.75a2.25 2.25 0 00-2.25 2.25v6.75a2.25 2.25 0 002.25 2.25z"
 			/>
 		</svg>
 	);
