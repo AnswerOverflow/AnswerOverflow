53989202e366bd672c2527fa448e915e2390b27a
JoltCode <joe@joecc.dev>
2023-05-06 13:13:53 +0100

    tweak(ui/primitives): command component logic, styling

--- packages/ui/src/components/primitives/base/Command.tsx
+++ packages/ui/src/components/primitives/base/Command.tsx
@@ -7,20 +7,18 @@ export const Command: React.FC<{
 	 * The command to display, excluding the leading slash.
 	 * @example "server-settings"
 	 */
-	command?: string;
+	command: string;
 }> = ({ command }) => {
 	const [, copy] = useCopyToClipboard();
 
 	return (
 		<div className="flex flex-col">
-			<div className="flex w-min flex-row items-center justify-center gap-5 rounded-standard border-1 border-[#32353A] bg-[#1F2226] px-4 py-2 text-black dark:text-white">
+			<div className="flex w-max flex-row items-center justify-center gap-5 rounded-standard border-1 border-[#CBCED1] bg-[#F4F4F4] px-4 py-2 text-black dark:border-[#32353A] dark:bg-[#1F2226] dark:text-white">
 				<span className="font-body text-lg">/{command}</span>
 				<button
-					className="rounded-standard bg-[#2A2E33] p-1 drop-shadow-2xl transition-colors hover:bg-[#363b42] md:active:translate-y-0.5"
-					onClick={() => {
-						if (!command) return;
-						copy(`/${command}`);
-					}}
+					className="rounded-standard border-2 border-[#d4d4d4] bg-[#F2F2F2] p-1 transition-colors hover:bg-[#e6e6e6] dark:border-0 dark:bg-[#2A2E33] dark:drop-shadow-2xl dark:hover:bg-[#363b42] md:active:translate-y-0.5"
+					onClick={() => copy(`/${command}`)}
+					aria-label={`Copy /${command} to clipboard`}
 				>
 					<CopyIcon className="h-6 w-6" />
 				</button>
