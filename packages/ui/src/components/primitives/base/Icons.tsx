675ba23228ed3d49325c934459e4e854d686e832
JoltCode <joe@joecc.dev>
2023-05-06 16:44:15 +0100

    remove(ui/icons): copy icon

--- packages/ui/src/components/primitives/base/Icons.tsx
+++ packages/ui/src/components/primitives/base/Icons.tsx
@@ -47,7 +47,7 @@ export function DiscordIcon(
 }
 
 import * as React from 'react';
-import { classNames, cn } from '~ui/utils/styling';
+import { classNames } from '~ui/utils/styling';
 
 export function AnswerOverflowLogo({
 	className,
@@ -134,25 +134,6 @@ export const ExternalLinkIcon = () => {
 				strokeLinecap="round"
 				strokeLinejoin="round"
 				d="M13.5 6H5.25A2.25 2.25 0 003 8.25v10.5A2.25 2.25 0 005.25 21h10.5A2.25 2.25 0 0018 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25"
-			/>
-		</svg>
-	);
-};
-
-export const CopyIcon = ({ className }: { className?: string }) => {
-	return (
-		<svg
-			xmlns="http://www.w3.org/2000/svg"
-			fill="none"
-			viewBox="0 0 24 24"
-			strokeWidth={1.5}
-			stroke="currentColor"
-			className={cn('h-4 w-4', className)}
-		>
-			<path
-				strokeLinecap="round"
-				strokeLinejoin="round"
-				d="M15.666 3.888A2.25 2.25 0 0013.5 2.25h-3c-1.03 0-1.9.693-2.166 1.638m7.332 0c.055.194.084.4.084.612v0a.75.75 0 01-.75.75H9a.75.75 0 01-.75-.75v0c0-.212.03-.418.084-.612m7.332 0c.646.049 1.288.11 1.927.184 1.1.128 1.907 1.077 1.907 2.185V19.5a2.25 2.25 0 01-2.25 2.25H6.75A2.25 2.25 0 014.5 19.5V6.257c0-1.108.806-2.057 1.907-2.185a48.208 48.208 0 011.927-.184"
 			/>
 		</svg>
 	);
