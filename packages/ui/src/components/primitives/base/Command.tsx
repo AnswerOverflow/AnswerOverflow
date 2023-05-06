40433937ca2358cfc99bf13deb164dcad5aeb2a7
JoltCode <joe@joecc.dev>
2023-05-06 16:45:20 +0100

    tweak(ui/command): refactor

--- packages/ui/src/components/primitives/base/Command.tsx
+++ packages/ui/src/components/primitives/base/Command.tsx
@@ -1,7 +1,5 @@
 import type React from 'react';
-import { CopyIcon } from './Icons';
-import { useCopyToClipboard } from 'react-use';
-import { Button } from './Button';
+import { CopyButton } from './CopyButton';
 
 export const Command: React.FC<{
 	/**
@@ -10,20 +8,11 @@ export const Command: React.FC<{
 	 */
 	command: string;
 }> = ({ command }) => {
-	const [, copy] = useCopyToClipboard();
-
 	return (
 		<div className="flex flex-col">
-			<div className="flex w-max flex-row items-center justify-center gap-5 rounded-standard border-1 border-[#CBCED1] bg-[#F4F4F4] px-4 py-2 text-black dark:border-[#32353A] dark:bg-[#1F2226] dark:text-white">
+			<div className="flex w-max flex-row items-center justify-center rounded-standard border-1 border-[#CBCED1] bg-[#F4F4F4] px-4 py-2 text-black dark:border-[#32353A] dark:bg-[#1F2226] dark:text-white">
 				<span className="font-body text-lg">/{command}</span>
-				<Button
-					className="h-auto rounded-standard border-2 border-[#d4d4d4] bg-[#F2F2F2] p-1 transition-colors hover:bg-[#e6e6e6] dark:border-0 dark:bg-[#2A2E33] dark:drop-shadow-2xl dark:hover:bg-[#363b42]"
-					onClick={() => copy(`/${command}`)}
-					aria-label={`Copy /${command} to clipboard`}
-					variant="ghost"
-				>
-					<CopyIcon className="h-6 w-6" />
-				</Button>
+				<CopyButton textToCopy={`/${command}`} />
 			</div>
 		</div>
 	);
