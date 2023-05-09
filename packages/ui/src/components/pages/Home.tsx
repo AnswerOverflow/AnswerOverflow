985dde081a236a0400939828a09d53cd35f948ac
JoltCode <joe@joecc.dev>
2023-05-09 11:05:45 +0100

    fix(ui/all): font-body as standard

--- packages/ui/src/components/pages/Home.tsx
+++ packages/ui/src/components/pages/Home.tsx
@@ -28,7 +28,7 @@ export const Home = () => {
 		});
 
 	return (
-		<div className="flex w-full flex-col items-center bg-ao-white dark:bg-[linear-gradient(180.49deg,_#1A1818_-12.07%,_#0E0D0D_-12.07%,_#040405_-12.06%,_#101214_103.52%)]">
+		<div className="flex w-full flex-col items-center bg-ao-white font-body dark:bg-[linear-gradient(180.49deg,_#1A1818_-12.07%,_#0E0D0D_-12.07%,_#040405_-12.06%,_#101214_103.52%)]">
 			<div className="max-w-screen-3xl">
 				<div className="relative ">
 					<Navbar />
--- packages/ui/src/components/primitives/base/Button.tsx
+++ packages/ui/src/components/primitives/base/Button.tsx
@@ -4,7 +4,7 @@ import { type VariantProps, cva } from 'cva';
 import { cn } from '~ui/utils/styling';
 
 const buttonVariants = cva(
-	'active:scale-95 inline-flex items-center justify-center rounded-md text-sm font-medium transition-colors focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 dark:hover:bg-slate-800 dark:hover:text-slate-100 disabled:opacity-50 dark:focus:ring-slate-400 disabled:pointer-events-none dark:focus:ring-offset-slate-900 data-[state=open]:bg-slate-100 dark:data-[state=open]:bg-slate-800',
+	'active:scale-95 inline-flex items-center justify-center rounded-md text-sm font-medium transition-colors focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 dark:hover:bg-slate-800 dark:hover:text-slate-100 disabled:opacity-50 dark:focus:ring-slate-400 disabled:pointer-events-none dark:focus:ring-offset-slate-900 data-[state=open]:bg-slate-100 dark:data-[state=open]:bg-slate-800 font-body',
 	{
 		variants: {
 			variant: {
