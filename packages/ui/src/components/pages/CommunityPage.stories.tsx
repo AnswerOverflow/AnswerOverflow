ef637b661ee5c6883a75f167c6a409fb6d8433f8
JoltCode <joe@joecc.dev>
2023-05-18 18:33:16 +0100

    feat(ui/stories): community page randomly generate long messages

--- packages/ui/src/components/pages/CommunityPage.stories.tsx
+++ packages/ui/src/components/pages/CommunityPage.stories.tsx
@@ -1,4 +1,5 @@
 import type { Meta, StoryObj } from '@storybook/react';
+import { loremIpsum } from 'lorem-ipsum';
 import {
 	mockChannelWithSettings,
 	mockMessageFull,
@@ -19,28 +20,55 @@ export default meta;
 
 type Story = StoryObj<typeof meta>;
 
-function makeMockedChannelQuestions(amount: number) {
+function makeMockedChannelQuestions(
+	amount: number,
+	someLongMessages?: boolean,
+) {
 	if (amount > 20) {
 		amount = 20;
 	}
-	return Array.from({ length: amount }).map(() => ({
-		message: mockMessageFull(),
-		thread: mockChannelWithSettings({
-			type: ChannelType.PublicThread,
-		}),
-	}));
+	return Array.from({ length: amount }).map(() => {
+		const randomBoolean = Math.random() >= 0.5;
+
+		if (!randomBoolean || !someLongMessages) {
+			return {
+				message: mockMessageFull(),
+				thread: mockChannelWithSettings({
+					type: ChannelType.PublicThread,
+				}),
+			};
+		}
+
+		return {
+			message: mockMessageFull({
+				content: loremIpsum({
+					count: 3500,
+				}),
+			}),
+			thread: mockChannelWithSettings({
+				type: ChannelType.PublicThread,
+			}),
+		};
+	});
 }
 
-function makeMockedChannels(amount: number) {
+function makeMockedChannels(amount: number, someLongMessages?: boolean) {
 	return Array.from({ length: amount }).map(() => ({
 		channel: mockChannelWithSettings(),
-		questions: makeMockedChannelQuestions(100),
+		questions: makeMockedChannelQuestions(100, someLongMessages),
 	}));
 }
 
 export const CommunityPageStory: Story = {
 	args: {
 		channels: makeMockedChannels(100),
+		server: mockServer(),
+	},
+};
+
+export const CommunityPageStoryWithSomeSuperLongQuestions: Story = {
+	args: {
+		channels: makeMockedChannels(100, true),
 		server: mockServer(),
 	},
 };
