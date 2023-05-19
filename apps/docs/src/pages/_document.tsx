46daf33afa42f93d423c00504391a10f7dd8571e
JoltCode <joe@joecc.dev>
2023-05-19 09:53:54 +0100

    chore(ui,docs,main-site): remove poppins font

--- apps/docs/src/pages/_document.tsx
+++ apps/docs/src/pages/_document.tsx
@@ -19,12 +19,6 @@ class MyDocument extends Document {
 	override render() {
 		return (
 			<Html className="dark">
-				<Head>
-					<link
-						href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;600&display=swap"
-						rel="stylesheet"
-					/>
-				</Head>
 				<Main />
 				<NextScript />
 			</Html>
