f1cf3147ed3f46db71987552d8bd0f6dfc5b42cf
JoltCode <joe@joecc.dev>
2023-05-22 09:29:45 +0100

    tweak(ui/stories): message collapse content bool to int

--- packages/ui/src/components/primitives/Message.stories.tsx
+++ packages/ui/src/components/primitives/Message.stories.tsx
@@ -66,7 +66,7 @@ export const SuperLongMessage: Story = {
 export const SuperLongMessageCollapsed: Story = {
 	args: {
 		...Primary.args,
-		collapseContent: true,
+		collapseContent: 3500,
 		message: {
 			...mockMessageWithDiscordAccount(),
 			// (3674 characters - discord nitro is max 4000 characters)
