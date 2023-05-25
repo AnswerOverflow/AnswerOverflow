8dc1272b29ee1b9c785bcb333ba2b3eced4fc932
Rhys Sullivan <39114868+RhysSullivan@users.noreply.github.com>
2023-05-25 08:31:47 +0000

    fix(db): adjust where filtering of indexed channels happens for search

--- packages/db/src/message.ts
+++ packages/db/src/message.ts
@@ -368,9 +368,7 @@ export async function searchMessages(opts: MessageSearchOptions) {
 		addReferencesToMessages(messages),
 	]);
 	const messagesWithAuthors = await addAuthorsToMessages(messagesWithRefs);
-	const channelLookup = new Map(
-		channels.filter((x) => x.flags.indexingEnabled).map((c) => [c.id, c]),
-	);
+	const channelLookup = new Map(channels.map((c) => [c.id, c]));
 	const serverLookup = new Map(
 		servers.filter((x) => x.kickedTime === null).map((s) => [s.id, s]),
 	);
@@ -383,6 +381,9 @@ export async function searchMessages(opts: MessageSearchOptions) {
 				? channelLookup.get(m.channelId)
 				: undefined;
 			if (!channel || !server) return null;
+			if (!channel.flags.indexingEnabled) {
+				return null;
+			}
 			return {
 				message: m,
 				channel,
