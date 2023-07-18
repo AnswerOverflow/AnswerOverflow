df8d01b3feae21a4fb48f54abb96543ef8dcc626
Rhys Sullivan <39114868+RhysSullivan@users.noreply.github.com>
2023-07-18 08:39:10 -0700

    feat(web): display progress on page views card

--- packages/ui/src/components/dashboard/cards.tsx
+++ packages/ui/src/components/dashboard/cards.tsx
@@ -1,4 +1,12 @@
-import { Card, Flex, LineChart, Metric, Title, Text } from '@tremor/react';
+import {
+	Card,
+	Flex,
+	LineChart,
+	Metric,
+	Title,
+	Text,
+	ProgressBar,
+} from '@tremor/react';
 import type { Plan } from '@answeroverflow/db';
 import { trpc } from '~ui/utils/trpc';
 import { AOLink } from '../primitives';
@@ -8,33 +16,57 @@ import { PricingDialog } from '~ui/components/pages';
 export function PageViewsCardRenderer(props: {
 	numberOfPageViews?: number;
 	status: 'success' | 'loading' | 'error';
+	plan: Plan;
 }) {
-	const limit = 100000;
+	let limit: number | undefined = undefined;
+	switch (props.plan) {
+		case 'FREE':
+			limit = 50000;
+			break;
+		case 'PRO':
+			limit = 100000;
+			break;
+		case 'OPEN_SOURCE':
+			limit = undefined;
+			break;
+		case 'ENTERPRISE':
+			limit = 500000;
+			break;
+	}
 	// eslint-disable-next-line @typescript-eslint/no-unused-vars
-	const progress = (props.numberOfPageViews ?? 0) / limit;
+	const AmountUsed = () => {
+		if (!limit) return null;
+		const progress = (props.numberOfPageViews ?? 0) / limit;
+		return (
+			<>
+				<Flex className="mt-4">
+					<Text className="truncate">{`${(progress * 100).toFixed(2)}%`}</Text>
+					<Text>{limit.toLocaleString('en-US')} Limit</Text>
+				</Flex>
+				<ProgressBar value={progress * 100} className="mt-2" />
+			</>
+		);
+	};
 	return (
 		<Card className="mx-auto">
 			<Flex alignItems="start">
 				<div>
 					<Title>Page Views</Title>
-					<Metric>{props.numberOfPageViews ?? ''}</Metric>
+					<Metric>{props.numberOfPageViews?.toLocaleString() ?? ''}</Metric>
 				</div>
 			</Flex>
-			{/* <Flex className="mt-4">
-				<Text className="truncate">{`${(progress * 100).toFixed(2)}%`}</Text>
-				<Text>{limit.toLocaleString('en-US')} Limit</Text>
-			</Flex>
-			<ProgressBar value={progress} className="mt-2" /> */}
+			<AmountUsed />
 		</Card>
 	);
 }
 
 export function PageViewsCard() {
-	const { id } = useDashboardContext();
+	const { id, plan } = useDashboardContext();
 	const { data, status } = trpc.servers.fetchPageViewsAsLineChart.useQuery(id);
 
 	return (
 		<PageViewsCardRenderer
+			plan={plan}
 			numberOfPageViews={
 				status === 'success'
 					? data.reduce((acc, cur) => acc + cur['View Count'], 0)
