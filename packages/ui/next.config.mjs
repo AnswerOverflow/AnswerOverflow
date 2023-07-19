5b1c1c95c65da582864f1759e7e3efd1eef8d3c6
Rhys Sullivan <39114868+RhysSullivan@users.noreply.github.com>
2023-07-18 23:36:57 -0700

    Revert "chore(ui): remove unused next files"
    This reverts commit 6dabd2cb1d1578405ce1c603fcef61610c904525.

---
+++ packages/ui/next.config.mjs
@@ -0,0 +1,16 @@
+// @ts-check
+/**
+ * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation.
+ * This is especially useful for Docker builds.
+ */
+
+/** @type {import("next").NextConfig} */
+const config = {
+	reactStrictMode: true,
+	swcMinify: true,
+	i18n: {
+		locales: ['en'],
+		defaultLocale: 'en',
+	},
+};
+export default config;
