e1b971cf3f6460bc8f18b5e06aab9228aa969b74
Rhys Sullivan <39114868+RhysSullivan@users.noreply.github.com>
2023-07-19 00:54:04 -0700

    fix(bot): refine variables better

--- packages/env/bot.ts
+++ packages/env/bot.ts
@@ -15,48 +15,85 @@ export const botEnv = {
 				.string()
 				.optional()
 				.default('false')
-				.refine((value) => {
-					if (value === 'true' || value === 'false') {
-						return true;
+				.transform((s) => {
+					if (s.length === 0) {
+						return 'false';
 					}
-					return 'Must be either "true" or "false"';
+					return s;
 				})
-				.pipe(z.boolean())
-				.optional(),
+				.refine(
+					(value) => {
+						return value === 'true' || value === 'false';
+					},
+					{
+						message: 'Must be either "true" or "false"',
+					},
+				)
+				.transform((s) => s === 'true')
+				.pipe(z.boolean()),
 			STATUS_UPDATE_INTERVAL_IN_HOURS: z
 				.string()
 				.optional()
 				.default('1')
-				.transform((s) => parseFloat(s))
+				.transform((s) => {
+					if (s.length === 0) {
+						return parseFloat('1');
+					}
+					return parseFloat(s);
+				})
 				.pipe(z.number()),
 			INDEXING_DISABLED: z
 				.string()
+				.optional()
 				.default('false')
-				.refine((value) => {
-					if (value === 'true' || value === 'false') {
-						return true;
+				.transform((s) => {
+					if (s?.length === 0) {
+						return 'false';
 					}
-					return 'Must be either "true" or "false"';
+					return s;
 				})
-				.pipe(z.boolean())
-				.optional(),
+				.refine(
+					(value) => {
+						return value === 'true' || value === 'false';
+					},
+					{
+						message: 'Must be either "true" or "false"',
+					},
+				)
+				.transform((s) => s === 'true')
+				.pipe(z.boolean()),
 			INDEXING_INTERVAL_IN_HOURS: z
 				.string()
 				.optional()
 				.default('6')
-				.transform((s) => parseFloat(s))
+				.transform((s) => {
+					if (s.length === 0) {
+						return parseFloat('6');
+					}
+					return parseFloat(s);
+				})
 				.pipe(z.number()),
 			MAX_NUMBER_OF_THREADS_TO_COLLECT: z
 				.string()
 				.optional()
 				.default('5000')
-				.transform((s) => parseInt(s, 10))
+				.transform((s) => {
+					if (s.length === 0) {
+						return parseInt('5000', 10);
+					}
+					return parseInt(s, 10);
+				})
 				.pipe(z.number()),
 			MAX_NUMBER_OF_MESSAGES_TO_COLLECT: z
 				.string()
 				.optional()
 				.default('20000')
-				.transform((s) => parseInt(s, 10))
+				.transform((s) => {
+					if (s.length === 0) {
+						return parseInt('25000', 10);
+					}
+					return parseInt(s, 10);
+				})
 				.pipe(z.number()),
 		},
 		experimental__runtimeEnv: process.env,
