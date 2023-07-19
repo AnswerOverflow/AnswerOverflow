49354190383962ce2e15f09d55ed9e3457cce602
Rhys Sullivan <39114868+RhysSullivan@users.noreply.github.com>
2023-07-18 22:48:04 -0700

    Update next.config.mjs

--- apps/main-site/next.config.mjs
+++ apps/main-site/next.config.mjs
@@ -5,7 +5,6 @@
  */
 
 !process.env.SKIP_ENV_VALIDATION &&
-	// @ts-expect-error
 	(await import('@answeroverflow/env/web-schema.mjs'));
 const nextJSMDX = await import('@next/mdx');
 import remarkGfm from 'remark-gfm';
