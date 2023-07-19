8a605bb8209de8e0b6f9e895098eff7f1add6069
Rhys Sullivan <39114868+RhysSullivan@users.noreply.github.com>
2023-07-19 09:23:40 -0700

    fix(web): use NEXT_PUBLIC_NODE_ENV

--- packages/ui/src/utils/server.ts
+++ packages/ui/src/utils/server.ts
@@ -1,10 +1,10 @@
 import type { ServerPublic } from '@answeroverflow/api';
-import { sharedEnvs } from '@answeroverflow/env/shared';
+import { webClientEnv } from '@answeroverflow/env/web';
 export function getServerHomepageUrl(server: ServerPublic) {
 	if (!server.customDomain) {
 		return `/c/${server.id}`;
 	}
-	return `http${sharedEnvs.NODE_ENV === 'production' ? 's' : ''}://${
-		server.customDomain
-	}`;
+	return `http${
+		webClientEnv.NEXT_PUBLIC_NODE_ENV === 'production' ? 's' : ''
+	}://${server.customDomain}`;
 }
