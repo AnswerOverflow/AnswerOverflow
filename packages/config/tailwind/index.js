fb55267b206b83aac32d8816eb0c0aabc113cd20
Rhys Sullivan <39114868+RhysSullivan@users.noreply.github.com>
2023-07-26 20:28:35 -0700

    wip

--- packages/config/tailwind/index.js
+++ packages/config/tailwind/index.js
@@ -14,10 +14,18 @@ module.exports = {
 		// eslint-disable-next-line no-undef
 		require('tailwind-scrollbar-hide'),
 		require('@headlessui/tailwindcss'),
+		require('tailwindcss-animate'),
 	],
 	theme: {
 		transparent: 'transparent',
 		current: 'currentColor',
+		container: {
+			center: true,
+			padding: '2rem',
+			screens: {
+				'2xl': '1400px',
+			},
+		},
 		extend: {
 			fontFamily: {
 				discord: ['Noto Sans', 'sans-serif'],
@@ -62,7 +70,6 @@ module.exports = {
 						inverted: '#ffffff', // white
 					},
 				},
-				// dark mode
 				'dark-tremor': {
 					brand: {
 						faint: '#0B1229', // custom
@@ -92,6 +99,53 @@ module.exports = {
 						inverted: '#000000', // black
 					},
 				},
+				border: 'hsl(var(--border))',
+				input: 'hsl(var(--input))',
+				ring: 'hsl(var(--ring))',
+				background: 'hsl(var(--background))',
+				foreground: 'hsl(var(--foreground))',
+				primary: {
+					DEFAULT: 'hsl(var(--primary))',
+					foreground: 'hsl(var(--primary-foreground))',
+				},
+				secondary: {
+					DEFAULT: 'hsl(var(--secondary))',
+					foreground: 'hsl(var(--secondary-foreground))',
+				},
+				destructive: {
+					DEFAULT: 'hsl(var(--destructive))',
+					foreground: 'hsl(var(--destructive-foreground))',
+				},
+				muted: {
+					DEFAULT: 'hsl(var(--muted))',
+					foreground: 'hsl(var(--muted-foreground))',
+				},
+				accent: {
+					DEFAULT: 'hsl(var(--accent))',
+					foreground: 'hsl(var(--accent-foreground))',
+				},
+				popover: {
+					DEFAULT: 'hsl(var(--popover))',
+					foreground: 'hsl(var(--popover-foreground))',
+				},
+				card: {
+					DEFAULT: 'hsl(var(--card))',
+					foreground: 'hsl(var(--card-foreground))',
+				},
+			},
+			keyframes: {
+				'accordion-down': {
+					from: { height: 0 },
+					to: { height: 'var(--radix-accordion-content-height)' },
+				},
+				'accordion-up': {
+					from: { height: 'var(--radix-accordion-content-height)' },
+					to: { height: 0 },
+				},
+			},
+			animation: {
+				'accordion-down': 'accordion-down 0.2s ease-out',
+				'accordion-up': 'accordion-up 0.2s ease-out',
 			},
 			boxShadow: {
 				// light
@@ -112,6 +166,9 @@ module.exports = {
 				'tremor-default': '0.5rem',
 				'tremor-full': '9999px',
 				standard: '5px',
+				lg: 'var(--radius)',
+				md: 'calc(var(--radius) - 2px)',
+				sm: 'calc(var(--radius) - 4px)',
 			},
 			fontSize: {
 				'tremor-label': ['0.75rem'],
