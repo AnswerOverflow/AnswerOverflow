02a3c424f9e023998257767c0178b0c7b8faf7e3
Rhys Sullivan <39114868+RhysSullivan@users.noreply.github.com>
2023-07-28 00:31:04 -0700

    fix(web): block robots.txt from next routes

--- apps/main-site/src/pages/[domain]/robots.txt.ts
+++ apps/main-site/src/pages/[domain]/robots.txt.ts
@@ -5,6 +5,16 @@ export function getServerSideProps({ res, params }: GetServerSidePropsContext) {
 	res.setHeader('content-type', 'text/plain');
 	res.write(`User-agent: *
 Allow: /
+Disallow: /api/
+Disallow: /dashboard/
+Disallow: /oemf7z50uh7w/
+Disallow: /*.json$
+Disallow: /*_buildManifest.js$
+Disallow: /*_middlewareManifest.js$
+Disallow: /*_ssgManifest.js$
+Disallow: /*.js$
+Sitemap: https://www.answeroverflow.com/sitemap.xml
+
 Sitemap: https://${domain}/sitemap.xml
 `);
 	res.end();
