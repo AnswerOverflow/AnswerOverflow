6c9d45e0aa28727a765747a763d4fae7e0352a46
Rhys Sullivan <39114868+RhysSullivan@users.noreply.github.com>
2023-08-01 11:13:14 -0700

    wip

--- apps/main-site/src/pages/api/og/message.tsx
+++ apps/main-site/src/pages/api/og/message.tsx
@@ -6,6 +6,7 @@ import { sharedEnvs } from '@answeroverflow/env/shared';
 import { findMessageResultPage } from '@answeroverflow/db';
 import { z } from 'zod';
 import { getSnowflakeUTCDate } from '@answeroverflow/ui/src/utils/snowflake';
+import { ServerPublic } from '~api/router/server/types';
 
 const currentPath = process.cwd();
 const satoshiBLack = fs.readFileSync(
@@ -18,7 +19,13 @@ const satoshiBold = fs.readFileSync(
 function truncate(str: string, n: number = 30) {
 	return str.length > n ? str.slice(0, n - 1) + '...' : str;
 }
-
+const makeServerIconLink = (
+	server: Pick<ServerPublic, 'id' | 'icon'>,
+	size: number = 64,
+) => {
+	if (!server.icon) return undefined;
+	return `https://cdn.discordapp.com/icons/${server.id}/${server.icon}.png?size=${size}`;
+};
 export default async function handler(
 	req: NextApiRequest,
 	res: NextApiResponse,
@@ -42,6 +49,105 @@ export default async function handler(
 	const isSolved = rootMessage.solutionIds.length > 0;
 	const numReplies = messages.length - 1;
 	const date = getSnowflakeUTCDate(rootMessage.id);
+	const icon = makeServerIconLink(server);
+
+	const ServerIcon = () => {
+		if (icon) {
+			return (
+				<img
+					src={icon}
+					alt="Server Icon"
+					style={{
+						width: '80px',
+						height: '80px',
+						position: 'absolute',
+						top: '40px',
+						right: '40px',
+						borderRadius: '50%',
+						border: '1px solid #8c8c8c',
+					}}
+				/>
+			);
+		}
+		return null;
+	};
+
+	const Header = () => (
+		<div
+			style={{
+				width: '100%',
+
+				justifyContent: 'flex-start',
+				alignItems: 'center',
+				display: 'flex',
+			}}
+		>
+			<h1>{truncate(server.name)}</h1>
+			<div
+				style={{
+					height: '32px',
+					transform: 'rotate(30deg)',
+					borderLeftWidth: '2px',
+					marginRight: '20px',
+					marginLeft: '20px',
+					borderLeftColor: '#8c8c8c',
+				}}
+			/>
+
+			<h2>{truncate(channel.name)}</h2>
+		</div>
+	);
+
+	const Body = () => (
+		<div
+			style={{
+				width: '100%',
+				display: 'flex',
+				flexDirection: 'column',
+			}}
+		>
+			<p
+				style={{
+					fontSize: '50px',
+					fontFamily: 'Satoshi Bold',
+					color: 'black',
+					opacity: 0.8,
+					marginTop: '0px',
+					width: '100%',
+					textAlign: 'left',
+				}}
+			>
+				{truncate(title, 200)}
+			</p>
+			<div
+				style={{
+					display: 'flex',
+					flexDirection: 'row',
+					alignItems: 'center',
+					justifyContent: 'space-between',
+				}}
+			>
+				{isSolved && (
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						width="24"
+						height="24"
+						viewBox="0 0 24 24"
+						fill="none"
+						stroke="currentColor"
+						stroke-width="2"
+						stroke-linecap="round"
+						stroke-linejoin="round"
+						className="lucide lucide-check-circle-2"
+					>
+						<path d="M12 22c5.523 0 10-4.477 10-10S17.523 2 12 2 2 6.477 2 12s4.477 10 10 10z" />
+						<path d="m9 12 2 2 4-4" />
+					</svg>
+				)}
+			</div>
+		</div>
+	);
+
 	const svg = await satori(
 		<div
 			style={{
@@ -56,57 +162,9 @@ export default async function handler(
 			}}
 		>
 			{/* Header */}
-			<div
-				style={{
-					width: '100%',
-
-					justifyContent: 'flex-start',
-					alignItems: 'center',
-					display: 'flex',
-				}}
-			>
-				<h1>{truncate(server.name)}</h1>
-				<div
-					style={{
-						height: '32px',
-						transform: 'rotate(30deg)',
-						borderLeftWidth: '2px',
-						marginRight: '20px',
-						marginLeft: '20px',
-						borderLeftColor: '#8c8c8c',
-					}}
-				/>
-
-				<h2>{truncate(channel.name)}</h2>
-			</div>
-			{/* eslint-disable-next-line @next/next/no-img-element */}
-			<img
-				src={
-					'https://cdn.discordapp.com/icons/222078108977594368/1ad76bddd2af468c31fdb10cbce63d74.png?size=64'
-				}
-				alt="Server Icon"
-				style={{
-					width: '80px',
-					height: '80px',
-					position: 'absolute',
-					top: '40px',
-					right: '40px',
-				}}
-			/>
-
-			<p
-				style={{
-					fontSize: '50px',
-					fontFamily: 'Satoshi Bold',
-					color: 'black',
-					opacity: 0.8,
-					marginTop: '0px',
-					width: '100%',
-					textAlign: 'left',
-				}}
-			>
-				{truncate(title, 200)}
-			</p>
+			<Header />
+			<ServerIcon />
+			<Body />
 			<div
 				style={{
 					height: '100%',
