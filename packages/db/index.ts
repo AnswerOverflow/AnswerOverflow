bc3deffb5384bf7b4fe407b46e05068048433e5b
JoltCode <joe@joecc.dev>
2023-08-31 17:44:21 +0100

    feat(db): provide schema to drizzle

--- packages/db/index.ts
+++ packages/db/index.ts
@@ -1,9 +1,12 @@
 import { drizzle } from 'drizzle-orm/planetscale-serverless';
 import { connect } from '@planetscale/database';
 import { sharedEnvs } from '@answeroverflow/env/shared';
+import * as schema from './src/schema';
 
 const connection = connect({
 	url: sharedEnvs.DATABASE_URL,
 });
 
-export const db = drizzle(connection);
+export const db = drizzle(connection, {
+	schema,
+});
