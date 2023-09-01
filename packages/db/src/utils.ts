45498f02cc0c16e9cedc93ced8b6a409e891ec1b
JoltCode <joe@joecc.dev>
2023-09-01 13:37:56 +0100

    feat(db/utils): migrate wipe database function prisma -> drizzle

--- packages/db/src/utils.ts
+++ packages/db/src/utils.ts
@@ -1,6 +1,18 @@
-import { prisma } from '@answeroverflow/prisma-types';
 import { elastic } from '@answeroverflow/elastic-types';
 import { sharedEnvs } from '@answeroverflow/env/shared';
+import { db } from '../index';
+import {
+	accounts,
+	channels,
+	discordAccounts,
+	ignoredDiscordAccounts,
+	servers,
+	sessions,
+	tenantSessions,
+	users,
+	userServerSettings,
+} from './schema';
+import { isNotNull } from 'drizzle-orm';
 export async function clearDatabase() {
 	if (sharedEnvs.NODE_ENV !== 'test') {
 		throw new Error('clearDatabase can only be used in test environment');
@@ -15,22 +27,16 @@ export async function clearDatabase() {
 
 	console.log('Wiping database...');
 
-	await prisma.userServerSettings.deleteMany({});
-	await prisma.channel.deleteMany({
-		where: {
-			parentId: {
-				not: null,
-			},
-		},
-	});
-	await prisma.tenantSession.deleteMany({});
-	await prisma.channel.deleteMany({});
-	await prisma.server.deleteMany({});
-	await prisma.account.deleteMany({});
-	await prisma.discordAccount.deleteMany({});
-	await prisma.session.deleteMany({});
-	await prisma.user.deleteMany({});
-	await prisma.ignoredDiscordAccount.deleteMany({});
+	await db.delete(userServerSettings);
+	await db.delete(channels).where(isNotNull(channels.parentId));
+	await db.delete(tenantSessions);
+	await db.delete(channels);
+	await db.delete(servers);
+	await db.delete(accounts);
+	await db.delete(discordAccounts);
+	await db.delete(sessions);
+	await db.delete(users);
+	await db.delete(ignoredDiscordAccounts);
 	await elastic.createMessagesIndex();
 	console.log('Database wiped successfully');
 }
