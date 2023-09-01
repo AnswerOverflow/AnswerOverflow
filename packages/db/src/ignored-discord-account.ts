3f6328eb0307e979ffb46b57018011fcfdc7cec6
JoltCode <joe@joecc.dev>
2023-09-01 12:53:07 +0100

    feat(db/ignoredDiscordAccount): migrate prisma -> drizzle

--- packages/db/src/ignored-discord-account.ts
+++ packages/db/src/ignored-discord-account.ts
@@ -1,16 +1,19 @@
 import { prisma } from '@answeroverflow/prisma-types';
 import { findDiscordAccountById } from './discord-account';
 import { DBError } from './utils/error';
+import { db } from '../index';
+import { ignoredDiscordAccounts } from './schema';
+import { eq, inArray } from 'drizzle-orm';
 
 export function findIgnoredDiscordAccountById(id: string) {
-	return prisma.ignoredDiscordAccount.findUnique({
-		where: { id },
+	return db.query.ignoredDiscordAccounts.findFirst({
+		where: eq(ignoredDiscordAccounts.id, id),
 	});
 }
 
 export function findManyIgnoredDiscordAccountsById(ids: string[]) {
-	return prisma.ignoredDiscordAccount.findMany({
-		where: { id: { in: ids } },
+	return db.query.ignoredDiscordAccounts.findMany({
+		where: inArray(ignoredDiscordAccounts.id, ids),
 	});
 }
 
@@ -18,19 +21,31 @@ export async function upsertIgnoredDiscordAccount(id: string) {
 	const discordAccount = await findDiscordAccountById(id);
 	if (discordAccount)
 		throw new DBError('Account is not ignored', 'NOT_IGNORED_ACCOUNT');
-	return prisma.ignoredDiscordAccount.upsert({
-		where: { id },
-		create: {
-			id,
-		},
-		update: {
+	await db.insert(ignoredDiscordAccounts).values({ id }).onDuplicateKeyUpdate({
+		set: {
 			id,
 		},
 	});
+	const upserted = await db.query.ignoredDiscordAccounts.findFirst({
+		where: eq(ignoredDiscordAccounts.id, id),
+	});
+
+	if (!upserted) throw new Error('Failed to upsert account');
+	return upserted;
 }
 
 export async function deleteIgnoredDiscordAccount(id: string) {
-	return prisma.ignoredDiscordAccount.delete({
-		where: { id },
+	await db
+		.delete(ignoredDiscordAccounts)
+		.where(eq(ignoredDiscordAccounts.id, id));
+
+	const deleted = await db.query.ignoredDiscordAccounts.findFirst({
+		where: eq(ignoredDiscordAccounts.id, id),
 	});
+
+	if (deleted) throw new Error('Failed to delete account');
+
+	return {
+		id,
+	};
 }
