d3535090ad9fbf59d95d3c340dc492868de24d27
Rhys Sullivan <39114868+RhysSullivan@users.noreply.github.com>
2023-09-09 14:03:23 -0700

    create db on container startup

--- packages/db/src/db.ts
+++ packages/db/src/db.ts
@@ -11,14 +11,16 @@ import { drizzle as mysqlDrizzle } from 'drizzle-orm/mysql2';
 import mysql from 'mysql2/promise';
 
 import * as schema from './schema';
-const connection = connect({
-	url: sharedEnvs.DATABASE_URL,
-	fetch,
-});
 
 export const db: PlanetScaleDatabase<typeof schema> =
 	sharedEnvs.NEXT_PUBLIC_DEPLOYMENT_ENV === 'production'
-		? psDrizzle(connection, { schema })
+		? psDrizzle(
+				connect({
+					url: sharedEnvs.DATABASE_URL,
+					fetch,
+				}),
+				{ schema },
+		  )
 		: // It's probably fine to lie to TS here, the api should be the same for both
 		  // @ts-expect-error
 		  (mysqlDrizzle(mysql.createPool(sharedEnvs.DATABASE_URL), {
