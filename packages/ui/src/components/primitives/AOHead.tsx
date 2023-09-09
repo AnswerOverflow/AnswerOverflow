91c3bf51a4057980329cfb515ed108602a9ebbf6
Rhys Sullivan <39114868+RhysSullivan@users.noreply.github.com>
2023-09-09 14:55:20 -0700

    fix(web): load adsense verification with next script

--- packages/ui/src/components/primitives/AOHead.tsx
+++ packages/ui/src/components/primitives/AOHead.tsx
@@ -3,6 +3,7 @@ import Head from 'next/head';
 import { makeServerIconLink } from './ServerIcon';
 import { useTenantContext } from '@answeroverflow/hooks';
 import { webClientEnv } from '@answeroverflow/env/web';
+import Script from 'next/script';
 
 interface HeadProps {
 	title: string;
@@ -124,15 +125,6 @@ export const AOHead = ({
 				content={description}
 			/>
 			<meta property="twitter:image" key="twitter:image" content={image} />
-
-			{!isOnTenantSite && (
-				// Adsense verification
-				<script
-					async
-					src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1392153990042810"
-					crossOrigin="anonymous"
-				></script>
-			)}
 		</Head>
 	);
 };
