b287f9f3f71670b79c6319612d14e0068ae6e920
Rhys Sullivan <39114868+RhysSullivan@users.noreply.github.com>
2023-09-09 19:58:58 -0700

    Revert "fix(web): lint"
    This reverts commit 9b5309f6e76057d8726af17d4c07d768fcacea87.

--- packages/ui/src/components/primitives/SearchResult.tsx
+++ packages/ui/src/components/primitives/SearchResult.tsx
@@ -12,6 +12,7 @@ import { ServerInvite } from './ServerInvite';
 import { createContext, useContext } from 'react';
 import Link from 'next/link';
 import { MessageFull } from '@answeroverflow/db';
+import { Heading } from '~ui/components/primitives/base/Heading';
 import { Paragraph } from '~ui/components/primitives/base/Paragraph';
 
 // eslint-disable-next-line @typescript-eslint/naming-convention
