316abbbd1fe13bc2fa816521bb7d00c186bdc3ad
Rhys Sullivan <39114868+RhysSullivan@users.noreply.github.com>
2023-09-16 22:22:00 -0700

    fix(db): remove mysql2 imports from db

--- packages/db/src/db.ts
+++ packages/db/src/db.ts
@@ -6,28 +6,14 @@ import {
 } from 'drizzle-orm/planetscale-serverless';
 import { connect } from '@planetscale/database';
 
-import { drizzle as mysqlDrizzle } from 'drizzle-orm/mysql2';
-import mysql from 'mysql2/promise';
-
 import * as schema from './schema';
 
-const dbUrl =
-	sharedEnvs.NODE_ENV === 'test' && sharedEnvs.TEST_DATABASE_URL
-		? sharedEnvs.TEST_DATABASE_URL
-		: sharedEnvs.DATABASE_URL;
+const dbUrl = sharedEnvs.DATABASE_URL;
 
 // Allow for connecting to a local database
-export const db: PlanetScaleDatabase<typeof schema> =
-	dbUrl.includes('psdb') || dbUrl.includes('pscale_pw')
-		? psDrizzle(
-				connect({
-					url: dbUrl,
-				}),
-				{ schema },
-		  )
-		: // It's probably fine to lie to TS here, the api should be the same for both
-		  // @ts-expect-error
-		  (mysqlDrizzle(mysql.createPool(dbUrl), {
-				schema,
-				mode: 'default',
-		  }) as PlanetScaleDatabase<typeof schema>);
+export const db: PlanetScaleDatabase<typeof schema> = psDrizzle(
+	connect({
+		url: dbUrl,
+	}),
+	{ schema },
+);
