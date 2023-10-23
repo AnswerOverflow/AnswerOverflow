dd4fdc20215d7f915695a5ebda0ce4fe7f6b31af
Rhys Sullivan <39114868+RhysSullivan@users.noreply.github.com>
2023-10-22 22:33:58 -0700

    metadata

--- apps/main-site/src/app/c/[communityId]/page.tsx
+++ apps/main-site/src/app/c/[communityId]/page.tsx
@@ -2,12 +2,42 @@ import { findServerWithCommunityPageData } from '@answeroverflow/db';
 import { CommunityPage } from '@answeroverflow/ui/src/components/pages/CommunityPage';
 import { sharedEnvs } from '@answeroverflow/env/shared';
 import { notFound, permanentRedirect, RedirectType } from 'next/navigation';
-
-export default async function CommunityPageContainer({
-	params,
-}: {
+import { Metadata } from 'next';
+type Props = {
 	params: { communityId: string };
-}) {
+};
+
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+	const communityPageData = await findServerWithCommunityPageData({
+		idOrVanityUrl: params.communityId,
+		selectedChannel: undefined,
+		page: 0,
+	});
+	if (!communityPageData || communityPageData.server.kickedTime != null) {
+		return notFound();
+	}
+	if (communityPageData.server.customDomain) {
+		return permanentRedirect(
+			`http${sharedEnvs.NODE_ENV === 'production' ? 's' : ''}://${
+				communityPageData.server.customDomain
+			}`,
+			RedirectType.replace,
+		);
+	}
+	return {
+		title: `${communityPageData.server.name} Community - Answer Overflow`,
+		description:
+			communityPageData.server.description ??
+			`Questions and answers related to ${communityPageData.server.name}`,
+		openGraph: {
+			title: `${communityPageData.server.name} Community - Answer Overflow`,
+			description:
+				communityPageData.server.description ??
+				`Questions and answers related to ${communityPageData.server.name}`,
+		},
+	};
+}
+export default async function CommunityPageContainer({ params }: Props) {
 	const communityPageData = await findServerWithCommunityPageData({
 		idOrVanityUrl: params.communityId,
 		selectedChannel: undefined,
