45c2b522293882c6ee8cb7fcdfac66b79e7bd61c
Rhys Sullivan <39114868+RhysSullivan@users.noreply.github.com>
2023-10-25 22:25:55 -0700

    fix(web): remove progress bar

--- apps/main-site/src/components/providers.tsx
+++ apps/main-site/src/components/providers.tsx
@@ -1,19 +1,11 @@
 'use client';
 import { ThemeProvider } from 'next-themes';
-import { Next13ProgressBar } from 'next13-progressbar';
 import React from 'react';
 
 export function Providers(props: { children: React.ReactNode }) {
 	return (
 		<ThemeProvider attribute="class" defaultTheme={'dark'} enableSystem>
 			{props.children}
-			<Next13ProgressBar
-				height="1.5px"
-				color="#0094ff"
-				options={{ trickle: true, trickleSpeed: 50, showSpinner: false }}
-				showOnShallow
-				delay={100}
-			/>
 		</ThemeProvider>
 	);
 }
