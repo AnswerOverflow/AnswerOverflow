3edd94c3159d2a3ea823b2862bad915c8a8267f4
Rhys Sullivan <39114868+RhysSullivan@users.noreply.github.com>
2023-11-06 22:24:33 -0800

    fix(ci): build content layer before lint

--- apps/main-site/contentlayer-from-root.config.ts
+++
@@ -1,7 +0,0 @@
-// contentlayer has issues in monorepos, this allows us to build it from the root postinstall script
-import { config } from './contentlayer.config';
-import { makeSource } from 'contentlayer/source-files';
-export default makeSource({
-	...config,
-	contentDirPath: './apps/main-site/src/content',
-});
