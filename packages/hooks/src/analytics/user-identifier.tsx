7ebd78893cb8efc39eb9b0a60de2ea0cf428e508
Rhys Sullivan <39114868+RhysSullivan@users.noreply.github.com>
2023-11-12 12:56:55 -0800

    fix(web): use radix avatars

--- packages/hooks/src/analytics/user-identifier.tsx
+++ packages/hooks/src/analytics/user-identifier.tsx
@@ -2,6 +2,7 @@
 import { usePostHog } from 'posthog-js/react';
 export function IdentifyUser(props: { userId?: string }) {
 	const posthog = usePostHog();
+	if (typeof window === 'undefined') return null;
 	if (props.userId) {
 		posthog.identify(props.userId);
 	} else {
