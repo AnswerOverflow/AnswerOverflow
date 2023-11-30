228d0d9b9402b6e98209c7c46c48fc551f10d138
Rhys Sullivan <39114868+RhysSullivan@users.noreply.github.com>
2023-11-30 10:56:46 -0800

    chore(web): add adsense verification snippet

--- apps/main-site/src/app/layout.tsx
+++ apps/main-site/src/app/layout.tsx
@@ -1,5 +1,11 @@
 import '../styles/globals.css';
-export {
-	Layout as default,
-	metadata,
-} from '@answeroverflow/ui/src/components/layouts/root';
+export { Layout as default } from '@answeroverflow/ui/src/components/layouts/root';
+
+import { metadata as baseMetadata } from '@answeroverflow/ui/src/components/layouts/root';
+import { Metadata } from 'next';
+export const metadata: Metadata = {
+	...baseMetadata,
+	other: {
+		'google-adsense-account': 'ca-pub-1392153990042810',
+	},
+};
