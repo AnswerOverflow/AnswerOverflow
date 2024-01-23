2dd2146e1157eb4e2ac3a6f75b24b21ed977812f
Rhys Sullivan <39114868+RhysSullivan@users.noreply.github.com>
2024-01-22 22:07:27 -0800

    fix(web): remove adsense id from tenant sites

--- apps/main-site/src/app/(main-site)/layout.tsx
+++ apps/main-site/src/app/(main-site)/layout.tsx
@@ -1,5 +1,13 @@
 import { allChangelogs } from '../../data/contentlayer';
 import { sortContentByDateNewestFirst } from '../../utils/sort-content-by-date';
+import { Metadata } from 'next';
+import { metadata as baseMetadata } from '@answeroverflow/ui/src/components/layouts/root';
+export const metadata: Metadata = {
+	...baseMetadata,
+	other: {
+		'google-adsense-account': 'ca-pub-1392153990042810',
+	},
+};
 
 export default function AnnouncementBannerLayout({
 	children,
--- apps/main-site/src/app/layout.tsx
+++ apps/main-site/src/app/layout.tsx
@@ -1,11 +1,5 @@
 import '../styles/globals.css';
-export { Layout as default } from '@answeroverflow/ui/src/components/layouts/root';
-
-import { metadata as baseMetadata } from '@answeroverflow/ui/src/components/layouts/root';
-import { Metadata } from 'next';
-export const metadata: Metadata = {
-	...baseMetadata,
-	other: {
-		'google-adsense-account': 'ca-pub-1392153990042810',
-	},
-};
+export {
+	Layout as default,
+	metadata,
+} from '@answeroverflow/ui/src/components/layouts/root';
