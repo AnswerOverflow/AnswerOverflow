73657d791933976768b76f299c99d7aa680c1201
Rhys Sullivan <39114868+RhysSullivan@users.noreply.github.com>
2024-01-25 10:18:24 -0800

    fix: fmt

--- packages/ui/src/components/primitives/server-invite.tsx
+++ packages/ui/src/components/primitives/server-invite.tsx
@@ -73,12 +73,12 @@ export const ChannelName = ({
 	channel: ChannelPublicWithFlags;
 }) => {
 	return (
-		<div className="flex flex-row gap-1 items-center justify-start">
+		<div className="flex flex-row items-center justify-start gap-1">
 			<ChannelIcon
 				channelType={channel.type}
 				className={'text-left font-bold'}
 			/>
-			<p className="truncate text-left text-base font-bold leading-5 grow">
+			<p className="grow truncate text-left text-base font-bold leading-5">
 				{channel.name}
 			</p>
 		</div>
