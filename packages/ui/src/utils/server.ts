456ab0d35d187dd715f361d5a1447839886b6dd6
Rhys Sullivan <39114868+RhysSullivan@users.noreply.github.com>
2024-01-27 00:29:05 -0800

    fix linting

--- packages/ui/src/utils/server.ts
+++ packages/ui/src/utils/server.ts
@@ -1,6 +1,7 @@
 import type { ServerPublic } from '@answeroverflow/api';
 export function getServerHomepageUrl(server: ServerPublic) {
 	if (
+		// eslint-disable-next-line n/no-process-env,turbo/no-undeclared-env-vars
 		process.env.NEXT_PUBLIC_NODE_ENV !== 'production' &&
 		!server.customDomain
 	) {
