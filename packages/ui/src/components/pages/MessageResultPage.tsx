24461d7853653863eb600f337251e89cc3af2675
Rhys Sullivan <39114868+RhysSullivan@users.noreply.github.com>
2024-01-26 23:03:25 -0800

    Update MessageResultPage.tsx

--- packages/ui/src/components/pages/MessageResultPage.tsx
+++ packages/ui/src/components/pages/MessageResultPage.tsx
@@ -131,7 +131,6 @@ export async function MessageResultPage({
 		} else {
 			consecutivePrivateMessages = 0;
 		}
-
 		// TODO: Remove when embeds are supported
 		if (
 			(message.public || isUserInServer === 'in_server') &&
