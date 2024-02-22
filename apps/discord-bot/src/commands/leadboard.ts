e996ffd05f95e8f190ad0e36dc6a1d18e4a103e9
Rhys Sullivan <39114868+RhysSullivan@users.noreply.github.com>
2024-02-21 22:50:10 -0800

    finish

--- apps/discord-bot/src/commands/leadboard.ts
+++ apps/discord-bot/src/commands/leadboard.ts
@@ -1,71 +1,162 @@
 import { ApplyOptions } from '@sapphire/decorators';
 import { type ChatInputCommand, Command } from '@sapphire/framework';
-import { ChatInputCommandInteraction, SlashCommandBuilder } from 'discord.js';
+import {
+	ChatInputCommandInteraction,
+	EmbedBuilder,
+	SlashCommandBuilder,
+} from 'discord.js';
 import { getCommandIds } from '~discord-bot/utils/utils';
 import { PostHog } from '@typelytics/posthog';
 import { events } from '../../events';
 import { sharedEnvs } from '@answeroverflow/env/shared';
 
 const posthog = new PostHog({
-  events: events,
-  apiKey: sharedEnvs.POSTHOG_PERSONAL_API_KEY!,
-  projectId: sharedEnvs.POSTHOG_PROJECT_ID!.toString(),
+	events: events,
+	apiKey: sharedEnvs.POSTHOG_PERSONAL_API_KEY!,
+	projectId: sharedEnvs.POSTHOG_PROJECT_ID!.toString(),
 });
 
-@ApplyOptions<Command.Options>({
-  name: 'leaderboard',
-  description: 'See who has solved the most questions in the server.',
-  runIn: ['GUILD_ANY'],
-})
-export class ConsentCommand extends Command {
-  public override registerApplicationCommands(
-    registry: ChatInputCommand.Registry,
-  ) {
-    const ids = getCommandIds({
-      // local: '1073363500585468084',
-      // staging: '982084090251595786',
-      // production: '980880200566964264',
-    });
+const medalMap = new Map<number, string>([
+	[0, ':first_place:'],
+	[1, ':second_place:'],
+	[2, ':third_place:'],
+]);
 
-    registry.registerChatInputCommand(
-      new SlashCommandBuilder()
-        .setName(this.name)
-        .setDescription(this.description)
-        .setDMPermission(false)
-        .addBooleanOption((option) =>
-          option
-            .setName('ephemeral')
-            .setDescription('Show the leaderboard only to you.'),
-        ),
-      {
-        idHints: ids,
-      },
-    );
-  }
-  public override async chatInputRun(interaction: ChatInputCommandInteraction) {
-    await interaction.deferReply({
-      ephemeral: interaction.options.getBoolean('ephemeral') ?? false,
-    });
-    const topUsers = await posthog
-      .query()
-      .addSeries('Solved Question', {
-        sampling: 'total',
-      })
-      // .addFilterGroup({
-      // 	match: 'AND',
-      // 	filters: {
-      // 		compare: 'exact',
-      // 		value: interaction.guildId,
-      // 		property: 'Server Id',
-      // 	},
-      // })
-      .execute({
-        type: 'table',
-        // date_from: 'All time',
-        // breakdown_hide_other_aggregation: true,
-        // breakdown: 'Question Solver Id',
-      });
-    console.log(topUsers.results);
-    await interaction.editReply('Leaderboard goes here');
-  }
+@ApplyOptions<Command.Options>({
+	name: 'leaderboard',
+	description: 'See who has solved the most questions in the server.',
+	runIn: ['GUILD_ANY'],
+})
+export class LeaderboardCommand extends Command {
+	public override registerApplicationCommands(
+		registry: ChatInputCommand.Registry,
+	) {
+		const ids = getCommandIds({
+			// local: '1073363500585468084',
+			// staging: '982084090251595786',
+			// production: '980880200566964264',
+		});
+
+		registry.registerChatInputCommand(
+			new SlashCommandBuilder()
+				.setName(this.name)
+				.setDescription(this.description)
+				.setDMPermission(false)
+				.addBooleanOption((option) =>
+					option
+						.setName('ephemeral')
+						.setDescription('Show the leaderboard only to you.'),
+				),
+			{
+				idHints: ids,
+			},
+		);
+	}
+	public override async chatInputRun(interaction: ChatInputCommandInteraction) {
+		await interaction.deferReply({
+			ephemeral: interaction.options.getBoolean('ephemeral') ?? false,
+		});
+		const topUsers = await posthog
+			.query()
+			.addSeries('Solved Question', {
+				sampling: 'total',
+			})
+			.addFilterGroup({
+				match: 'AND',
+				filters: {
+					compare: 'exact',
+					value: interaction.guildId,
+					property: 'Server Id',
+				},
+			})
+			.execute({
+				type: 'table',
+				date_from: 'All time',
+				breakdown_hide_other_aggregation: true,
+				breakdown: 'Question Solver Id',
+			})
+			.then((x) => x.results['Solved Question']);
+
+		const keys = Object.keys(topUsers);
+		// const toDisplay = await Promise.all(
+		// 	keys.map((key) => {
+		// 		const entry = topUsers[key];
+		// 		if (!entry) {
+		// 			return null;
+		// 		}
+		// 		return {
+		// 			user: key,
+		// 			questionsSolved: entry.aggregated_value,
+		// 		};
+		// 	}),
+		// );
+
+		const toDisplay = [
+			{
+				user: '12345647890',
+				questionsSolved: 10,
+			},
+			{
+				user: '123441567890',
+				questionsSolved: 10,
+			},
+			{
+				user: '12345467890',
+				questionsSolved: 10,
+			},
+			{
+				user: '12345678190',
+				questionsSolved: 10,
+			},
+			{
+				user: '123456789140',
+				questionsSolved: 10,
+			},
+			{
+				user: '123456789210',
+				questionsSolved: 10,
+			},
+			{
+				user: '12345678920',
+				questionsSolved: 10,
+			},
+			{
+				user: '12345678290',
+				questionsSolved: 10,
+			},
+			{
+				user: '12345678490',
+				questionsSolved: 10,
+			},
+			{
+				user: '12345678901',
+				questionsSolved: 10,
+			},
+		];
+		const embedDescription = toDisplay
+			.filter((x) => x !== null)
+			.sort((a, b) => b!.questionsSolved - a!.questionsSolved)
+			.map((x, i) => {
+				const user = x!.user;
+				const questionsSolved = x!.questionsSolved;
+				const medal = medalMap.get(i);
+				const msg = `<@${user}> - ${questionsSolved} solved`;
+				const position = i + 1;
+				const spacer = position < 10 ? '\u200B \u200B \u200B' : '\u200B ';
+				return medal
+					? `${medal}: ${msg}`
+					: ` ${spacer} ${position}\u200B: ${msg}`;
+			})
+			.join('\n');
+
+		const embed = new EmbedBuilder()
+			.setTitle('Leaderboard - Questions Solved')
+			.setDescription(embedDescription)
+			.setColor('#89D3F8')
+			.setTimestamp();
+
+		await interaction.editReply({
+			embeds: [embed],
+		});
+	}
 }
