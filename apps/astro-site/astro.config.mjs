53d2e315b9cb34362582a109101f5e6f5aac7210
JoltCode <joe@joecc.dev>
2024-02-28 19:02:14 +0000

    fix(astro-site): ui hmr

--- apps/astro-site/astro.config.mjs
+++ apps/astro-site/astro.config.mjs
@@ -1,9 +1,14 @@
 import { defineConfig } from 'astro/config';
 
-import tailwind from "@astrojs/tailwind";
-import react from "@astrojs/react";
+import tailwind from '@astrojs/tailwind';
+import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()]
+	integrations: [tailwind(), react()],
+	vite: {
+		ssr: {
+			noExternal: ['@answeroverflow/ui'],
+		},
+	},
 });
