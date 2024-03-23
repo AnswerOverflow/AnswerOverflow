4df6926fc4b86a7586d71f6aa532bdf99e86c3ea
Rhys Sullivan <39114868+RhysSullivan@users.noreply.github.com>
2024-03-22 22:59:13 -0700

    fix(fmt)

--- apps/discord-bot/src/listeners/parity/guild-parity.ts
+++ apps/discord-bot/src/listeners/parity/guild-parity.ts
@@ -2,22 +2,22 @@ import { ApplyOptions } from '@sapphire/decorators';
 import { Listener } from '@sapphire/framework';
 import { ChannelType, EmbedBuilder, Events, Guild } from 'discord.js';
 import {
-  ALLOWED_ROOT_CHANNEL_TYPES,
-  upsertServer,
-  upsertManyChannels,
-  findAllServers,
-  updateServer,
+	ALLOWED_ROOT_CHANNEL_TYPES,
+	upsertServer,
+	upsertManyChannels,
+	findAllServers,
+	updateServer,
 } from '@answeroverflow/db';
 import {
-  getMemberCount,
-  toAOChannel,
-  toAOServer,
+	getMemberCount,
+	toAOChannel,
+	toAOServer,
 } from '~discord-bot/utils/conversions';
 import { delay } from '@answeroverflow/discordjs-mock';
 import { registerServerGroup } from '@answeroverflow/analytics';
 import {
-  serverWithDiscordInfoToAnalyticsData,
-  trackDiscordEvent,
+	serverWithDiscordInfoToAnalyticsData,
+	trackDiscordEvent,
 } from '~discord-bot/utils/analytics';
 import { sharedEnvs } from '@answeroverflow/env/shared';
 import { leaveServerIfNecessary } from '~discord-bot/utils/denylist';
@@ -28,153 +28,153 @@ import { leaveServerIfNecessary } from '~discord-bot/utils/denylist';
 
 // Sync server properties that aren't set by the user
 async function autoUpdateServerInfo(guild: Guild) {
-  const convertedServer = toAOServer(guild);
-  const upserted = await upsertServer({
-    create: convertedServer,
-    update: {
-      icon: convertedServer.icon,
-      name: convertedServer.name,
-      description: convertedServer.description,
-      kickedTime: null,
-      vanityInviteCode: convertedServer.vanityInviteCode,
-      approximateMemberCount:
-        getMemberCount(guild) > 0 ? getMemberCount(guild) : undefined,
-    },
-  });
-  registerServerGroup(
-    serverWithDiscordInfoToAnalyticsData({
-      guild,
-      serverWithSettings: upserted,
-    }),
-  );
-  return upserted;
+	const convertedServer = toAOServer(guild);
+	const upserted = await upsertServer({
+		create: convertedServer,
+		update: {
+			icon: convertedServer.icon,
+			name: convertedServer.name,
+			description: convertedServer.description,
+			kickedTime: null,
+			vanityInviteCode: convertedServer.vanityInviteCode,
+			approximateMemberCount:
+				getMemberCount(guild) > 0 ? getMemberCount(guild) : undefined,
+		},
+	});
+	registerServerGroup(
+		serverWithDiscordInfoToAnalyticsData({
+			guild,
+			serverWithSettings: upserted,
+		}),
+	);
+	return upserted;
 }
 
 async function syncServer(guild: Guild) {
-  // If the server doesn't exist we want to initialize it with the default values
-  const updated = await autoUpdateServerInfo(guild);
-  const channelsToUpsert = guild.channels.cache.filter((channel) =>
-    ALLOWED_ROOT_CHANNEL_TYPES.has(channel.type),
-  );
-  await upsertManyChannels(
-    channelsToUpsert.map((channel) => ({
-      create: toAOChannel(channel),
-      update: {
-        name: channel.name,
-      },
-    })),
-  );
-  return updated;
+	// If the server doesn't exist we want to initialize it with the default values
+	const updated = await autoUpdateServerInfo(guild);
+	const channelsToUpsert = guild.channels.cache.filter((channel) =>
+		ALLOWED_ROOT_CHANNEL_TYPES.has(channel.type),
+	);
+	await upsertManyChannels(
+		channelsToUpsert.map((channel) => ({
+			create: toAOChannel(channel),
+			update: {
+				name: channel.name,
+			},
+		})),
+	);
+	return updated;
 }
 
 function makeGuildEmbed(guild: Guild, joined: boolean) {
-  const numberOfForumChannels = guild.channels.cache
-    .filter((c) => c.type === ChannelType.GuildForum)
-    .size.toString();
-  return new EmbedBuilder()
-    .setTitle(joined ? 'Joined Server' : 'Left Server')
-    .setDescription(
-      joined ? `Joined server ${guild.name}` : `Left server ${guild.name}`,
-    )
-    .setThumbnail(guild.iconURL())
-    .setTimestamp()
-    .setColor(joined ? 'Green' : 'Red')
-    .setImage(guild.bannerURL() ?? guild.splashURL())
-    .setFields([
-      {
-        name: 'Members',
-        value: guild.memberCount.toString(),
-        inline: true,
-      },
-      {
-        name: 'Forum Channels',
-        value: numberOfForumChannels,
-        inline: true,
-      },
-      {
-        name: 'Age',
-        value: guild.createdAt.toDateString(),
-        inline: false,
-      },
-      {
-        name: 'Id',
-        value: guild.id,
-        inline: false,
-      },
-    ]);
+	const numberOfForumChannels = guild.channels.cache
+		.filter((c) => c.type === ChannelType.GuildForum)
+		.size.toString();
+	return new EmbedBuilder()
+		.setTitle(joined ? 'Joined Server' : 'Left Server')
+		.setDescription(
+			joined ? `Joined server ${guild.name}` : `Left server ${guild.name}`,
+		)
+		.setThumbnail(guild.iconURL())
+		.setTimestamp()
+		.setColor(joined ? 'Green' : 'Red')
+		.setImage(guild.bannerURL() ?? guild.splashURL())
+		.setFields([
+			{
+				name: 'Members',
+				value: guild.memberCount.toString(),
+				inline: true,
+			},
+			{
+				name: 'Forum Channels',
+				value: numberOfForumChannels,
+				inline: true,
+			},
+			{
+				name: 'Age',
+				value: guild.createdAt.toDateString(),
+				inline: false,
+			},
+			{
+				name: 'Id',
+				value: guild.id,
+				inline: false,
+			},
+		]);
 }
 
 @ApplyOptions<Listener.Options>({ once: true, event: Events.ClientReady })
 export class SyncOnReady extends Listener {
-  public async run() {
-    if (sharedEnvs.NODE_ENV === 'production') await delay(600 * 1000); // give time for dbs to start up
-    // 1. Sync all of the servers to have the most up to date data
-    const guilds = this.container.client.guilds.cache.sort(
-      // sort by member count to give priority to larger servers
-      (a, b) => b.memberCount - a.memberCount,
-    )
-    const activeServerIds = new Set();
-    for await (const guild of guilds.values()) {
-      // eslint-disable-next-line no-await-in-loop
-      try {
-        this.container.logger.info(`Syncing guild ${guild.name}`);
-        await syncServer(guild);
-      } catch (error) {
-        this.container.logger.error(
-          `Error syncing guild ${guild.name}: ${error as string}`,
-        );
-      }
-      // dont kick in error
-      activeServerIds.add(guild.id);
-    }
-    // 2. For any servers that are in the database and not in the guilds the bot is in, mark them as kicked
-    const servers =
-      sharedEnvs.NODE_ENV === 'test' ? [] : await findAllServers();
-    const serversToMarkAsKicked = servers.filter(
-      (server) => !activeServerIds.has(server.id) && !server.kickedTime,
-    );
+	public async run() {
+		if (sharedEnvs.NODE_ENV === 'production') await delay(600 * 1000); // give time for dbs to start up
+		// 1. Sync all of the servers to have the most up to date data
+		const guilds = this.container.client.guilds.cache.sort(
+			// sort by member count to give priority to larger servers
+			(a, b) => b.memberCount - a.memberCount,
+		);
+		const activeServerIds = new Set();
+		for await (const guild of guilds.values()) {
+			// eslint-disable-next-line no-await-in-loop
+			try {
+				this.container.logger.info(`Syncing guild ${guild.name}`);
+				await syncServer(guild);
+			} catch (error) {
+				this.container.logger.error(
+					`Error syncing guild ${guild.name}: ${error as string}`,
+				);
+			}
+			// dont kick in error
+			activeServerIds.add(guild.id);
+		}
+		// 2. For any servers that are in the database and not in the guilds the bot is in, mark them as kicked
+		const servers =
+			sharedEnvs.NODE_ENV === 'test' ? [] : await findAllServers();
+		const serversToMarkAsKicked = servers.filter(
+			(server) => !activeServerIds.has(server.id) && !server.kickedTime,
+		);
 
-    for await (const server of serversToMarkAsKicked.values()) {
-      // eslint-disable-next-line no-await-in-loop
-      this.container.logger.info(`Marking server ${server.name} as kicked`);
-      await updateServer({
-        existing: server,
-        update: {
-          id: server.id,
-          kickedTime: new Date(),
-        },
-      });
-    }
+		for await (const server of serversToMarkAsKicked.values()) {
+			// eslint-disable-next-line no-await-in-loop
+			this.container.logger.info(`Marking server ${server.name} as kicked`);
+			await updateServer({
+				existing: server,
+				update: {
+					id: server.id,
+					kickedTime: new Date(),
+				},
+			});
+		}
 
-    for await (const server of guilds.values())
-      await leaveServerIfNecessary(server);
-  }
+		for await (const server of guilds.values())
+			await leaveServerIfNecessary(server);
+	}
 }
 @ApplyOptions<Listener.Options>({
-  event: Events.GuildCreate,
-  name: 'Guild Sync On Join',
+	event: Events.GuildCreate,
+	name: 'Guild Sync On Join',
 })
 export class SyncOnJoin extends Listener {
-  public async run(guild: Guild) {
-    const leftServer = await leaveServerIfNecessary(guild);
-    if (leftServer) return;
-    const synced = await syncServer(guild);
-    trackDiscordEvent('Server Join', {
-      ...serverWithDiscordInfoToAnalyticsData({
-        guild,
-        serverWithSettings: synced,
-      }),
-      'Answer Overflow Account Id': guild.ownerId, // <---TODO: Not a great id to track with but best we've got
-    });
-    if (sharedEnvs.NODE_ENV !== 'test') {
-      const rhysUser = await this.container.client.users.fetch(
-        '523949187663134754',
-      );
-      await rhysUser.send({
-        embeds: [makeGuildEmbed(guild, true)],
-      });
-    }
-  }
+	public async run(guild: Guild) {
+		const leftServer = await leaveServerIfNecessary(guild);
+		if (leftServer) return;
+		const synced = await syncServer(guild);
+		trackDiscordEvent('Server Join', {
+			...serverWithDiscordInfoToAnalyticsData({
+				guild,
+				serverWithSettings: synced,
+			}),
+			'Answer Overflow Account Id': guild.ownerId, // <---TODO: Not a great id to track with but best we've got
+		});
+		if (sharedEnvs.NODE_ENV !== 'test') {
+			const rhysUser = await this.container.client.users.fetch(
+				'523949187663134754',
+			);
+			await rhysUser.send({
+				embeds: [makeGuildEmbed(guild, true)],
+			});
+		}
+	}
 }
 
 /*
@@ -183,42 +183,42 @@ export class SyncOnJoin extends Listener {
  * A background job will periodically clean up servers that have been kicked for a long time.
  */
 @ApplyOptions<Listener.Options>({
-  event: Events.GuildDelete,
-  name: 'Guild Sync On Delete',
+	event: Events.GuildDelete,
+	name: 'Guild Sync On Delete',
 })
 export class SyncOnDelete extends Listener {
-  public async run(guild: Guild) {
-    const upserted = await upsertServer({
-      create: {
-        ...toAOServer(guild),
-        kickedTime: new Date(),
-      },
-      update: { kickedTime: new Date() },
-    });
-    trackDiscordEvent('Server Leave', {
-      'Answer Overflow Account Id': guild.ownerId, // <---TODO: Not a great id to track with but best we've got
-      ...serverWithDiscordInfoToAnalyticsData({
-        guild,
-        serverWithSettings: upserted,
-      }),
-    });
-    if (sharedEnvs.NODE_ENV !== 'test') {
-      const rhysUser = await this.container.client.users.fetch(
-        '523949187663134754',
-      );
-      await rhysUser.send({
-        embeds: [makeGuildEmbed(guild, false)],
-      });
-    }
-  }
+	public async run(guild: Guild) {
+		const upserted = await upsertServer({
+			create: {
+				...toAOServer(guild),
+				kickedTime: new Date(),
+			},
+			update: { kickedTime: new Date() },
+		});
+		trackDiscordEvent('Server Leave', {
+			'Answer Overflow Account Id': guild.ownerId, // <---TODO: Not a great id to track with but best we've got
+			...serverWithDiscordInfoToAnalyticsData({
+				guild,
+				serverWithSettings: upserted,
+			}),
+		});
+		if (sharedEnvs.NODE_ENV !== 'test') {
+			const rhysUser = await this.container.client.users.fetch(
+				'523949187663134754',
+			);
+			await rhysUser.send({
+				embeds: [makeGuildEmbed(guild, false)],
+			});
+		}
+	}
 }
 
 @ApplyOptions<Listener.Options>({
-  event: Events.GuildUpdate,
-  name: 'Guild Sync On Update',
+	event: Events.GuildUpdate,
+	name: 'Guild Sync On Update',
 })
 export class SyncOnUpdate extends Listener {
-  public async run(_: Guild, newGuild: Guild) {
-    await autoUpdateServerInfo(newGuild);
-  }
+	public async run(_: Guild, newGuild: Guild) {
+		await autoUpdateServerInfo(newGuild);
+	}
 }
