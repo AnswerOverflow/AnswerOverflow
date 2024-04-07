e3028fa057e036037308a026851aef8422d68256
Rhys Sullivan <39114868+RhysSullivan@users.noreply.github.com>
2024-04-07 13:31:22 -0700

    move to layout

--- apps/dashboard/src/app/dashboard/[serverId]/layout.tsx
+++ apps/dashboard/src/app/dashboard/[serverId]/layout.tsx
@@ -1,3 +1,4 @@
+'use client';
 import Link from 'next/link';
 import { Button } from '@answeroverflow/ui/src/ui/button';
 import {
@@ -6,10 +7,13 @@ import {
 	SheetTrigger,
 } from '@answeroverflow/ui/src/ui/sheet';
 import { UserSection } from '@answeroverflow/ui/src/navbar/client';
-import { AnswerOverflowLogo } from 'packages/ui/src/icons/answer-overflow-logo';
+import { AnswerOverflowLogo } from '@answeroverflow/ui/src/icons/answer-overflow-logo';
 import { FaHome } from 'react-icons/fa';
 import { FaGear } from 'react-icons/fa6';
 import { ServerSelectDropdown } from './components/navbar';
+import { DashboardProvider } from './components/dashboard-context';
+import { trpc } from '@answeroverflow/ui/src/utils/client';
+import { demoServerData } from './components/mock';
 
 export default function Layout(props: {
 	children?: React.ReactNode;
@@ -27,47 +31,31 @@ export default function Layout(props: {
 			icon: <FaGear className="size-5" />,
 		},
 	];
+	const { data } = trpc.servers.byId.useQuery(props.params.serverId, {
+		initialData: props.params.serverId === '1000' ? demoServerData : undefined,
+	});
+	if (!data) return null;
 	return (
-		<div className="grid min-h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
-			<div className="hidden border-r bg-muted/40 md:block">
-				<div className="flex h-full max-h-screen flex-col gap-2">
-					<div className="flex h-14 items-center border-b px-4 lg:h-[60px] lg:px-6">
-						<Link href="/" className="flex items-center gap-2 font-semibold">
-							<AnswerOverflowLogo width={140} />
-						</Link>
-					</div>
-					<div className="flex-1">
-						<nav className="grid items-start px-2 text-sm font-medium lg:px-4">
-							{dashboardPaths.map((path) => {
-								return (
-									<Link
-										href={path.path}
-										key={path.path}
-										className="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary"
-									>
-										{path.icon}
-										{path.label}
-									</Link>
-								);
-							})}
-						</nav>
-					</div>
-				</div>
-			</div>
-			<div className="flex flex-col">
-				<header className="flex h-14 items-center gap-4 border-b bg-muted/40 px-4 lg:h-[60px] lg:px-6">
-					<Sheet>
-						<SheetTrigger asChild>
-							<Button
-								variant="outline"
-								size="icon"
-								className="shrink-0 md:hidden"
-							>
-								<span className="sr-only">Toggle navigation menu</span>
-							</Button>
-						</SheetTrigger>
-						<SheetContent side="left" className="flex flex-col">
-							<nav className="grid gap-2 text-lg font-medium">
+		<DashboardProvider
+			value={{
+				options: {
+					serverId: data.id,
+					from: new Date(new Date().setDate(new Date().getDate() - 7)),
+					to: new Date(),
+				},
+				server: data,
+			}}
+		>
+			<div className="grid min-h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
+				<div className="hidden border-r bg-muted/40 md:block">
+					<div className="flex h-full max-h-screen flex-col gap-2">
+						<div className="flex h-14 items-center border-b px-4 lg:h-[60px] lg:px-6">
+							<Link href="/" className="flex items-center gap-2 font-semibold">
+								<AnswerOverflowLogo width={140} />
+							</Link>
+						</div>
+						<div className="flex-1">
+							<nav className="grid items-start px-2 text-sm font-medium lg:px-4">
 								{dashboardPaths.map((path) => {
 									return (
 										<Link
@@ -81,17 +69,48 @@ export default function Layout(props: {
 									);
 								})}
 							</nav>
-						</SheetContent>
-					</Sheet>
-					<div className="w-full flex-1">
-						<ServerSelectDropdown />
+						</div>
 					</div>
-					<UserSection tenant={undefined} />
-				</header>
-				<main className="flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6">
-					{props.children}
-				</main>
+				</div>
+				<div className="flex flex-col">
+					<header className="flex h-14 items-center gap-4 border-b bg-muted/40 px-4 lg:h-[60px] lg:px-6">
+						<Sheet>
+							<SheetTrigger asChild>
+								<Button
+									variant="outline"
+									size="icon"
+									className="shrink-0 md:hidden"
+								>
+									<span className="sr-only">Toggle navigation menu</span>
+								</Button>
+							</SheetTrigger>
+							<SheetContent side="left" className="flex flex-col">
+								<nav className="grid gap-2 text-lg font-medium">
+									{dashboardPaths.map((path) => {
+										return (
+											<Link
+												href={path.path}
+												key={path.path}
+												className="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary"
+											>
+												{path.icon}
+												{path.label}
+											</Link>
+										);
+									})}
+								</nav>
+							</SheetContent>
+						</Sheet>
+						<div className="w-full flex-1">
+							<ServerSelectDropdown />
+						</div>
+						<UserSection tenant={undefined} />
+					</header>
+					<main className="flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6">
+						{props.children}
+					</main>
+				</div>
 			</div>
-		</div>
+		</DashboardProvider>
 	);
 }
--- apps/dashboard/src/app/dashboard/[serverId]/page.tsx
+++ apps/dashboard/src/app/dashboard/[serverId]/page.tsx
@@ -1,40 +1,22 @@
 'use client';
-import { trpc } from '@answeroverflow/ui/src/utils/client';
 import { DatePickerWithPresets } from '@answeroverflow/ui/src/ui/date-picker';
-import { DashboardProvider } from './components/dashboard-context';
 import {
 	PageViewsLineChart,
 	PageViewsTotalCard,
 	ServerInvitesClickedTotalCard,
 } from './components/home';
-import { demoServerData } from './components/mock';
 
-export default function Dashboard(props: { params: { serverId: string } }) {
-	const { data } = trpc.servers.byId.useQuery(props.params.serverId, {
-		initialData: props.params.serverId === '1000' ? demoServerData : undefined,
-	});
-	if (!data) return null;
+export default function Dashboard() {
 	return (
 		<div>
-			<DashboardProvider
-				value={{
-					options: {
-						serverId: data.id,
-						from: new Date(new Date().setDate(new Date().getDate() - 7)),
-						to: new Date(),
-					},
-					server: data,
-				}}
-			>
-				<DatePickerWithPresets />
-				<div>
-					<div className="flex flex-row justify-start">
-						<PageViewsTotalCard />
-						<ServerInvitesClickedTotalCard />
-					</div>
-					<PageViewsLineChart />
+			<DatePickerWithPresets />
+			<div>
+				<div className="flex flex-row justify-start">
+					<PageViewsTotalCard />
+					<ServerInvitesClickedTotalCard />
 				</div>
-			</DashboardProvider>
+				<PageViewsLineChart />
+			</div>
 		</div>
 	);
 }
