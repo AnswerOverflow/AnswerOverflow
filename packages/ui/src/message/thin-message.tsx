f1e1b9fb3df323b59b9c9f78f0f51ec1a549bf96
Rhys Sullivan <39114868+RhysSullivan@users.noreply.github.com>
2024-04-13 23:11:11 -0700

    fix(web): aligment of author again

--- packages/ui/src/message/thin-message.tsx
+++ packages/ui/src/message/thin-message.tsx
@@ -11,12 +11,12 @@ export function ThinMessage(
 ) {
 	const { message } = props;
 	return (
-		<div className="grid grid-flow-col">
-			<div className="mb-8">
+		<div className="flex flex-row">
+			<div className="mb-8 w-[40px] flex-shrink-0">
 				<DiscordAvatar user={message.author} size={40} />
 				<div className={cn('mx-auto h-full w-0 rounded-full border-1')} />
 			</div>
-			<div className="flex flex-col pl-2 pt-2 ">
+			<div className="flex flex-col pl-2 pt-2">
 				<div className="flex flex-row items-center gap-2 text-muted-foreground">
 					<span className="mr-1">{message.author.name}</span>
 					<span className="text-sm ">•</span>
