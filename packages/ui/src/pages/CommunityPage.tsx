b7b542bfc3ea017c461648027db81ee9c9e16d22
Rhys Sullivan <39114868+RhysSullivan@users.noreply.github.com>
2024-04-16 23:21:14 -0700

    fix(web): sort community page channels & add pubg mobile to home

--- packages/ui/src/pages/CommunityPage.tsx
+++ packages/ui/src/pages/CommunityPage.tsx
@@ -25,6 +25,7 @@ import { LuArrowLeft, LuArrowRight } from 'react-icons/lu';
 import { TrackLoad } from '../ui/track-load';
 import { serverToAnalyticsData } from '@answeroverflow/constants';
 import { Suspense } from 'react';
+import { ChannelType } from '../utils/discord';
 type ChannelSelectProps = {
 	channels: ChannelPublicWithFlags[];
 	selectedChannel: ChannelPublicWithFlags;
@@ -148,7 +149,38 @@ export const CommunityPage = (
 		page: number | undefined;
 	},
 ) => {
-	const { server, channels, selectedChannel, tenant, posts: questions } = props;
+	const {
+		server,
+		channels: channelsRaw,
+		selectedChannel,
+		tenant,
+		posts: questions,
+	} = props;
+	// sort order: unarchived forums, unarchived text, archived forums, archived text
+	const channels = channelsRaw.sort((a, b) => {
+		if (
+			a.type === ChannelType.GuildForum.valueOf() &&
+			b.type !== ChannelType.GuildForum.valueOf()
+		)
+			return -1;
+		if (
+			a.type !== ChannelType.GuildForum.valueOf() &&
+			b.type === ChannelType.GuildForum.valueOf()
+		)
+			return 1;
+		if (
+			a.type === ChannelType.GuildText.valueOf() &&
+			b.type !== ChannelType.GuildText.valueOf()
+		)
+			return -1;
+		if (
+			a.type !== ChannelType.GuildText.valueOf() &&
+			b.type === ChannelType.GuildText.valueOf()
+		)
+			return 1;
+
+		return 0;
+	});
 	// useTrackEvent('Community Page View', serverToAnalyticsData(server));
 	const { page = 0 } = props;
 	const HeroArea = () => {
