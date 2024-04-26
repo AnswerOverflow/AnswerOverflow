5f7fb59b90ebc86ef6d37d57578b32a05299830b
Rhys Sullivan <39114868+RhysSullivan@users.noreply.github.com>
2024-04-25 23:42:50 -0700

    test(web): s3 sitemaps

--- apps/discord-bot/src/listeners/events/sitemap-generator.ts
+++ apps/discord-bot/src/listeners/events/sitemap-generator.ts
@@ -1,36 +1,27 @@
 import { ApplyOptions } from '@sapphire/decorators';
 import { Listener } from '@sapphire/framework';
 import { Events } from 'discord.js';
-import { hoursToMs } from '~discord-bot/utils/utils';
-// import { generateSitemap } from '@answeroverflow/db/src/sitemap';
+import { generateSitemap } from '@answeroverflow/db/src/sitemap';
 import { sharedEnvs } from '@answeroverflow/env/shared';
-import { findAllServers } from '@answeroverflow/db';
-import { cacheQuestionsForSitemap } from '@answeroverflow/cache';
+import { CronJob } from 'cron';
+
 @ApplyOptions<Listener.Options>({ event: Events.ClientReady })
 export class LoopSitemap extends Listener {
 	public run() {
-		// temporarily disabled
 		if (sharedEnvs.NODE_ENV === 'development') {
 			this.container.logger.info(
 				'Skipping sitemap generation in development mode.',
 			);
 			return;
 		}
-
-		// for now, just fetch the existing sitemap index and sub sitemaps
-		// to keep the sitemap up to date
-		const refreshSitemaps = async () => {
-			const servers = await findAllServers({
-				includeCustomDomain: true,
-				includeKicked: false,
-			});
-			for await (const server of servers) {
-				await cacheQuestionsForSitemap(server.id);
-				// wait for 1 second
-				await new Promise((resolve) => setTimeout(resolve, 1000));
-			}
-		};
-		// refresh sitemaps every 6 hours
-		setInterval(() => refreshSitemaps, hoursToMs(6));
+		CronJob.from({
+			// every morning at 1 am pst
+			cronTime: '0 1 * * *',
+			onTick: async () => {
+				await generateSitemap();
+			},
+			start: true,
+			timeZone: 'America/Los_Angeles',
+		});
 	}
 }
