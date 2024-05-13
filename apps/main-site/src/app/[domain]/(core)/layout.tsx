b3bed42ed1275489c77a71371faa4bc2238456fe
JoltCode <joe@joecc.dev>
2024-05-13 21:24:15 +0100

    feat(ui): make navbar sticky

--- apps/main-site/src/app/[domain]/(core)/layout.tsx
+++ apps/main-site/src/app/[domain]/(core)/layout.tsx
@@ -21,8 +21,8 @@ export default async function RootLayout({
 		<div className="mx-auto flex w-full flex-col items-center overflow-y-auto bg-background font-body">
 			<div className="w-full justify-center">
 				<Navbar tenant={server} />
-				<div className="flex flex-row">
-					<div className="mx-auto w-full max-w-screen-2xl px-2 pt-2 xl:pt-8">
+				<div className="mt-16 flex flex-row">
+					<div className="ml-[250px] mr-auto max-w-screen-2xl px-2 pt-2 xl:pt-8">
 						<main className="w-full">{children}</main>
 						<Footer tenant={server} />
 					</div>
