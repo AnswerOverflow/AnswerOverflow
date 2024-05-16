8c843fd69085e62902971268d49e21ee87d94efc
Rhys Sullivan <39114868+RhysSullivan@users.noreply.github.com>
2024-05-16 07:00:22 -0700

    fix(web): caching

--- apps/discord-bot/src/listeners/events/sitemap-generator.ts
+++ apps/discord-bot/src/listeners/events/sitemap-generator.ts
@@ -5,6 +5,8 @@ import { generateSitemap } from '@answeroverflow/db/src/sitemap';
 import { sharedEnvs } from '@answeroverflow/env/shared';
 // eslint-disable-next-line n/no-extraneous-import
 import { CronJob } from 'cron';
+import { findAllServers } from '@answeroverflow/db';
+import { cacheQuestionsForSitemap } from '@answeroverflow/cache';
 
 @ApplyOptions<Listener.Options>({ event: Events.ClientReady })
 export class LoopSitemap extends Listener {
@@ -20,6 +22,15 @@ export class LoopSitemap extends Listener {
 			cronTime: '0 1 * * *',
 			onTick: async () => {
 				await generateSitemap();
+				const servers = await findAllServers({
+					includeCustomDomain: true,
+					includeKicked: false,
+				});
+				for await (const server of servers) {
+					await cacheQuestionsForSitemap(server.id);
+					// wait for 1 second
+					await new Promise((resolve) => setTimeout(resolve, 1000));
+				}
 			},
 			start: true,
 			timeZone: 'America/Los_Angeles',
