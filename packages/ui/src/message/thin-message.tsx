8a714802cb9f0fd4d60d54390a9b419afef6e400
Rhys Sullivan <39114868+RhysSullivan@users.noreply.github.com>
2024-05-16 23:08:32 -0700

    fix(web): blur thin messages & fallback to discord avatars

--- packages/ui/src/message/thin-message.tsx
+++ packages/ui/src/message/thin-message.tsx
@@ -1,7 +1,7 @@
 import { DiscordAvatar } from '../discord-avatar';
 import { TimeAgo } from '../ui/time-ago';
 import { cn } from '../utils/utils';
-import { MessageBody } from './Message';
+import { MessageBlurrer, MessageBody } from './Message';
 import { MessageProps } from './props';
 
 export function ThinMessage(
@@ -11,21 +11,23 @@ export function ThinMessage(
 ) {
 	const { message } = props;
 	return (
-		<div className="flex flex-row">
-			<div className="mb-8 w-[40px] flex-shrink-0">
-				<DiscordAvatar user={message.author} size={40} />
-				<div className={cn('mx-auto h-full w-0 rounded-full border-1')} />
-			</div>
-			<div className="flex flex-col pl-2 pt-2">
-				<div className="flex flex-row items-center gap-2 text-muted-foreground">
-					<span className="mr-1">{message.author.name}</span>
-					<span className="text-sm ">•</span>
-					<TimeAgo snowflake={message.id} />
+		<MessageBlurrer message={message}>
+			<div className="flex flex-row">
+				<div className="mb-8 w-[40px] flex-shrink-0">
+					<DiscordAvatar user={message.author} size={40} />
+					<div className={cn('mx-auto h-full w-0 rounded-full border-1')} />
 				</div>
-				<div>
-					<MessageBody message={message} />
+				<div className="flex flex-col pl-2 pt-2">
+					<div className="flex flex-row items-center gap-2 text-muted-foreground">
+						<span className="mr-1">{message.author.name}</span>
+						<span className="text-sm ">•</span>
+						<TimeAgo snowflake={message.id} />
+					</div>
+					<div>
+						<MessageBody message={message} />
+					</div>
 				</div>
 			</div>
-		</div>
+		</MessageBlurrer>
 	);
 }
