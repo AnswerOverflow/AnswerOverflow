822b6f6edb586058242f18e0117fcd531495f52f
Rhys Sullivan <39114868+RhysSullivan@users.noreply.github.com>
2024-05-19 21:50:10 -0700

    feat(web): comments on user page

---
+++ apps/main-site/src/app/[domain]/(core)/u/[userId]/components.tsx
@@ -0,0 +1,71 @@
+import { notFound } from 'next/navigation';
+import {
+	findDiscordAccountById,
+	findLatestCommentsFromAuthor,
+	findLatestThreadsFromAuthor,
+	findManyMessagesWithAuthors,
+	findServerByCustomDomain,
+	messages,
+} from '@answeroverflow/db';
+import { LinkButton } from '@answeroverflow/ui/src/ui/link-button';
+
+export type Props = {
+	params: { userId: string; domain: string };
+	searchParams?: { s?: string };
+};
+
+export async function getUserPageData(props: Props) {
+	const server = await findServerByCustomDomain(
+		decodeURIComponent(props.params.domain),
+	);
+	const userInfo = await findDiscordAccountById(props.params.userId);
+	if (!userInfo || !server || !server.customDomain) return notFound();
+	const [threads, comments] = await Promise.all([
+		findLatestThreadsFromAuthor(props.params.userId, {
+			serverId: props.searchParams?.s,
+		}),
+		findLatestCommentsFromAuthor(props.params.userId, {
+			serverId: props.searchParams?.s,
+		}),
+	]);
+	const commentsWithMessage = await findManyMessagesWithAuthors(
+		comments.map((x) => x.id),
+	);
+	const msgs = await messages.loadMany(threads.map((x) => x.id));
+	const hasSomePublicData =
+		commentsWithMessage.some((comment) => comment.public) ||
+		msgs.some(
+			(thread) => typeof thread?.message === 'object' && thread.message.public,
+		);
+	if (!hasSomePublicData) return notFound();
+	return { userInfo, threads, comments: commentsWithMessage, server };
+}
+
+export async function ActualLayout(
+	props: {
+		children: React.ReactNode;
+	} & Props,
+) {
+	const { userInfo } = await getUserPageData(props);
+	return (
+		<>
+			<div className="flex flex-row gap-4">
+				<LinkButton
+					variant={'outline'}
+					selectedVariant={'secondary'}
+					href={`/u/${userInfo.id}`}
+				>
+					Posts
+				</LinkButton>
+				<LinkButton
+					variant={'outline'}
+					selectedVariant={'secondary'}
+					href={`/u/${userInfo.id}/comments`}
+				>
+					Comments
+				</LinkButton>
+			</div>
+			{props.children}
+		</>
+	);
+}
--- apps/main-site/src/app/[domain]/(core)/u/[userId]/page.tsx
+++ apps/main-site/src/app/[domain]/(core)/u/[userId]/page.tsx
@@ -1,64 +1,26 @@
-import {
-	findDiscordAccountById,
-	findLatestThreadsFromAuthor,
-	findServerByCustomDomain,
-	messages,
-} from '@answeroverflow/db';
-import { DiscordAvatar } from '@answeroverflow/ui/src/discord-avatar';
-import { notFound } from 'next/navigation';
-import { Metadata } from 'next';
 import { FeedPost } from '@answeroverflow/ui/src/feed-post';
+import { ActualLayout, getUserPageData } from './components';
+import { GiSpiderWeb } from 'react-icons/gi';
 
 type Props = {
 	params: { userId: string; domain: string };
+	searchParams: { s?: string };
 };
 
-async function getUserPageData(props: Props) {
-	const server = await findServerByCustomDomain(
-		decodeURIComponent(props.params.domain),
-	);
-	const userInfo = await findDiscordAccountById(props.params.userId);
-	if (!userInfo || !server) return notFound();
-	const threads = await findLatestThreadsFromAuthor(props.params.userId, {
-		serverId: server.id,
-	});
-	const msgs = await messages.loadMany(threads.map((x) => x.id));
-	if (!msgs.some((x) => typeof x?.message === 'object' && x.message?.public))
-		return notFound();
-	return { userInfo, threads, server };
-}
-
-export async function generateMetadata(props: Props): Promise<Metadata> {
-	const { userInfo, server } = await getUserPageData(props);
-	return {
-		title: `${userInfo.name} Posts - ${server.name}`,
-		description: `See posts from ${userInfo.name} from ${server.name}'s Discord`,
-		alternates: {
-			canonical: `https://${server.customDomain}/u/${userInfo.id}`,
-		},
-		openGraph: {
-			title: `${userInfo.name} Posts - ${server.name}`,
-			description: `See posts from ${userInfo.name} from ${server.name}'s Discord`,
-		},
-	};
-}
-
 export default async function UserPage(props: Props) {
-	const { userInfo, threads } = await getUserPageData(props);
+	const { threads } = await getUserPageData(props);
 	return (
-		<main className="flex w-full justify-center pt-4">
-			<div className="flex w-full max-w-[850px]  flex-col gap-4">
-				<div className="flex flex-row items-center gap-2">
-					<DiscordAvatar user={userInfo} size={64} />
-					<span className="text-4xl font-semibold">{userInfo.name}</span>
-				</div>
-				<span className="text-xl">Posts</span>
-				<div className={'flex flex-col gap-4'}>
-					{threads.map((x) => (
-						<FeedPost postId={x.id} key={x.id} />
-					))}
-				</div>
+		<ActualLayout {...props}>
+			<div className={'flex flex-col gap-4'}>
+				{threads.length > 0 ? (
+					threads.map((x) => <FeedPost postId={x.id} key={x.id} />)
+				) : (
+					<div className={'flex flex-row items-center justify-start gap-4'}>
+						<GiSpiderWeb size={64} className="text-muted-foreground" />
+						<span className={'text-xl'}>No posts found</span>
+					</div>
+				)}
 			</div>
-		</main>
+		</ActualLayout>
 	);
 }
