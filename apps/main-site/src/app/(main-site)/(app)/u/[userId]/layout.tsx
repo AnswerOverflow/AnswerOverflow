01b1173db7657018575d086666291dd9e38ab159
Rhys Sullivan <39114868+RhysSullivan@users.noreply.github.com>
2024-05-28 22:08:11 -0700

    feat(web): jsonld for profile pages

--- apps/main-site/src/app/(main-site)/(app)/u/[userId]/layout.tsx
+++ apps/main-site/src/app/(main-site)/(app)/u/[userId]/layout.tsx
@@ -1,6 +1,12 @@
-import { DiscordAvatar } from '@answeroverflow/ui/src/discord-avatar';
+import {
+	DiscordAvatar,
+	makeUserIconLink,
+} from '@answeroverflow/ui/src/discord-avatar';
 import { Metadata } from 'next';
 import { getUserPageData, Props } from './components';
+import { JsonLd } from 'react-schemaorg';
+import type { ProfilePage } from 'schema-dts';
+import { getDate } from '@answeroverflow/ui/src/utils/snowflake';
 
 export async function generateMetadata(
 	props: Omit<Props, 'searchParams'>,
@@ -25,6 +31,35 @@ export default async function Layout(
 	const { userInfo } = await getUserPageData(props);
 	return (
 		<main className=" flex w-full justify-center pt-4">
+			<JsonLd<ProfilePage>
+				item={{
+					'@context': 'https://schema.org',
+					'@type': 'ProfilePage',
+					alternateName: userInfo.name,
+					dateCreated: getDate(userInfo.id).toISOString(),
+					image: {
+						'@type': 'ImageObject',
+						url: makeUserIconLink(userInfo, 256),
+						height: {
+							'@type': 'QuantitativeValue',
+							value: 256,
+						},
+						width: {
+							'@type': 'QuantitativeValue',
+							value: 256,
+						},
+					},
+					name: userInfo.name,
+					url: `https://www.answeroverflow.com/u/${userInfo.id}`,
+					mainEntity: {
+						'@type': 'Person',
+						identifier: userInfo.id,
+						name: userInfo.name,
+						url: `https://www.answeroverflow.com/u/${userInfo.id}`,
+					},
+				}}
+			/>
+
 			<div className="flex w-full max-w-[850px] flex-col gap-4">
 				<div className="flex flex-row items-center gap-2">
 					<DiscordAvatar user={userInfo} size={64} />
--- apps/main-site/src/app/[domain]/(core)/u/[userId]/layout.tsx
+++ apps/main-site/src/app/[domain]/(core)/u/[userId]/layout.tsx
@@ -1,6 +1,12 @@
-import { DiscordAvatar } from '@answeroverflow/ui/src/discord-avatar';
+import {
+	DiscordAvatar,
+	makeUserIconLink,
+} from '@answeroverflow/ui/src/discord-avatar';
 import { Metadata } from 'next';
 import { getUserPageData, Props } from './components';
+import { JsonLd } from 'react-schemaorg';
+import { ProfilePage } from 'schema-dts';
+import { getDate } from '@answeroverflow/ui/src/utils/snowflake';
 
 export async function generateMetadata(props: Props): Promise<Metadata> {
 	const { userInfo, server } = await getUserPageData(props);
@@ -19,9 +25,37 @@ export async function generateMetadata(props: Props): Promise<Metadata> {
 export default async function Layout(
 	props: { children: React.ReactNode } & Props,
 ) {
-	const { userInfo } = await getUserPageData(props);
+	const { userInfo, server } = await getUserPageData(props);
 	return (
 		<main className=" flex w-full justify-center pt-4">
+			<JsonLd<ProfilePage>
+				item={{
+					'@context': 'https://schema.org',
+					'@type': 'ProfilePage',
+					alternateName: userInfo.name,
+					dateCreated: getDate(userInfo.id).toISOString(),
+					image: {
+						'@type': 'ImageObject',
+						url: makeUserIconLink(userInfo, 512),
+						height: {
+							'@type': 'QuantitativeValue',
+							value: 512,
+						},
+						width: {
+							'@type': 'QuantitativeValue',
+							value: 512,
+						},
+					},
+					name: userInfo.name,
+					url: `https://${server.customDomain}{/u/${userInfo.id}`,
+					mainEntity: {
+						'@type': 'Person',
+						identifier: userInfo.id,
+						name: userInfo.name,
+						url: `https://${server.customDomain}{/u/${userInfo.id}`,
+					},
+				}}
+			/>
 			<div className="flex w-full max-w-[850px] flex-col gap-4">
 				<div className="flex flex-row items-center gap-2">
 					<DiscordAvatar user={userInfo} size={64} />
