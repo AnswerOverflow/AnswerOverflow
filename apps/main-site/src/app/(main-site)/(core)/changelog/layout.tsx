9f26bbec3975e1deb8cd9215a4ce2404b247e421
Rhys Sullivan <39114868+RhysSullivan@users.noreply.github.com>
2024-06-11 13:42:25 -0700

    chore(web): next 15

--- apps/main-site/src/app/(main-site)/(core)/changelog/layout.tsx
+++
@@ -1,3 +0,0 @@
-export default function Layout({ children }: { children: React.ReactNode }) {
-	return <div className="mx-auto max-w-3xl">{children}</div>;
-}
--- apps/main-site/src/data/contentlayer.ts
+++
@@ -1,1 +0,0 @@
-export * from '../../.contentlayer/generated'; // Dumb hackery to make linting work
