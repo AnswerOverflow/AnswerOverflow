4ac3f31e1095ab0bc26e418b1d5fb86996ec841d
Rhys Sullivan <39114868+RhysSullivan@users.noreply.github.com>
2024-06-11 17:55:48 -0700

    fix(web): turn ads back on

--- packages/ui/src/ui/ads.tsx
+++ packages/ui/src/ui/ads.tsx
@@ -6,18 +6,18 @@ export function CarbonAds() {
 		<div
 			className={' w-full text-white'}
 			// NextJS is dumb and lifts script tags to <head/> so we have to use dangerouslySetInnerHTML
-			// dangerouslySetInnerHTML={{
-			// 	__html:
-			// 		'<script\n' +
-			// 		'async\n' +
-			// 		'type="text/javascript"\n' +
-			// 		// set parent display to none if ad fails to load
-			// 		'onerror="this.parentElement.style.display = \'none\'"\n' +
-			// 		'src="//cdn.carbonads.com/carbon.js?serve=CWYIV53I&placement=wwwansweroverflowcom&format=cover"\n' +
-			// 		'id="_carbonads_js"\n' +
-			// 		'></script>' +
-			// 		'',
-			// }}
+			dangerouslySetInnerHTML={{
+				__html:
+					'<script\n' +
+					'async\n' +
+					'type="text/javascript"\n' +
+					// set parent display to none if ad fails to load
+					'onerror="this.parentElement.style.display = \'none\'"\n' +
+					'src="//cdn.carbonads.com/carbon.js?serve=CWYIV53I&placement=wwwansweroverflowcom&format=cover"\n' +
+					'id="_carbonads_js"\n' +
+					'></script>' +
+					'',
+			}}
 		/>
 	);
 }
