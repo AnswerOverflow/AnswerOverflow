db34ffb396a6aea71ed4b70bf6c98cc404c670dc
Rhys Sullivan <39114868+RhysSullivan@users.noreply.github.com>
2024-06-12 01:17:23 -0700

    prerender working

--- apps/main-site-astro/astro.config.mjs
+++ apps/main-site-astro/astro.config.mjs
@@ -1,8 +1,15 @@
 import { defineConfig } from 'astro/config';
-
 import react from "@astrojs/react";
+
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()]
+  integrations: [react(), tailwind()],
+  output: "hybrid",
+  vite: {
+    ssr: {
+      noExternal: ['@answeroverflow/ui']
+    }
+  }
 });
