32a10c44ffb2924efc5c3edcaa40ff91159833ec
JoltCode <joe@joecc.dev>
2024-08-18 13:21:28 +0100

    chore(testing): bump vitest

--- packages/db/src/db.ts
+++ packages/db/src/db.ts
@@ -9,62 +9,62 @@ const dbUrl = sharedEnvs.DATABASE_URL;
 
 const decoder = new TextDecoder('utf-8');
 function bytes(text: string): number[] {
-	return text.split('').map((c) => c.charCodeAt(0));
+  return text.split('').map((c) => c.charCodeAt(0));
 }
 
 const cachedFetch = (input: any, init?: any) =>
-	// @ts-ignore
-	// eslint-disable-next-line @typescript-eslint/no-unsafe-return, @typescript-eslint/no-unsafe-call,@typescript-eslint/no-unsafe-argument
-	fetch(input, {
-		...init,
-		cache: sharedEnvs.ENVIRONMENT === 'main-site' ? 'force-cache' : 'no-store',
-	});
+  // @ts-ignore
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-return, @typescript-eslint/no-unsafe-call,@typescript-eslint/no-unsafe-argument
+  fetch(input, {
+    ...init,
+    cache: sharedEnvs.ENVIRONMENT === 'main-site' ? 'force-cache' : 'no-store',
+  });
 
 const client = new Client({
-	url: dbUrl,
-	fetch: cachedFetch,
-	cast: (field, value) => {
-		if (field.type === 'JSON') {
-			// eslint-disable-next-line @typescript-eslint/no-unsafe-return
-			if (value === null) return null;
-			return JSONParse(decoder.decode(Uint8Array.from(bytes(value as string))));
-		}
-		// eslint-disable-next-line @typescript-eslint/no-unsafe-return
-		return cast(field, value);
-	},
+  url: dbUrl,
+  fetch: cachedFetch,
+  cast: (field, value) => {
+    if (field.type === 'JSON') {
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-return
+      if (value === null) return null;
+      return JSONParse(decoder.decode(Uint8Array.from(bytes(value as string))));
+    }
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-return
+    return cast(field, value);
+  },
 });
 export const db: PlanetScaleDatabase<typeof schema> = psDrizzle(
-	// @ts-ignore
-	client,
-	{ schema },
+  // @ts-ignore
+  client,
+  { schema },
 );
 
 const clientReplica = new Client({
-	url: sharedEnvs.DATABASE_URL_REPLICA ?? dbUrl,
-	fetch: cachedFetch,
-	cast: (field, value) => {
-		if (field.type === 'JSON') {
-			// eslint-disable-next-line @typescript-eslint/no-unsafe-return
-			if (value === null) return null;
-			return JSONParse(decoder.decode(Uint8Array.from(bytes(value as string))));
-		}
-		// eslint-disable-next-line @typescript-eslint/no-unsafe-return
-		return cast(field, value);
-	},
-	format(query, args) {
-		if (
-			sharedEnvs.NODE_ENV === 'test' &&
-			!query.toUpperCase().startsWith('SELECT')
-		) {
-			throw new Error('execute on clientReplica must start with SELECT');
-		}
-		// eslint-disable-next-line @typescript-eslint/no-unsafe-argument
-		return format(query, args);
-	},
+  url: sharedEnvs.DATABASE_URL_REPLICA ?? dbUrl,
+  fetch: cachedFetch,
+  cast: (field, value) => {
+    if (field.type === 'JSON') {
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-return
+      if (value === null) return null;
+      return JSONParse(decoder.decode(Uint8Array.from(bytes(value as string))));
+    }
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-return
+    return cast(field, value);
+  },
+  format(query, args) {
+    if (
+      sharedEnvs.NODE_ENV === 'test' &&
+      !query.toUpperCase().startsWith('SELECT')
+    ) {
+      throw new Error('execute on clientReplica must start with SELECT');
+    }
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+    return format(query, args);
+  },
 });
 
 export const dbReplica: PlanetScaleDatabase<typeof schema> = psDrizzle(
-	// @ts-ignore
-	clientReplica,
-	{ schema },
+  // @ts-ignore
+  clientReplica,
+  { schema },
 );
