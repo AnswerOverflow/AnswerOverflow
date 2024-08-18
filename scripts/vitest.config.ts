4f4de1a8817c62b4f3bd0429a2d3a82518dad5da
JoltCode <joe@joecc.dev>
2024-08-18 13:27:48 +0100

    fix(testing): switch from c8 to v8

--- scripts/vitest.config.ts
+++ scripts/vitest.config.ts
@@ -3,30 +3,30 @@ import type { ESBuildOptions } from 'vite';
 import { defineConfig, type UserConfig } from 'vitest/config';
 
 export const createVitestConfig = (options: UserConfig = {}) =>
-	defineConfig({
-		...options,
-		test: {
-			...options?.test,
-			environment: 'node',
-			testTimeout: 60000,
-			globals: true,
-			coverage: {
-				...options.test?.coverage,
-				enabled: false,
-				provider: 'c8',
-				reporter: ['text', 'lcov', 'clover'],
-				exclude: [
-					...(options.test?.coverage?.exclude ?? []),
-					'**/node_modules/**',
-					'**/dist/**',
-					'**/tests/**',
-				],
-			},
-		},
-		esbuild: {
-			...options?.esbuild,
-			platform: 'node',
-			target:
-				(options?.esbuild as ESBuildOptions | undefined)?.target ?? 'esnext',
-		},
-	});
+  defineConfig({
+    ...options,
+    test: {
+      ...options?.test,
+      environment: 'node',
+      testTimeout: 60000,
+      globals: true,
+      coverage: {
+        ...options.test?.coverage,
+        enabled: false,
+        provider: 'v8',
+        reporter: ['text', 'lcov', 'clover'],
+        exclude: [
+          ...(options.test?.coverage?.exclude ?? []),
+          '**/node_modules/**',
+          '**/dist/**',
+          '**/tests/**',
+        ],
+      },
+    },
+    esbuild: {
+      ...options?.esbuild,
+      platform: 'node',
+      target:
+        (options?.esbuild as ESBuildOptions | undefined)?.target ?? 'esnext',
+    },
+  });
