46fde83d1f3cb90fbefccc877d71db108ca071a1
Rhys Sullivan <39114868+RhysSullivan@users.noreply.github.com>
2024-08-24 11:49:26 -0700

    Revert "next.config.mjs -> next.config.ts"
    This reverts commit f911faddeb2844926f755f74bf078bc04871c020.

--- packages/ui/next.config.mjs
+++ packages/ui/next.config.mjs
@@ -1,10 +1,11 @@
+// @ts-check
 /**
  * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation.
  * This is especially useful for Docker builds.
  */
-import { NextConfig } from 'next';
 
-const config: NextConfig = {
+/** @type {import("next").NextConfig} */
+const config = {
 	reactStrictMode: true,
 	i18n: {
 		locales: ['en'],
