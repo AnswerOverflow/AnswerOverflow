f911faddeb2844926f755f74bf078bc04871c020
Rhys Sullivan <39114868+RhysSullivan@users.noreply.github.com>
2024-08-24 10:56:56 -0700

    next.config.mjs -> next.config.ts

--- packages/ui/next.config.ts
+++ packages/ui/next.config.ts
@@ -1,11 +1,10 @@
-// @ts-check
 /**
  * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation.
  * This is especially useful for Docker builds.
  */
+import { NextConfig } from 'next';
 
-/** @type {import("next").NextConfig} */
-const config = {
+const config: NextConfig = {
 	reactStrictMode: true,
 	i18n: {
 		locales: ['en'],
