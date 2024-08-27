6b83d9388c2a37faa058b61880564558e11934f0
JoltCode <joe@joecc.dev>
2024-08-27 23:32:05 +0100

    chore: fix lint rules

--- packages/config/eslint-config-custom/next.js
+++ packages/config/eslint-config-custom/next.js
@@ -3,22 +3,9 @@
 // Reference: https://dev.to/mryechkin/streamlining-your-tailwind-css-workflow-with-eslint-prettier-1hg
 // eslint-disable-next-line no-undef
 module.exports = {
-	extends: ['./index.js', 'plugin:tailwindcss/recommended', 'next', 'prettier'],
-	env: {
-		browser: true,
-		es2021: true,
-	},
-	rules: {
-		'tailwindcss/no-custom-classname': [
-			'error', // TODO: Reenable this eventually, whitelist is not working
-			{
-				config: './tailwind.config.cjs',
-				whitelist: ['scrollbar-hide', 'dark'],
-			},
-		],
-		'tailwindcss/enforces-shorthand': 'off',
-		'tailwindcss/no-unnecessary-arbitrary-value': 'off',
-		// Disable tailwindcss rearrange class
-		'tailwindcss/classnames-order': 'off',
-	},
+  extends: ['./index.js', '@unocss', 'next', 'prettier'],
+  env: {
+    browser: true,
+    es2021: true,
+  },
 };
