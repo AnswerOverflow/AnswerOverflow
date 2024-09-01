ffbac745c33514bf62c0beb98001d67292249019
Rhys Sullivan <39114868+RhysSullivan@users.noreply.github.com>
2024-09-01 16:37:37 -0700

    fix(web): add link to community page to dashboard

--- apps/dashboard/src/app/dashboard/[serverId]/client.tsx
+++ apps/dashboard/src/app/dashboard/[serverId]/client.tsx
@@ -9,16 +9,30 @@ import {
 	TopQuestionSolversTable,
 } from './components/home';
 import { useDashboardContext } from './components/dashboard-context';
+import { makeMainSiteLink } from '@answeroverflow/constants';
+import { ExternalLinkIcon } from '@answeroverflow/ui/src/icons';
+import { LinkButton } from '@answeroverflow/ui/src/ui/link-button';
 
 export default function Client() {
 	const { options } = useDashboardContext();
 	return (
 		<div className="flex flex-col gap-4">
-			<DatePickerWithPresets
-				from={options.from}
-				to={options.to}
-				onValueChange={(range) => options.setRange(range)}
-			/>
+			<div className="flex flex-col justify-between gap-4 sm:flex-row">
+				<DatePickerWithPresets
+					from={options.from}
+					to={options.to}
+					onValueChange={(range) => options.setRange(range)}
+				/>
+				<LinkButton
+					href={makeMainSiteLink(`/c/${options.serverId}`)}
+					target="_blank"
+					variant={'outline'}
+					className="flex flex-row items-center gap-2 rounded-md border px-4 py-2"
+				>
+					View Community
+					<ExternalLinkIcon />
+				</LinkButton>
+			</div>
 			<div className="grid grid-cols-1 gap-4 xl:grid-cols-2">
 				<PageViewsLineChart />
 				<PopularPagesTable />
