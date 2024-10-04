60fe2a2489c75975b58e90261b53dd6ada269e14
Rhys Sullivan <39114868+RhysSullivan@users.noreply.github.com>
2024-10-04 00:31:27 -0400

    clean up stuff

--- v2/packages/ui/src/ui/follow.tsx
+++ v2/packages/ui/src/ui/follow.tsx
@@ -1,43 +1,44 @@
-'use client';
-import * as React from 'react';
-import { useSpring, animated } from 'react-spring';
+"use client";
+import * as React from "react";
+import { useSpring, animated } from "react-spring";
 
 export const FollowCursor = (props: {
-	className?: string;
-	children: React.ReactNode;
-	intensity?: number;
-	scale?: number;
+  className?: string;
+  children: React.ReactNode;
+  intensity?: number;
+  scale?: number;
 }) => {
-	const [spr, set] = useSpring(() => ({
-		xys: [0, 0, 1],
-		config: { mass: 2, tension: 350, friction: 100 },
-	}));
-	const animatedRef = React.useRef<HTMLDivElement>(null);
+  const [spr, set] = useSpring(() => ({
+    xys: [0, 0, 1],
+    config: { mass: 2, tension: 350, friction: 100 },
+  }));
+  const animatedRef = React.useRef<HTMLDivElement>(null);
 
-	const calc = (x: number, y: number) => {
-		if (!animatedRef.current) return [0, 0, 1];
-		const rect = animatedRef.current.getBoundingClientRect();
-		return [
-			-(y - rect.top - rect.height / 2) / (props.intensity ?? 10),
-			(x - rect.left - rect.width / 2) / (props.intensity ?? 10),
-			props.scale ?? 1,
-		];
-	};
+  const calc = (x: number, y: number) => {
+    if (!animatedRef.current) return [0, 0, 1];
+    const rect = animatedRef.current.getBoundingClientRect();
+    return [
+      -(y - rect.top - rect.height / 2) / (props.intensity ?? 10),
+      (x - rect.left - rect.width / 2) / (props.intensity ?? 10),
+      props.scale ?? 1,
+    ];
+  };
 
-	return (
-		<animated.div
-			className={props.className}
-			ref={animatedRef}
-			onMouseMove={({ clientX: x, clientY: y }) => set({ xys: calc(x, y) })}
-			onMouseLeave={() => set({ xys: [0, 0, 1] })}
-			style={{
-				transform: spr.xys.to(
-					(x, y, s) =>
-						`perspective(600px) rotateX(${x}deg) rotateY(${y}deg) scale(${s})`,
-				),
-			}}
-		>
-			{props.children}
-		</animated.div>
-	);
+  const AnimatedDiv = animated("div");
+  return (
+    <AnimatedDiv
+      className={props.className}
+      ref={animatedRef}
+      onMouseMove={({ clientX: x, clientY: y }) => set({ xys: calc(x, y) })}
+      onMouseLeave={() => set({ xys: [0, 0, 1] })}
+      style={{
+        transform: spr.xys.to(
+          (x, y, s) =>
+            `perspective(600px) rotateX(${x}deg) rotateY(${y}deg) scale(${s})`
+        ),
+      }}
+    >
+      {props.children}
+    </AnimatedDiv>
+  );
 };
