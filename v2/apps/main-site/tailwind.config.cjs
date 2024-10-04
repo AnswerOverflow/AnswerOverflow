51de9b740604420986adc3943ab23b425651e3cf
Rhys Sullivan <39114868+RhysSullivan@users.noreply.github.com>
2024-10-04 00:14:53 -0400

    19 broken

--- v2/apps/main-site/tailwind.config.cjs
+++ v2/apps/main-site/tailwind.config.cjs
@@ -1,12 +1,12 @@
 /** @type {import("tailwindcss").Config} */
-// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-const data = require('@answeroverflow/tailwind-config');
+
+const data = require("@answeroverflow/tailwind-config");
 module.exports = {
-	...data,
-	content: [
-		...data.content,
-		'../../packages/ui/src/**/*.{js,ts,jsx,tsx}', // Transpile breaks without this for tailwind styles
-		'./content/**/*.mdx',
-	],
-	plugins: [require('@tailwindcss/typography')],
+  ...data,
+  content: [
+    ...data.content,
+    "../../packages/ui/src/**/*.{js,ts,jsx,tsx}", // Transpile breaks without this for tailwind styles
+    "./content/**/*.mdx",
+  ],
+  plugins: [require("@tailwindcss/typography")],
 };
