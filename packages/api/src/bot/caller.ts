fd7d03484ad48edffef41bfeb911186e390bfdfa
Rhys Sullivan <39114868+RhysSullivan@users.noreply.github.com>
2024-10-27 22:41:25 -0700

    add auth to bot caller

--- packages/api/src/bot/caller.ts
+++ packages/api/src/bot/caller.ts
@@ -1,10 +1,14 @@
 import { createTRPCProxyClient, httpBatchLink } from '@trpc/client';
 import superjson from 'superjson';
 import { BotRouter } from './index';
+import { sharedEnvs } from '@answeroverflow/env/shared';
 export const botClient = createTRPCProxyClient<BotRouter>({
 	links: [
 		httpBatchLink({
 			url: process.env.BOT_URL ?? 'http://localhost:2022/',
+			headers: () => ({
+				Authorization: `Bearer ${sharedEnvs.DISCORD_CLIENT_SECRET}`,
+			}),
 		}),
 	],
 	transformer: superjson,
