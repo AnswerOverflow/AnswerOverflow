da2497071739d9ddf91ebe57d5f2eb2584d5a0cb
Rhys Sullivan <39114868+RhysSullivan@users.noreply.github.com>
2024-11-03 18:04:55 -0800

    fix op styling

--- packages/ui/src/message/Message.tsx
+++ packages/ui/src/message/Message.tsx
@@ -16,7 +16,11 @@ import { MessageAttachments } from './attachments';
 import { parse } from './markdown/render';
 import { MessageProps } from './props';
 
-export const MessageOPTag = () => <span className="text-muted-foreground border-2 border-muted-foreground px-1 rounded-md">OP</span>;
+export const MessageOPTag = () => (
+	<span className="text-muted-foreground border-[1.5px] text-sm border-muted-foreground px-1 rounded-md">
+		OP
+	</span>
+);
 
 export const MessageAuthorArea = (props: Pick<MessageProps, 'message'>) => {
 	const { message } = props;
