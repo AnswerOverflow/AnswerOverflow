8e9bbd326556a0dedd791b5846e59b013bc2f1ae
Rhys Sullivan <39114868+RhysSullivan@users.noreply.github.com>
2024-12-21 17:10:56 -0800

    Update stats-reporter.ts

--- apps/discord-bot/src/listeners/events/stats-reporter.ts
+++ apps/discord-bot/src/listeners/events/stats-reporter.ts
@@ -1,58 +1,12 @@
 import { Analytics } from '@answeroverflow/core/analytics';
-import { botEnv } from '@answeroverflow/env/bot';
 import { ApplyOptions } from '@sapphire/decorators';
 import { Events, Listener } from '@sapphire/framework';
 import { CronJob } from 'cron';
-import { Client } from 'discord.js';
-
-// Create a function to represent the stats in board format
-function createBoard(statistics: {
-	totalPageViews: number;
-	newServers: number;
-	questionsAsked: number;
-	serverInviteClicks: number;
-}) {
-	const board = Array(6)
-		.fill(0)
-		.map(() => Array(22).fill(0));
-	function writeRow(row: number, text: string) {
-		for (let i = 0; i < text.length; i++) {
-			const cell = board[row]?.[i];
-			if (cell !== undefined) {
-				// do character codes as they appear in the alphabet, i.e a is 1, b is 2, etc.
-				// if it is a number, do the number + 26
-				// a colon is 50, a space is 0
-				const charCode =
-					text.charCodeAt(i) === 58
-						? 50
-						: text.charCodeAt(i) === 32
-							? 0
-							: text.charCodeAt(i) - 96 > 0
-								? text.charCodeAt(i) - 96
-								: text.charCodeAt(i) - 64 + 26;
-				// if negative, log what was meant to be written
-				if (charCode < 0) {
-					console.error(
-						'Negative character code',
-						text.charCodeAt(i),
-						`character: ${text.at(i)}`,
-					);
-				}
-				board[row]![i] = charCode;
-			} else {
-				console.error('Cell not found', row, i);
-			}
-		}
-	}
-	writeRow(0, 'Daily AO Stats:');
-	writeRow(2, `Page Views: ${statistics.totalPageViews}`);
-	writeRow(3, `New Servers: ${statistics.newServers}`);
-	writeRow(4, `Questions Asked: ${statistics.questionsAsked}`);
-	writeRow(5, `Invites Used: ${statistics.serverInviteClicks}`);
-	return board;
-}
 
 async function reportStats() {
+	if (!process.env.VESTABOARD_API_KEY) {
+		return;
+	}
 	const totalPageViewsPromise = Analytics.queryClient!.query()
 		.addSeries('$pageview', {
 			sampling: 'total',
@@ -117,7 +71,6 @@ Page Views  :${totalPageViewsRounded}
 New Servers :${newServersRounded}
 Questions   :${questionsAskedRounded}
 Invites Used:${serverInviteClicksRounded}`;
-	console.log(wrapped);
 	const content = await fetch('https://vbml.vestaboard.com/format/', {
 		body: JSON.stringify({
 			message: wrapped,
