07280b6950c679b692d1a0f5eb6720e5d1908d3d
Rhys Sullivan <39114868+RhysSullivan@users.noreply.github.com>
2025-01-14 23:29:05 -0800

    fix missing invites

--- packages/api/src/bot/index.ts
+++ packages/api/src/bot/index.ts
@@ -1,12 +1,12 @@
 import { sharedEnvs } from '@answeroverflow/env/shared';
 import { TRPCError, initTRPC } from '@trpc/server';
 import { ChannelType } from 'discord-api-types/v10';
-import { Client } from 'discord.js';
+import { SapphireClient } from '@sapphire/framework';
 import superjson from 'superjson';
 import { z } from 'zod';
 
 type BotContext = {
-	client: Client;
+	client: SapphireClient;
 	token: string | undefined;
 };
 
@@ -35,6 +35,67 @@ export const botRouter = router({
 				return null;
 			}
 			return channel.availableTags;
+		}),
+	enableIndexing: protectedProcedure
+		.input(z.string())
+		.mutation(async ({ input, ctx }) => {
+			const channel = await ctx.client.channels.fetch(input);
+			if (!channel) {
+				throw new TRPCError({
+					code: 'NOT_FOUND',
+					message: 'Channel not found',
+				});
+			}
+			if (channel.isDMBased()) {
+				throw new TRPCError({
+					code: 'BAD_REQUEST',
+					message:
+						'Channel is a DM, can only enable indexing on server channels',
+				});
+			}
+			if (channel.isThread()) {
+				throw new TRPCError({
+					code: 'BAD_REQUEST',
+					message:
+						'Channel is a thread, can only enable indexing on text based channels',
+				});
+			}
+			if (channel.type === ChannelType.GuildCategory) {
+				throw new TRPCError({
+					code: 'BAD_REQUEST',
+					message:
+						'Channel is a category, can only enable indexing on text based channels',
+				});
+			}
+			const clientPermissions = channel.permissionsFor(channel.client.user.id);
+			if (!clientPermissions?.has('ReadMessageHistory')) {
+				throw new TRPCError({
+					code: 'FORBIDDEN',
+					message:
+						'You need to give the bot read message history permission to enable indexing',
+				});
+			}
+			if (
+				!clientPermissions.has('CreateInstantInvite') &&
+				!channel.guild.vanityURLCode
+			) {
+				throw new TRPCError({
+					code: 'FORBIDDEN',
+					message:
+						'You need to give the bot create invite permission to enable indexing',
+				});
+			}
+
+			const invite = await channel.createInvite({
+				maxAge: 0,
+				maxUses: 0,
+				reason: 'Channel indexing enabled invite',
+				unique: false,
+				temporary: false,
+			});
+			return {
+				invite: invite.code,
+			};
 		}),
 });
 
