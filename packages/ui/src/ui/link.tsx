7f27330a08d5bd80b048f95f145ceeb507e42491
Rhys Sullivan <39114868+RhysSullivan@users.noreply.github.com>
2025-06-03 22:38:24 -0700

    Revert "use next/link for subpaths"
    This reverts commit 4ff69da73264ff8dd2a901a7a072d04cf83e2780.

--- packages/ui/src/ui/link.tsx
+++ packages/ui/src/ui/link.tsx
@@ -18,6 +18,21 @@ export function Link(
 			? `/${serverContent.subpath}${rest.href}`
 			: rest.href;
 
+	if (finalHref !== rest.href) {
+		if (icon)
+			return (
+				<a
+					prefetch={false}
+					{...rest}
+					href={finalHref}
+					className={cn('flex flex-row items-center gap-2', className)}
+				>
+					{props.icon}
+					{props.children}
+				</a>
+			);
+		return <a prefetch={false} {...props} href={finalHref} />;
+	}
 	if (icon)
 		return (
 			<NextLink
