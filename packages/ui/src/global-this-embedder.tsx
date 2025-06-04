cd9b6b89edd20616689c0786c731a1ec2355fa26
Rhys Sullivan <39114868+RhysSullivan@users.noreply.github.com>
2025-06-03 23:26:32 -0700

    use reach cache to try to make server content per request?

--- packages/ui/src/global-this-embedder.tsx
+++ packages/ui/src/global-this-embedder.tsx
@@ -2,14 +2,24 @@
 import { useServerInsertedHTML } from 'next/navigation';
 import { useId } from 'react';
 import { GlobalThisEmbed } from './global-this-embed';
+import { cache } from 'react';
+
+const updateGlobalThis = cache((embedOnServer: GlobalThisEmbed) => {
+	if (typeof window === 'undefined') {
+		// @ts-expect-error - globalThis is not typed
+		globalThis.__SERVER_CONTENT = embedOnServer;
+	} else {
+		// @ts-expect-error - globalThis is not typed
+		globalThis.__SERVER_CONTENT = embedOnServer;
+	}
+});
 
 export function GlobalThisEmbedder(props: {
 	embedOnServer: GlobalThisEmbed;
 	children: React.ReactNode;
 }) {
 	const id = useId();
-	// @ts-expect-error - globalThis is not typed
-	globalThis.__SERVER_CONTENT = props.embedOnServer;
+	updateGlobalThis(props.embedOnServer);
 
 	useServerInsertedHTML(() => {
 		const html = [
