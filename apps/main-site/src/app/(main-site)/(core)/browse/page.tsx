e263ca8733f5b38d920e3417f7b8894efea47062
Rhys Sullivan <39114868+RhysSullivan@users.noreply.github.com>
2025-06-03 23:25:59 -0700

    change cache times + force static

--- apps/main-site/src/app/(main-site)/(core)/browse/page.tsx
+++ apps/main-site/src/app/(main-site)/(core)/browse/page.tsx
@@ -15,7 +15,7 @@ export const metadata: Metadata = {
 	},
 };
 
-export const revalidate = 86400;
+export const revalidate = 3600;
 export default async function BrowseCommunitiesPage() {
 	const data = await fetchBrowseServers();
 	return (
