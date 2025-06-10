b450742ecd74449438a40fbcac5a77d8895bdd5e
Rhys Sullivan <39114868+RhysSullivan@users.noreply.github.com>
2025-06-09 21:13:34 -0700

    add community page theme

--- packages/core/scripts/seed.ts
+++ packages/core/scripts/seed.ts
@@ -78,7 +78,21 @@ async function seedTwo() {
 		},
 		type: ChannelType.GuildForum,
 	});
+	const channel2 = mockChannelWithFlags(server, {
+		flags: {
+			indexingEnabled: true,
+			autoThreadEnabled: false,
+			forumGuidelinesConsentEnabled: false,
+			markSolutionEnabled: true,
+			sendMarkSolutionInstructionsInNewThreads: true,
+		},
+		name: 'Channel 2',
+		type: ChannelType.GuildForum,
+	});
 	const thread = mockThread(channel, {
+		type: ChannelType.PublicThread,
+	});
+	const thread2 = mockThread(channel2, {
 		type: ChannelType.PublicThread,
 	});
 	const account = mockDiscordAccount();
@@ -89,6 +103,12 @@ async function seedTwo() {
 			parentChannelId: channel.id,
 		}),
 	);
+	const messages2 = Array.from({ length: messagesToMock }, (_, i) =>
+		mockMessage(server, thread2, account, {
+			id: (BigInt(thread2.id) + BigInt(i)).toString(),
+			parentChannelId: channel2.id,
+		}),
+	);
 	messages.push(
 		mockMessage(server, thread, account, {
 			id: '100',
@@ -97,6 +117,21 @@ async function seedTwo() {
 				'Can you make sure that your internal address is composed of http://ip:port, ',
 		}),
 	);
+	messages2.push(
+		mockMessage(server, thread2, account, {
+			id: '400',
+			parentChannelId: channel2.id,
+			content: 'This is a test message',
+		}),
+	);
+	await upsertChannel({
+		create: channel2,
+		update: channel2,
+	});
+	await upsertChannel({
+		create: thread2,
+		update: thread2,
+	});
 	await upsertServer({
 		create: server,
 		update: server,
@@ -111,6 +146,7 @@ async function seedTwo() {
 	});
 	await upsertDiscordAccount(account);
 	await upsertManyMessages(messages);
+	await upsertManyMessages(messages2);
 }
 
 void (async () => {
