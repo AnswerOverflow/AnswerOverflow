94e1b3ce3c1a3b1e471e4b54a2cd0a8181e2ca23
Rhys Sullivan <39114868+RhysSullivan@users.noreply.github.com>
2025-11-30 17:38:09 -0800

    Update json-ld-script.tsx

--- apps/main-site/src/components/json-ld-script.tsx
+++ apps/main-site/src/components/json-ld-script.tsx
@@ -5,6 +5,7 @@ export function JsonLdScript(props: { data: object; scriptKey: string }) {
 		<script
 			type="application/ld+json"
 			id={props.scriptKey}
+			// biome-ignore lint/security/noDangerouslySetInnerHtml: This is safe because the data is serialized
 			dangerouslySetInnerHTML={{
 				__html: serialize(props.data, { isJSON: true }),
 			}}
