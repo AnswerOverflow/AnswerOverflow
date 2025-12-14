a2c85dec3ba421d458bd08f4310e774e3a11f629
Rhys Sullivan <39114868+RhysSullivan@users.noreply.github.com>
2025-12-13 20:43:16 -0800

    remove leftover metadata

--- apps/main-site/src/app/layout.tsx
+++ apps/main-site/src/app/layout.tsx
@@ -8,34 +8,9 @@ import { NavbarWrapper } from "../components/navbar-wrapper";
 const isProduction = process.env.NEXT_PUBLIC_DEPLOYMENT_ENV === "production";
 
 export const metadata: Metadata = {
-	title: "Answer Overflow - Discord Content Discovery",
-	metadataBase: new URL("https://www.answeroverflow.com/"),
-	description:
-		"Build the best Discord support server with Answer Overflow. Index your content into Google, answer questions with AI, and gain insights into your community.",
 	robots: {
 		index: isProduction,
 		follow: isProduction,
-	},
-	openGraph: {
-		type: "website",
-		title: "Answer Overflow - Discord Content Discovery",
-		siteName: "Answer Overflow",
-		description:
-			"Build the best Discord support server with Answer Overflow. Index your content into Google, answer questions with AI, and gain insights into your community.",
-		images: [
-			{
-				url: "https://www.answeroverflow.com/answer-overflow-banner-v3.png",
-				width: 1200,
-				height: 630,
-			},
-		],
-	},
-	twitter: {
-		card: "summary_large_image",
-		title: "Answer Overflow - Discord Content Discovery",
-		description:
-			"Build the best Discord support server with Answer Overflow. Index your content into Google, answer questions with AI, and gain insights into your community.",
-		images: ["https://www.answeroverflow.com/answer-overflow-banner-v3.png"],
 	},
 };
 
