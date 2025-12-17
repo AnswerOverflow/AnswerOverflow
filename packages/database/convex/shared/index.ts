9bb59e3d5760adc0500edb8e751f56365e665d84
Rhys Sullivan <39114868+RhysSullivan@users.noreply.github.com>
2025-12-17 01:03:07 -0800

    enrich suggested messages

--- packages/database/convex/shared/index.ts
+++ packages/database/convex/shared/index.ts
@@ -28,7 +28,6 @@ export {
 	findMessagesByAuthorId,
 	findReactionsByMessageId,
 	findSolutionsByQuestionId,
-	getFirstMessageInChannel,
 	getFirstMessagesInChannels,
 	getMessageById,
 	getThreadStartMessage,
