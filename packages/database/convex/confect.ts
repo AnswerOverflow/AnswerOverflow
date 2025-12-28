9d304c85c0e2d1bc877192c34725e94da9ec28e2
Rhys Sullivan <39114868+RhysSullivan@users.noreply.github.com>
2025-12-28 14:46:25 +0000

    feat: add confect schemas and setup in database

---
+++ packages/database/convex/confect.ts
@@ -0,0 +1,40 @@
+import {
+	ConfectActionCtx as ConfectActionCtxService,
+	type ConfectActionCtx as ConfectActionCtxType,
+	type ConfectDataModelFromConfectSchemaDefinition,
+	type ConfectDoc as ConfectDocType,
+	ConfectMutationCtx as ConfectMutationCtxService,
+	type ConfectMutationCtx as ConfectMutationCtxType,
+	ConfectQueryCtx as ConfectQueryCtxService,
+	type ConfectQueryCtx as ConfectQueryCtxType,
+	makeFunctions,
+	type TableNamesInConfectDataModel,
+} from "@packages/confect/server";
+import { confectSchema } from "./schema";
+
+export const {
+	query,
+	mutation,
+	action,
+	internalQuery,
+	internalMutation,
+	internalAction,
+} = makeFunctions(confectSchema);
+
+type ConfectSchema = typeof confectSchema;
+
+type ConfectDataModel =
+	ConfectDataModelFromConfectSchemaDefinition<ConfectSchema>;
+
+export type ConfectDoc<
+	TableName extends TableNamesInConfectDataModel<ConfectDataModel>,
+> = ConfectDocType<ConfectDataModel, TableName>;
+
+export const ConfectQueryCtx = ConfectQueryCtxService<ConfectDataModel>();
+export type ConfectQueryCtx = ConfectQueryCtxType<ConfectDataModel>;
+
+export const ConfectMutationCtx = ConfectMutationCtxService<ConfectDataModel>();
+export type ConfectMutationCtx = ConfectMutationCtxType<ConfectDataModel>;
+
+export const ConfectActionCtx = ConfectActionCtxService<ConfectDataModel>();
+export type ConfectActionCtx = ConfectActionCtxType<ConfectDataModel>;
