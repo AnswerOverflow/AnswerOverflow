c6efe5c2df0dbcbae6003c8afadc87456296b202
Rhys Sullivan <39114868+RhysSullivan@users.noreply.github.com>
2026-04-01 01:48:13 -0700

    the real april fools was trying to do an april fools

--- apps/main-site/src/app/(main-site)/layout.tsx
+++ apps/main-site/src/app/(main-site)/layout.tsx
@@ -3,7 +3,6 @@ import { ScrollContainerProvider } from "@packages/ui/hooks/use-scroll-container
 import type { Metadata } from "next";
 import { Suspense } from "react";
 import { MainSiteFooter, MainSiteNavbar } from "@/components/navbar-wrapper";
-import { StarWarsTransition } from "@/components/star-wars-transition";
 
 export const metadata: Metadata = {
 	title: "Answer Overflow - Discord Content Discovery",
@@ -44,17 +43,15 @@ export default function MainSiteLayout({
 }) {
 	return (
 		<Providers tenant={null}>
-			<StarWarsTransition>
-				<ScrollContainerProvider>
-					<Suspense>
-						<MainSiteNavbar />
-					</Suspense>
-					<div className="pt-navbar">{children}</div>
-					<Suspense>
-						<MainSiteFooter />
-					</Suspense>
-				</ScrollContainerProvider>
-			</StarWarsTransition>
+			<ScrollContainerProvider>
+				<Suspense>
+					<MainSiteNavbar />
+				</Suspense>
+				<div className="pt-navbar">{children}</div>
+				<Suspense>
+					<MainSiteFooter />
+				</Suspense>
+			</ScrollContainerProvider>
 		</Providers>
 	);
 }
